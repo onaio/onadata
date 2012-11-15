030c333aac45145cf5a1e340ce37744f464c94c3
Larry Weya <larryweya@gmail.com>
2012-11-15 11:40:49 +0300

    Removed EnvJasmine

--- js_tests/utils.js
+++
@@ -1,7 +0,0 @@
-if(typeof(Array.prototype.reduce) === "undefined" || Array.prototype.reduce === null)
-{
-    Array.prototype.reduce = function(fn)
-    {
-        return _.reduce(this, fn);
-    }
-}
