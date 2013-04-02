e0110b964c77ee7285d42e2815380e73108d422d
Ukang'a Dickson <ukanga@gmail.com>
2013-04-02 17:36:23 +0300

    import InstanceHistory and ColumnRename into models/__init__.py

--- odk_logger/models/__init__.py
+++ odk_logger/models/__init__.py
@@ -1,5 +1,5 @@
 from attachment import Attachment
-from instance import Instance
+from instance import Instance, InstanceHistory
 from survey_type import SurveyType
 from xform import XForm
 from odk_logger.xform_instance_parser import InstanceParseError
