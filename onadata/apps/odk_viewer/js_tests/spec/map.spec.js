cd01923fa9af66dbee23cf0e45b4f84318a001fa
Larry Weya <larryweya@gmail.com>
2013-11-06 12:59:22 +0300

    basic marker popup

--- onadata/apps/odk_viewer/js_tests/spec/map.spec.js
+++ onadata/apps/odk_viewer/js_tests/spec/map.spec.js
@@ -164,4 +164,35 @@ describe("FeatureLayer", function () {
             });
         });
     });
+
+    describe("templateFromFields", function () {
+        it("creates a table row for each question", function () {
+            var fields = new FH.FieldSet(),
+                raw_questions = [
+                    {
+                        name: "name",
+                        type: "text",
+                        label: "Name"
+                    },
+                    {
+                        name: "age",
+                        type: "integer",
+                        label: "Age"
+                    }
+                ],
+                result;
+
+            FH.Form.parseQuestions(raw_questions).forEach( function (field) {
+                fields.add(field);
+            });
+
+            result = FH.FeatureLayer.templateFromFields(fields);
+            expect(result).toEqual(
+                '<table class="table table-bordered table-striped">' +
+                  '<tr><th>Question</th><th>Response</th></tr>' +
+                  '<tr><td>Name</td><td><%= record["name"] %></td></tr>' +
+                  '<tr><td>Age</td><td><%= record["age"] %></td></tr>' +
+                '</table>');
+        });
+    });
 });
