dd9049641e5da2b3cddf93c03977669d4bcaa864
Ukang'a Dickson <ukanga@gmail.com>
2013-11-22 12:37:42 +0300

    UK: use api function for submission stats

--- stats/utils.py
+++ stats/utils.py
@@ -1,32 +1,7 @@
-from odk_viewer.models import ParsedInstance
+from api.tools import get_form_submissions_grouped_by_field
 
 
 def get_form_submissions_per_day(xform):
     """Number of submissions per day for the form."""
-    query = {}
-    query[ParsedInstance.USERFORM_ID] =\
-        u'%s_%s' % (xform.user.username, xform.id_string)
-    pipeline = [
-        {
-            "$group": {
-                "_id": {
-                    "$substr": ['$_submission_time', 0, 10]
-                },
-                "count": {"$sum": 1}
-            }
-        },
-        {
-            "$sort": {"_id": 1}
-        },
-        {
-            "$project": {
-                "date": "$_id",
-                "count": 1
-            }
-        }
-    ]
-    kargs = {
-        'query': query,
-        'pipeline': pipeline
-    }
-    return ParsedInstance.mongo_aggregate(**kargs)
+    return get_form_submissions_grouped_by_field(xform, '_submission_time',
+                                                 'date')
