f8e50d1fc8d8c486ad21dcb56c25497eedcdee20
Ukang'a Dickson <ukanga@gmail.com>
2013-11-23 02:02:22 +0300

    uk: mock enketo queries

--- main/tests/test_form_enter_data.py
+++ main/tests/test_form_enter_data.py
@@ -10,6 +10,8 @@ from django.test import RequestFactory
 from django.core.urlresolvers import reverse
 from django.core.validators import URLValidator
 from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
+
 from nose import SkipTest
 
 from test_base import MainTestCase
@@ -52,20 +54,21 @@ class TestFormEnterData(MainTestCase):
 
         #for enketo use only
 
-    def _running_enketo(self):
+    def _running_enketo(self, check_url=False):
         if hasattr(settings, 'ENKETO_URL') and \
-                self._check_url(settings.ENKETO_URL):
+                (not check_url or self._check_url(settings.ENKETO_URL)):
             return True
         return False
 
     def test_enketo_remote_server(self):
         if not self._running_enketo():
             raise SkipTest
-        server_url = 'https://testserver.com/bob'
-        form_id = "test_%s" % re.sub(re.compile("\."), "_", str(time()))
-        url = enketo_url(server_url, form_id)
-        self.assertIsInstance(url, basestring)
-        self.assertIsNone(URLValidator()(url))
+        with HTTMock(enketo_mock):
+            server_url = 'https://testserver.com/bob'
+            form_id = "test_%s" % re.sub(re.compile("\."), "_", str(time()))
+            url = enketo_url(server_url, form_id)
+            self.assertIsInstance(url, basestring)
+            self.assertIsNone(URLValidator()(url))
 
     def _get_grcode_view_response(self):
         factory = RequestFactory()
@@ -92,17 +95,18 @@ class TestFormEnterData(MainTestCase):
     def test_enter_data_redir(self):
         if not self._running_enketo():
             raise SkipTest
-        factory = RequestFactory()
-        request = factory.get('/')
-        request.user = self.user
-        response = enter_data(
-            request, self.user.username, self.xform.id_string)
-        #make sure response redirect to an enketo site
-        enketo_base_url = urlparse(settings.ENKETO_URL).netloc
-        redirected_base_url = urlparse(response['Location']).netloc
-        #TODO: checking if the form is valid on enketo side
-        self.assertIn(enketo_base_url, redirected_base_url)
-        self.assertEqual(response.status_code, 302)
+        with HTTMock(enketo_mock):
+            factory = RequestFactory()
+            request = factory.get('/')
+            request.user = self.user
+            response = enter_data(
+                request, self.user.username, self.xform.id_string)
+            #make sure response redirect to an enketo site
+            enketo_base_url = urlparse(settings.ENKETO_URL).netloc
+            redirected_base_url = urlparse(response['Location']).netloc
+            #TODO: checking if the form is valid on enketo side
+            self.assertIn(enketo_base_url, redirected_base_url)
+            self.assertEqual(response.status_code, 302)
 
     def test_enter_data_no_permission(self):
         response = self.anon.get(self.url)
@@ -123,9 +127,15 @@ class TestFormEnterData(MainTestCase):
         self.xform.save()
         response = self.anon.get(self.show_url)
         self.assertEqual(response.status_code, 302)
-        response = self.anon.get(self.url)
-        status_code = 302 if self._running_enketo() else 403
-        self.assertEqual(response.status_code, status_code)
+        if not self._running_enketo():
+            raise SkipTest
+        with HTTMock(enketo_mock):
+            factory = RequestFactory()
+            request = factory.get('/')
+            request.user = AnonymousUser()
+            response = enter_data(
+                request, self.user.username, self.xform.id_string)
+            self.assertEqual(response.status_code, 302)
 
     def test_enter_data_non_existent_user(self):
         url = reverse(enter_data, kwargs={
