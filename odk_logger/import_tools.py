5d66fbad27587929804410414b69eb3b6c4d1ca4
pld <peter@helioid.com>
2013-11-30 23:44:50 -0500

    PLD: use with remove unused variables

--- odk_logger/import_tools.py
+++ odk_logger/import_tools.py
@@ -31,7 +31,8 @@ from odk_logger.xform_fs import XFormInstanceFS
 
 def django_file(path, field_name, content_type):
     # adapted from here:
-    # http://groups.google.com/group/django-users/browse_thread/thread/834f988876ff3c45/
+    # http://groups.google.com/group/django-users/browse_thread/thread/
+    # 834f988876ff3c45/
     f = open(path)
     return InMemoryUploadedFile(
         file=f,
@@ -45,27 +46,30 @@ def django_file(path, field_name, content_type):
 
 def import_instance(path_to_instance_folder, status, user):
     xml_files = glob.glob(os.path.join(path_to_instance_folder, "*.xml"))
+
     if len(xml_files) < 1:
         return
     if len(xml_files) > 1:
         raise Exception(_("Too many XML files."))
-    xml_file = django_file(xml_files[0],
-                           field_name="xml_file",
-                           content_type="text/xml")
-    images = []
-    for jpg in glob.glob(os.path.join(path_to_instance_folder, "*.jpg")):
-        image_file = django_file(jpg,
-                                 field_name="image",
-                                 content_type="image/jpeg")
-        images.append(image_file)
-    # todo: if an instance has been submitted make sure all the
-    # files are in the database.
-    # there shouldn't be any instances with a submitted status in the
-    instance = create_instance(user.username, xml_file, images, status)
-    # close the files
-    xml_file.close()
-    for i in images:
-        i.close()
+
+    instance = None
+
+    with django_file(xml_files[0], field_name="xml_file",
+                     content_type="text/xml") as xml_file:
+        images = []
+        for jpg in glob.glob(os.path.join(path_to_instance_folder, "*.jpg")):
+            image_file = django_file(jpg, field_name="image",
+                                     content_type="image/jpeg")
+            images.append(image_file)
+        # todo: if an instance has been submitted make sure all the
+        # files are in the database.
+        # there shouldn't be any instances with a submitted status in the
+        instance = create_instance(user.username, xml_file, images, status)
+
+        # close the files
+        for i in images:
+            i.close()
+
     return instance
 
 
@@ -73,6 +77,7 @@ def iterate_through_odk_instances(dirpath, callback):
     total_file_count = 0
     success_count = 0
     errors = []
+
     for directory, subdirs, subfiles in os.walk(dirpath):
         for filename in subfiles:
             filepath = os.path.join(directory, filename)
@@ -84,11 +89,11 @@ def iterate_through_odk_instances(dirpath, callback):
                     errors.append("%s => %s" % (xfxs.filename, str(e)))
                 del(xfxs)
                 total_file_count += 1
+
     return (total_file_count, success_count, errors)
 
 
 def import_instances_from_zip(zipfile_path, user, status="zip"):
-    count = 0
     try:
         temp_directory = tempfile.mkdtemp()
         zf = zipfile.ZipFile(zipfile_path)
@@ -102,30 +107,32 @@ def import_instances_from_zip(zipfile_path, user, status="zip"):
     finally:
         shutil.rmtree(temp_directory)
 
+
 def import_instances_from_path(path, user, status="zip"):
-    count = 0
     def callback(xform_fs):
         """
         This callback is passed an instance of a XFormInstanceFS.
         See xform_fs.py for more info.
         """
-        xml_file = django_file(xform_fs.path,
-            field_name="xml_file",
-            content_type="text/xml")
-        images = [django_file(jpg, field_name="image",
-            content_type="image/jpeg") for jpg in xform_fs.photos]
-        # TODO: if an instance has been submitted make sure all the
-        # files are in the database.
-        # there shouldn't be any instances with a submitted status in the
-        instance = create_instance(user.username, xml_file, images, status)
-        # close the files
-        xml_file.close()
-        for i in images:
-            i.close()
-        if instance:
-            return 1
-        else:
-            return 0
+        with django_file(xform_fs.path, field_name="xml_file",
+                         content_type="text/xml") as xml_file:
+            images = [django_file(jpg, field_name="image",
+                      content_type="image/jpeg") for jpg in xform_fs.photos]
+            # TODO: if an instance has been submitted make sure all the
+            # files are in the database.
+            # there shouldn't be any instances with a submitted status in the
+            # import.
+            instance = create_instance(user.username, xml_file, images, status)
+
+            for i in images:
+                i.close()
+
+            if instance:
+                return 1
+            else:
+                return 0
+
     total_count, success_count, errors = iterate_through_odk_instances(
         path, callback)
+
     return (total_count, success_count, errors)
