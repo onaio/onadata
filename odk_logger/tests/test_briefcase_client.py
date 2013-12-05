e2af4b8b9f21fdbadfb37ee3a200869537623bec
Ukang'a Dickson <ukanga@gmail.com>
2013-12-05 12:10:16 +0300

    uk: stub test for briefcase client

--- odk_logger/tests/test_briefcase_client.py
+++ odk_logger/tests/test_briefcase_client.py
@@ -1,10 +1,20 @@
+import os.path
+from django.core.files.storage import get_storage_class
 from main.tests.test_base import MainTestCase
+
+from utils.briefcase_client import BriefcaseClient
 
 
 class TestBriefcaseAPI(MainTestCase):
 
     def setUp(self):
         super(MainTestCase, self).setUp()
+        self._publish_transportation_form()
+        self._submit_transport_instance_w_attachment()
+        self._logout()
+        self._create_user_and_login('deno', 'deno')
+        self.bc = BriefcaseClient(
+            username='bob', password='bob', url=self.base_url, user=self.user)
 
     def test_download_xform_xml(self):
         """
@@ -12,7 +22,12 @@ class TestBriefcaseAPI(MainTestCase):
         """
         # check that [user]/briefcase/forms folder is created for user deno
         # check that [user]/briefcase/forms/[id_string].xml is created
-        pass
+        self.bc.download_forms()
+        storage = get_storage_class()()
+        forms_folder_path = os.path.join('deno', 'briefcase', 'forms')
+        self.assertTrue(storage.exists(forms_folder_path))
+        forms_path = os.path.join(forms_folder_path, 'tutorial.xml')
+        self.assertTrue(storage.exists(forms_path))
 
     def test_download_form_media(self):
         """
