4945e5cf308f0aacf7b1c5816cc6819c59a9bc37
pld <peter@helioid.com>
2013-12-11 04:27:30 -0500

    PLD: use context to simplify run in virtual env

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,7 @@
 import os
 import sys
 
-from fabric.api import env, run, cd
-
-
-DEFAULTS = {
-    'home': '/home/wsgi/srv/',
-    'repo_name': 'formhub',
-}
+from fabric.api import cd, env, prefix, run
 
 DEPLOYMENTS = {
     'dev': {
@@ -15,7 +9,6 @@ DEPLOYMENTS = {
         'host_string': 'ubuntu@dev.ona.io',
         'project': 'ona',
         'key_filename': os.path.expanduser('~/.ssh/ona.pem'),
-        'virtualenv': '/home/ubuntu/.virtualenvs/ona',
         'celeryd': '/etc/init.d/celeryd-ona'
     },
     'prod': {
@@ -38,13 +31,8 @@ DEPLOYMENTS = {
 }
 
 
-def run_in_virtualenv(command):
-    d = {
-        'activate': os.path.join(
-            env.virtualenv, 'bin', 'activate'),
-        'command': command,
-    }
-    run('source %(activate)s && %(command)s' % d)
+def source(path):
+    return prefix('source %s' % path)
 
 
 def check_key_filename(deployment_name):
@@ -57,10 +45,14 @@ def check_key_filename(deployment_name):
 
 
 def setup_env(deployment_name):
-    env.update(DEFAULTS)
     env.update(DEPLOYMENTS[deployment_name])
+
     if not check_key_filename(deployment_name):
         sys.exit(1)
+
+    env.virtualenv = os.path.join('/home', 'ubuntu', '.virtualenvs',
+                                  env.project, 'bin', 'activate')
+
     env.code_src = os.path.join(env.home, env.project)
     env.pip_requirements_file = os.path.join(env.code_src, 'requirements.pip')
 
@@ -73,17 +65,19 @@ def deploy(deployment_name, branch='master'):
         run("git submodule init")
         run("git submodule update")
         run('find . -name "*.pyc" -exec rm -rf {} \;')
+
     # numpy pip install from requirements file fails
-    run_in_virtualenv("pip install numpy")
-    run_in_virtualenv("pip install -r %s" % env.pip_requirements_file)
+    with source(env.virtualenv):
+        run("pip install numpy")
+        run("pip install -r %s" % env.pip_requirements_file)
+
     with cd(env.code_src):
-        run_in_virtualenv(
-            "python manage.py syncdb --settings=formhub.local_settings")
-        run_in_virtualenv(
-            "python manage.py migrate --settings=formhub.local_settings")
-        run_in_virtualenv(
-            "python manage.py collectstatic --settings=formhub.local_settings "
-            "--noinput")
+        with source(env.virtualenv):
+            run("python manage.py syncdb --settings=formhub.local_settings")
+            run("python manage.py migrate --settings=formhub.local_settings")
+            run("python manage.py collectstatic"
+                " --settings=formhub.local_settings --noinput")
+
     run("sudo %s restart" % env.celeryd)
     #run("sudo /etc/init.d/celerybeat-ona restart")
     run("sudo /usr/local/bin/uwsgi --reload /var/run/ona.pid")
