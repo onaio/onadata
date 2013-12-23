b62fda18fd75b1db855036ea803bd38b41225eb7
Ukang'a Dickson <ukanga@gmail.com>
2013-12-23 21:16:36 +0300

    UK: median util functions

--- api/tools.py
+++ api/tools.py
@@ -1,3 +1,5 @@
+import numpy
+
 from datetime import datetime
 
 from django.contrib.auth.models import User, Permission
@@ -182,3 +184,44 @@ def get_form_submissions_grouped_by_field(xform, field, name=None):
     for record in records:
         del record['_id']
     return records
+
+
+def get_numeric_fields(xform):
+    """List of numeric field names for specified xform"""
+    k = []
+    dd = xform.data_dictionary()
+    for element in dd.get_survey_elements_of_type('integer'):
+        name = _encode_for_mongo(element.get_abbreviated_xpath())
+        k.append(name)
+    for element in dd.get_survey_elements_of_type('decimal'):
+        name = _encode_for_mongo(element.get_abbreviated_xpath())
+        k.append(name)
+    return k
+
+
+def get_median_for_field(field, xform):
+    username = xform.user.username
+    id_string = xform.id_string
+    query = None
+    sort = None
+    query = {}
+    mongo_field = _encode_for_mongo(field)
+    query[ParsedInstance.USERFORM_ID] =\
+        u'%s_%s' % (xform.user.username, xform.id_string)
+    query[mongo_field] = {"$exists": True}
+    sort = {mongo_field: 1}
+
+    # check if requested field a datetime str
+    fields = [mongo_field]
+
+    cursor = ParsedInstance.query_mongo(
+        username, id_string, query, fields, sort)
+    return numpy.median([float(i[mongo_field]) for i in cursor])
+
+
+def get_median_for_numeric_fields_in_form(xform):
+    data = {}
+    for field_name in get_numeric_fields(xform):
+        median = get_median_for_field(field_name, xform)
+        data.update({field_name: median})
+    return data
