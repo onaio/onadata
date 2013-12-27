e9f7b1284873210775195158b7681a0bf40bf29d
pld <peter@helioid.com>
2013-12-27 17:56:07 -0500

    PLD: unit test for tools

---
+++ api/tests/test_tools.py
@@ -0,0 +1,21 @@
+from api.tools import get_form_submissions_grouped_by_field
+from main.tests.test_base import MainTestCase
+
+
+class TestTools(MainTestCase):
+
+    def setUp(self):
+        super(self.__class__, self).setUp()
+        self._create_user_and_login()
+        self._publish_transportation_form()
+        self._make_submissions()
+
+    def test_form_submissions_grouped_by_field(self):
+        xform = self.user.xforms.all()[0]
+        field = '_status'
+        count = len(xform.surveys.all())
+
+        result = get_form_submissions_grouped_by_field(xform, field)[0]
+
+        self.assertEqual(result['count'], count)
+        self.assertTrue(field in result.keys())
