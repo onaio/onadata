384ab9b9607b81a15e63f51c3651aa0902f876f3
pld <peter@helioid.com>
2014-01-02 14:59:38 -0500

    PLD: KCAT-74 use transaction test case to to ensure rollback takes effect

--- main/tests/test_base.py
+++ main/tests/test_base.py
@@ -9,15 +9,14 @@ from cStringIO import StringIO
 
 from django.contrib.auth.models import User
 from django_digest.test import Client as DigestClient
-from django.test import TestCase
-# from django_nose import FastFixtureTestCase as TestCase
+from django.test import TransactionTestCase
 from django.test.client import Client
 
 from odk_logger.models import XForm, Instance, Attachment
 from django.conf import settings
 
 
-class MainTestCase(TestCase):
+class MainTestCase(TransactionTestCase):
 
     surveys = ['transport_2011-07-25_19-05-49',
                'transport_2011-07-25_19-05-36',
