7f8cbfb3b3fed7eab9207ab70c2966709279bfe8
pld <peter@helioid.com>
2014-01-05 10:41:42 -0500

    PLD: templates path changes

--- settings/common.py
+++ settings/common.py
@@ -157,7 +157,7 @@ USE_TZ = True
 
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'templates'),
+    os.path.join(PROJECT_ROOT, 'libs/templates'),
     # Put strings here, like "/home/html/django_templates"
     # or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
