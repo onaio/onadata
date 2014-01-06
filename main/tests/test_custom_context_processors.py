90c6cdff468a0e8735d5f12fe282ee781e2d80d4
Ukang'a Dickson <ukanga@gmail.com>
2014-01-06 11:50:35 +0300

    UK: restore site_id, other tests should not be affected now

--- main/tests/test_custom_context_processors.py
+++ main/tests/test_custom_context_processors.py
@@ -7,6 +7,8 @@ class CustomContextProcessorsTest(TestCase):
     def test_site_name(self):
         context = site_name(None)
         self.assertEqual(context, {'SITE_NAME': 'example.com'})
+        restore_site_id = settings.SITE_ID
         settings.SITE_ID = 100
         context = site_name(None)
         self.assertEqual(context, {'SITE_NAME': 'example.org'})
+        settings.SITE_ID = restore_site_id
