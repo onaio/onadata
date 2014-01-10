8be9d68c5f5da320df0fd5bfa7e176824b412013
pld <peter@helioid.com>
2014-01-09 21:24:17 -0500

    PLD: set time in get_dict, test to ensure time is set

--- onadata/apps/odk_logger/tests/models/test_instance.py
+++ onadata/apps/odk_logger/tests/models/test_instance.py
@@ -1,5 +1,8 @@
+from mock import patch
+
 from onadata.apps.main.tests.test_base import TestBase
 from onadata.apps.odk_logger.models import Instance
+from onadata.libs.utils.common_tags import SUBMISSION_TIME
 
 
 class TestInstance(TestBase):
@@ -13,3 +16,13 @@ class TestInstance(TestBase):
 
         for instance in instances:
             self.assertNotEqual(instance.json, {})
+
+    @patch('onadata.apps.odk_logger.models.instance.submission_time')
+    def test_get_dict_assigns_submission_time(self, mock_time):
+        self._set_mock_time(mock_time)
+
+        instances = Instance.objects.all()
+
+        for instance in instances:
+            self.assertEqual(instance.get_dict()[SUBMISSION_TIME],
+                             mock_time.return_value)
