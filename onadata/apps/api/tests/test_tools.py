c97f2ae7b64c9ddbb5399db4f5993945bdc0cd00
pld <peter@helioid.com>
2014-01-10 09:34:01 -0500

    PLD: test submission time as well, mock time

--- onadata/apps/api/tests/test_tools.py
+++ onadata/apps/api/tests/test_tools.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from mock import patch
 from nose.tools import raises
 
 from onadata.apps.api.tools import get_form_submissions_grouped_by_field
@@ -10,35 +12,47 @@ class TestTools(TestBase):
         super(self.__class__, self).setUp()
         self._create_user_and_login()
         self._publish_transportation_form()
+
+    @patch('django.utils.timezone.now')
+    def test_get_form_submissions_grouped_by_field(self, mock_time):
+        mock_time.return_value = datetime.now()
         self._make_submissions()
 
-    def test_get_form_submissions_grouped_by_field(self):
         count_key = 'count'
-        field = '_xform_id_string'
+        fields = ['_submission_time', '_xform_id_string']
 
         xform = self.user.xforms.all()[0]
         count = len(xform.surveys.all())
 
-        result = get_form_submissions_grouped_by_field(xform, field)[0]
+        for field in fields:
+            result = get_form_submissions_grouped_by_field(xform, field)[0]
 
-        self.assertEqual([field, count_key], sorted(result.keys()))
-        self.assertEqual(result[count_key], count)
+            self.assertEqual([field, count_key], sorted(result.keys()))
+            self.assertEqual(result[count_key], count)
 
-    def test_get_form_submissions_grouped_by_field_sets_name(self):
+    @patch('django.utils.timezone.now')
+    def test_get_form_submissions_grouped_by_field_sets_name(self, mock_time):
+        mock_time.return_value = datetime.now()
+        self._make_submissions()
+
         count_key = 'count'
-        field = '_xform_id_string'
+        fields = ['_submission_time', '_xform_id_string']
         name = '_my_name'
 
         xform = self.user.xforms.all()[0]
         count = len(xform.surveys.all())
 
-        result = get_form_submissions_grouped_by_field(xform, field, name)[0]
+        for field in fields:
+            result = get_form_submissions_grouped_by_field(
+                xform, field, name)[0]
 
-        self.assertEqual([name, count_key], sorted(result.keys()))
-        self.assertEqual(result[count_key], count)
+            self.assertEqual([name, count_key], sorted(result.keys()))
+            self.assertEqual(result[count_key], count)
 
     @raises(ValueError)
     def test_get_form_submissions_grouped_by_field_bad_field(self):
+        self._make_submissions()
+
         field = '_bad_field'
         xform = self.user.xforms.all()[0]
 
