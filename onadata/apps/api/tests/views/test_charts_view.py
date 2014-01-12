7db3ed2e58a0ace57464a6ab6963352acda49897
Larry Weya <larryweya@gmail.com>
2014-01-12 20:19:30 +0300

    Chart view tests

--- onadata/apps/api/tests/views/test_charts_view.py
+++ onadata/apps/api/tests/views/test_charts_view.py
@@ -1,9 +1,37 @@
-import unittest
+from django.core.urlresolvers import reverse
+from onadata.apps.main.tests.test_base import TestBase
+from rest_framework.test import APIClient
+from onadata.apps.api.views.chart_views import ChartDetail
+from rest_framework.test import APIRequestFactory
 
 
-class TestChartsView(unittest.TestCase):
-    def test_allows_user_with_permissions_on_xform(self):
-        pass
+class TestChartsView(TestBase):
+    def setUp(self):
+        super(self.__class__, self).setUp()
+        self.api_client = APIClient()
+        self.api_client.login(username=self.login_username, password=self.login_password)
+        self._publish_transportation_form()
+        self.view = ChartDetail.as_view()
 
-    def test_rejects_authenticated_user_without_permissions(self):
+    def test_get_on_categorized_field(self):
+        url = '/api/v1/charts/{}/frequency_to_referral_facility'.format(self.xform.id)
+        response = self.api_client.get(url)
+        #factory = APIRequestFactory()
+        #request = factory.get(url)
+        import ipdb; ipdb.set_trace()
+        #response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        response_data = response.raw_post_data
+        self.assertEqual(response_data['field_type'], 'select one')
+        self.assertEqual(response_data['field_name'], 'frequency_to_referral_facility')
+        self.assertEqual(response_data['data_type'], 'categorized')
+
+    def test_get_on_date_field(self):
+        factory = APIRequestFactory()
+        request = factory.get('/charts')
+        import ipdb; ipdb.set_trace()
+        response = self.view(request, formid=self.xform.id, field_name='frequency_to_referral_facility')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_on_numeric_field(self):
         pass
