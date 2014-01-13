2f8825c24ecc068545a77efea6da4fb10d0a8b54
Larry Weya <larryweya@gmail.com>
2014-01-13 15:50:30 +0300

    Moved chart data logic to a utils module

--- onadata/apps/api/views/chart_views.py
+++ onadata/apps/api/views/chart_views.py
@@ -7,7 +7,8 @@ from rest_framework import authentication, permissions
 from rest_framework.renderers import TemplateHTMLRenderer, BrowsableAPIRenderer
 from onadata.libs.utils import common_tags
 from onadata.apps.odk_logger.models import XForm
-from onadata.apps.api.tools import get_xform, get_form_submissions_grouped_by_field
+from onadata.apps.api.tools import get_xform
+from onadata.libs.utils.chart_tools import build_chart_data_for_field
 
 
 class ChartDetail(APIView):
@@ -20,47 +21,23 @@ class ChartDetail(APIView):
         # TODO: seems like model is taking care of object-level perms, should we just rely on that
         xform = get_xform(formid, request)
 
-        # This will not be initialized for submission time
-        field = None
-
         # check if its the special _submission_time META
         if field_name == common_tags.SUBMISSION_TIME:
-            field_type = 'datetime'
+            field = common_tags.SUBMISSION_TIME
         else:
             # use specified field to get summary
             dd = xform.data_dictionary()
-            fields = filter(lambda f: f.name == field_name, [e for e in dd.survey_elements])
+            fields = filter(
+                lambda f: f.name == field_name, [e for e in dd.survey_elements])
 
             if len(fields) == 0:
-                raise Http404("Field {} doesnt not exist on the form".format(field_name))
+                raise Http404(
+                    "Field {} doesnt not exist on the form".format(field_name))
 
             field = fields[0]
-            # TODO: merge choices with results and set 0's on any missing fields, i.e. they didn't have responses
 
-            field_type = field.type
-
-        result = get_form_submissions_grouped_by_field(xform, field_name)
-
-        if field_type == 'select one':
-            # if the field is a select, get a summary of the choices
-            choices = [c for c in field.get('children')]
-
-        # numeric, categorized
-        data_type_map = {
-            'integer': 'numeric',
-            'decimal': 'numeric',
-            'datetime': 'time',
-            'date': 'time_based',
-            'start': 'time_based',
-            'end': 'time_based'
-        }
-        data_type = data_type_map.get(field_type, 'categorized')
-
-        data = {
-            'xform': xform,
-            'field_name': field_name,
-            'field_type': field_type,
-            'data_type': data_type,
-            'data': result
-        }
+        data = build_chart_data_for_field(xform, field)
+        data.update({
+            'xform': xform
+        })
         return Response(data, template_name='chart_detail.html')
---
+++ onadata/libs/utils/chart_tools.py
@@ -0,0 +1,56 @@
+from onadata.apps.api.tools import get_form_submissions_grouped_by_field
+from onadata.libs.utils import common_tags
+
+
+# list of fields we can chart
+CHART_FIELDS = ['select one', 'integer', 'decimal', 'date', 'datetime', 'start',
+                'end']
+# numeric, categorized
+DATA_TYPE_MAP = {
+    'integer': 'numeric',
+    'decimal': 'numeric',
+    'datetime': 'time',
+    'date': 'time_based',
+    'start': 'time_based',
+    'end': 'time_based'
+}
+
+
+def build_chart_data_for_field(xform, field):
+    # check if its the special _submission_time META
+    if isinstance(field, basestring) and field == common_tags.SUBMISSION_TIME:
+        field_type = 'datetime'
+        field_name = common_tags.SUBMISSION_TIME
+    else:
+        # TODO: merge choices with results and set 0's on any missing fields, i.e. they didn't have responses
+        field_type = field.type
+        field_name = field.name
+
+    if field_type == 'select one':
+        # TODO: if the field is a select, get a summary of the choices
+        choices = [c for c in field.get('children')]
+
+    result = get_form_submissions_grouped_by_field(xform, field_name)
+    data_type = DATA_TYPE_MAP.get(field_type, 'categorized')
+    data = {
+        'field_name': field_name,
+        'field_type': field_type,
+        'data_type': data_type,
+        'data': result
+    }
+    return data
+
+
+def build_chart_data(xform):
+    # use specified field to get summary
+    dd = xform.data_dictionary()
+    fields = filter(
+        lambda f: f.type in CHART_FIELDS, [e for e in dd.survey_elements])
+
+    data = []
+
+    for field in fields:
+        d = build_chart_data_for_field(xform, field.name)
+        data.append(d)
+
+    return data
