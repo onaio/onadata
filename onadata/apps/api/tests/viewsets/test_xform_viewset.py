58e3d58fb274b332f32ff40b4fb6710673baf0b1
Ukang'a Dickson <ukanga@gmail.com>
2014-01-16 15:33:49 +0300

    UK: move data export to XFormViewset

--- onadata/apps/api/tests/viewsets/test_xform_viewset.py
+++ onadata/apps/api/tests/viewsets/test_xform_viewset.py
@@ -1,13 +1,16 @@
 import os
 import requests
 
+from django.conf import settings
+from django.test import RequestFactory
+
 from httmock import urlmatch, HTTMock
 from xml.dom import minidom, Node
-from django.conf import settings
 
 from onadata.apps.api.tests.viewsets.test_abstract_viewset import \
     TestAbstractViewSet
 from onadata.apps.api.viewsets.xform_viewset import XFormViewSet
+from onadata.apps.main.tests.test_base import TestBase
 
 
 @urlmatch(netloc=r'(.*\.)?enketo\.formhub\.org$')
@@ -201,3 +204,49 @@ class TestXFormViewSet(TestAbstractViewSet):
                 'http://testserver/api/v1/forms/bob/%s' % xform.pk
             })
             self.assertDictContainsSubset(data, response.data)
+
+
+class TestExportViewSet(TestBase):
+
+    def setUp(self):
+        super(self.__class__, self).setUp()
+        self._create_user_and_login()
+        self._publish_transportation_form()
+        self.factory = RequestFactory()
+        self.extra = {
+            'HTTP_AUTHORIZATION': 'Token %s' % self.user.auth_token}
+
+    def _filename_from_disposition(self, content_disposition):
+        filename_pos = content_disposition.index('filename=')
+        self.assertTrue(filename_pos != -1)
+        return content_disposition[filename_pos + len('filename='):]
+
+    def test_form_data_export(self):
+        self._make_submissions()
+        view = XFormViewSet.as_view({
+            'get': 'retrieve'
+        })
+        formid = self.xform.pk
+        # csv
+        request = self.factory.get('/', **self.extra)
+        response = view(request, owner='bob', pk=formid, format='csv')
+        self.assertEqual(response.status_code, 200)
+        headers = dict(response.items())
+        content_disposition = headers['Content-Disposition']
+        filename = self._filename_from_disposition(content_disposition)
+        basename, ext = os.path.splitext(filename)
+        self.assertEqual(headers['Content-Type'], 'application/csv')
+        self.assertEqual(ext, '.csv')
+
+        # xls
+        request = self.factory.get('/', **self.extra)
+        response = view(request, owner='bob', pk=self.xform.id_string,
+                        format='xls')
+        self.assertEqual(response.status_code, 200)
+        headers = dict(response.items())
+        content_disposition = headers['Content-Disposition']
+        filename = self._filename_from_disposition(content_disposition)
+        basename, ext = os.path.splitext(filename)
+        self.assertEqual(headers['Content-Type'],
+                         'application/vnd.openxmlformats')
+        self.assertEqual(ext, '.xlsx')
