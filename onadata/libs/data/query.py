10e688504518d68560415b4d7c3d8bd26a254a81
Ukang'a Dickson <ukanga@gmail.com>
2014-01-20 17:09:44 +0300

    UK: using count(*) ensures null columns are counted

--- onadata/libs/data/query.py
+++ onadata/libs/data/query.py
@@ -54,7 +54,7 @@ def _postgres_count_group(field, name, xform):
         string_args['json'] = "to_char(to_date(%(json)s, 'YYYY-MM-DD'), 'YYYY"\
                               "-MM-DD')" % string_args
 
-    return "SELECT %(json)s AS %(name)s, COUNT(%(json)s) AS count FROM "\
+    return "SELECT %(json)s AS %(name)s, COUNT(*) AS count FROM "\
            "%(table)s WHERE %(restrict_field)s=%(restrict_value)s "\
            "GROUP BY %(json)s" % string_args
 
