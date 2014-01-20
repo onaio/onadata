c8c98fae07f2f1c0338db4270f08377ebb0121d9
Larry Weya <larryweya@gmail.com>
2014-01-20 14:13:15 +0300

    raise ValueError only if there is a single result whose value is None

--- onadata/libs/data/query.py
+++ onadata/libs/data/query.py
@@ -106,8 +106,12 @@ def get_form_submissions_grouped_by_field(xform, field, name=None):
 
     result = _execute_query(_count_group(field, name, xform))
 
-    if len(result) and result[0][name] is None:
+    # if we have a single None result, the field doesnt exist
+    if len(result) == 1 and result[0][name] is None:
         raise ValueError(_(u"Field '%s' does not exist." % field))
+    #elif len(result) > 0 and result[0][name] is None:
+    #    # strip out the first result if it has a count of 0 and value of None
+    #    result = result[1:]
 
     return result
 
