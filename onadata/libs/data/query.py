13216a779512366a48fca8dcdd70486df0d07050
Larry Weya <larryweya@gmail.com>
2014-01-21 15:18:00 +0300

    Log result to catch staging error

--- onadata/libs/data/query.py
+++ onadata/libs/data/query.py
@@ -109,7 +109,8 @@ def get_form_submissions_grouped_by_field(xform, field, name=None):
 
     # if we have a single None result, the field doesnt exist
     if len(result) == 1 and result[0][name] is None:
-        raise ValueError(_(u"Field '%s' does not exist." % field))
+        raise ValueError(_(
+            u"Field '{}' does not exist in result {}".format(field, result)))
     #elif len(result) > 0 and result[0][name] is None:
     #    # strip out the first result if it has a count of 0 and value of None
     #    result = result[1:]
