851e0b390e43d623d26c7af7aa7e306dbeec87ed
Ukang'a Dickson <ukanga@gmail.com>
2014-01-27 12:15:02 +0300

    UK: move imports on top of file

--- onadata/apps/odk_logger/management/__init__.py
+++ onadata/apps/odk_logger/management/__init__.py
@@ -1,4 +1,7 @@
 from south.signals import post_migrate
+from django.conf import settings
+from django.db.models import get_app, get_models
+from django.contrib.auth.management import create_permissions
 
 
 # Courtesy of http://devwithpassion.com/felipe/south-django-permissions/
@@ -6,9 +9,6 @@ def update_permissions_after_migration(app, **kwargs):
     """
     Update app permission just after every migration.
     """
-    from django.conf import settings
-    from django.db.models import get_app, get_models
-    from django.contrib.auth.management import create_permissions
 
     create_permissions(
         get_app(app), get_models(), 2 if settings.DEBUG else 0)
