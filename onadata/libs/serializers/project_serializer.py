108e8d311d1a46d89599ff42168de2da80126ef5
pld <peter@helioid.com>
2014-03-26 09:57:09 -0400

    PLD: move serializers to individual class files in lib

---
+++ onadata/libs/serializers/project_serializer.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from onadata.apps.api.fields import HyperlinkedMultiIdentityField
+from onadata.apps.api.models import Project
+
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    url = HyperlinkedMultiIdentityField(
+        view_name='project-detail',
+        lookup_fields=(('pk', 'pk'), ('owner', 'organization')))
+    owner = serializers.HyperlinkedRelatedField(
+        view_name='user-detail',
+        source='organization', lookup_field='username')
+    created_by = serializers.HyperlinkedRelatedField(
+        view_name='user-detail', lookup_field='username', read_only=True)
+
+    class Meta:
+        model = Project
+        exclude = ('organization', 'created_by')
+
+    def restore_object(self, attrs, instance=None):
+        if instance:
+            return super(ProjectSerializer, self)\
+                .restore_object(attrs, instance)
+        if 'request' in self.context:
+            created_by = self.context['request'].user
+            return Project(
+                name=attrs.get('name'),
+                organization=attrs.get('organization'),
+                created_by=created_by,)
+        return attrs
---
+++ onadata/libs/serializers/team_serializer.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from onadata.apps.api.fields import HyperlinkedMultiIdentityField,\
+    HyperlinkedMultiRelatedField
+from onadata.apps.api.models import Project, OrganizationProfile, Team
+
+
+class TeamSerializer(serializers.Serializer):
+    url = HyperlinkedMultiIdentityField(
+        view_name='team-detail',
+        lookup_fields=(('pk', 'pk'), ('owner', 'organization')))
+    name = serializers.CharField(max_length=100, source='team_name')
+    organization = serializers.HyperlinkedRelatedField(
+        view_name='user-detail', lookup_field='username',
+        source='organization',
+        queryset=User.objects.filter(
+            pk__in=OrganizationProfile.objects.values('user')))
+    projects = HyperlinkedMultiRelatedField(
+        view_name='project-detail', source='projects', many=True,
+        queryset=Project.objects.all(), read_only=True,
+        lookup_fields=(('pk', 'pk'), ('owner', 'organization')))
+
+    def restore_object(self, attrs, instance=None):
+        org = attrs.get('organization', None)
+        projects = attrs.get('projects', [])
+        if instance:
+            instance.organization = org if org else instance.organization
+            instance.name = attrs.get('team_name', instance.name)
+            instance.projects.clear()
+            for project in projects:
+                instance.projects.add(project)
+            return instance
+        team_name = attrs.get('team_name', None)
+        if not team_name:
+            self.errors['name'] = u'A team name is required'
+            return attrs
+        return Team(organization=org, name=team_name)
