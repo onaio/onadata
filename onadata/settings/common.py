37f8d02ce9a0d7451f63822ef128b058676672bb
pld <peter@helioid.com>
2014-05-15 14:27:35 -0400

    PLD: add digest auth, closes #286

--- onadata/settings/common.py
+++ onadata/settings/common.py
@@ -218,8 +218,9 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework_digestauth.authentication.DigestAuthentication',
+        'rest_framework.permissions.DjangoModelPermissions',
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-        'rest_framework.permissions.DjangoModelPermissions'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
