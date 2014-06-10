74494f2f6728ac6e8897daf6645b49c818a1b23b
Ukang'a Dickson <ukanga@gmail.com>
2014-06-10 14:52:02 +0300

    UK: add xform permissions, for manager role

--- onadata/libs/permissions.py
+++ onadata/libs/permissions.py
@@ -1,12 +1,17 @@
 from guardian.shortcuts import assign_perm
 from onadata.apps.api.models import OrganizationProfile
 from onadata.apps.main.models.user_profile import UserProfile
+from onadata.apps.logger.models import XForm
 
 CAN_ADD_XFORM = 'can_add_xform'
+CAN_CHANGE_XFORM = 'change_xform'
 
 
 class ManagerRole(object):
-    permissions = ((CAN_ADD_XFORM, (UserProfile, OrganizationProfile)), )
+    permissions = (
+        (CAN_ADD_XFORM, (UserProfile, OrganizationProfile)),
+        (CAN_CHANGE_XFORM, XForm)
+    )
 
     @classmethod
     def add(cls, user, obj):
