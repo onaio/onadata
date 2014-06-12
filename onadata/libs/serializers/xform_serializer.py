1e9f15d162680932e4449e63678e4050edcae9e2
Ukang'a Dickson <ukanga@gmail.com>
2014-06-12 11:30:56 +0300

    UK: custom BooleanField class for XFormSerializer, to allow only true and false values

--- onadata/libs/serializers/xform_serializer.py
+++ onadata/libs/serializers/xform_serializer.py
@@ -7,6 +7,17 @@ from onadata.apps.logger.models import XForm
 from onadata.libs.serializers.tag_list_serializer import TagListSerializer
 
 
+class BooleanField(serializers.BooleanField):
+    def from_native(self, value):
+        if value in ('true', 't', 'True', '1'):
+            return True
+
+        if value in ('false', 'f', 'False', '0'):
+            return False
+
+        return value
+
+
 class XFormSerializer(serializers.HyperlinkedModelSerializer):
     url = HyperlinkedMultiIdentityField(
         view_name='xform-detail',
@@ -15,9 +26,9 @@ class XFormSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.HyperlinkedRelatedField(
         view_name='user-detail',
         source='user', lookup_field='username')
-    public = serializers.BooleanField(
+    public = BooleanField(
         source='shared', widget=widgets.CheckboxInput())
-    public_data = serializers.BooleanField(
+    public_data = BooleanField(
         source='shared_data')
     tags = TagListSerializer(read_only=True)
 
