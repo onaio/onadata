01ee3577b26967f950533bb88c5f0346a98df959
Ukang'a Dickson <ukanga@gmail.com>
2014-06-13 23:08:24 +0300

    UK: remove all permissions for user on object, and set role specifc permissions

--- onadata/libs/permissions.py
+++ onadata/libs/permissions.py
@@ -1,12 +1,13 @@
-from guardian.shortcuts import assign_perm
+from django.contrib.contenttypes.models import ContentType
+from guardian.shortcuts import assign_perm, remove_perm
 from onadata.apps.api.models import OrganizationProfile
 from onadata.apps.main.models.user_profile import UserProfile
 from onadata.apps.logger.models import XForm
 
 CAN_ADD_XFORM_TO_PROFILE = 'can_add_xform'
-CAN_CHANGE_XFORM = 'logger.change_xform'
-CAN_ADD_XFORM = 'logger.add_xform'
-CAN_DELETE_XFORM = 'logger.delete_xform'
+CAN_CHANGE_XFORM = 'change_xform'
+CAN_ADD_XFORM = 'add_xform'
+CAN_DELETE_XFORM = 'delete_xform'
 CAN_VIEW_XFORM = 'view_xform'
 CAN_ADD_SUBMISSIONS = 'report_xform'
 CAN_TRANSFER_OWNERSHIP = 'transfer_xform'
@@ -18,10 +19,24 @@ class Role(object):
     name = None
 
     @classmethod
+    def _remove_obj_permissions(self, user, obj):
+        content_type = ContentType.objects.get(
+            model=obj.__class__.__name__.lower(),
+            app_label=obj.__class__._meta.app_label
+        )
+        object_permissions = user.userobjectpermission_set.filter(
+            object_pk=obj.pk, content_type=content_type)
+
+        for perm in object_permissions:
+            remove_perm(perm.permission.codename, user, obj)
+
+    @classmethod
     def add(cls, user, obj):
-        for permission, klass in cls.permissions:
+        cls._remove_obj_permissions(user, obj)
+
+        for codename, klass in cls.permissions:
             if isinstance(obj, klass):
-                assign_perm(permission, user, obj)
+                assign_perm(codename, user, obj)
 
     @classmethod
     def has_role(cls, user, obj):
