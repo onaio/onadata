764c41d9379036df57529c0ccb531f99bec4327b
royrutto <royrutto@gmail.com>
2014-06-30 16:01:40 +0300

    RR: remove basic auth from common.py

--- onadata/settings/common.py
+++ onadata/settings/common.py
@@ -222,7 +222,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
