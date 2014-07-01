9057e0b1c3e1c6b369e7a6a67706ebf5fa6a2beb
royrutto <royrutto@gmail.com>
2014-07-01 11:46:50 +0300

    RR: add user endpoint doc

--- onadata/apps/api/viewsets/connect_viewset.py
+++ onadata/apps/api/viewsets/connect_viewset.py
@@ -10,6 +10,31 @@ from onadata.apps.main.models.user_profile import UserProfile
 
 class ConnectViewSet (viewsets.GenericViewSet):
 
+    """This endpoint allows you retrieve the authenticated user's profile info.
+
+## Retrieve profile
+> Example
+>
+>       curl -X GET https://ona.io/api/v1/user
+
+> Response:
+
+>       {
+            "api_token": "76121138a080c5ae94f318a8b9be91e7ebebb484",
+            "city": "Nairobi",
+            "country": "Kenya",
+            "gravatar": "avatar.png",
+            "name": "Demo User",
+            "organization": "",
+            "require_auth": false,
+            "twitter": "",
+            "url": "http://localhost:8000/api/v1/profiles/demo",
+            "user": "http://localhost:8000/api/v1/users/demo",
+            "username": "demo",
+            "website": ""
+}
+
+"""
     model = UserProfile
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = UserProfileWithTokenSerializer
