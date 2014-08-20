2966a1f299dcf4dbeb597036cf3a651a2951c27a
Ukang'a Dickson <ukanga@gmail.com>
2014-08-21 00:10:16 +0300

    UK: add object permission filter to XFormList endpoint

--- onadata/apps/api/tests/viewsets/test_xform_list_api.py
+++ onadata/apps/api/tests/viewsets/test_xform_list_api.py
@@ -1,13 +1,15 @@
 import os
 
+from django.test import TransactionTestCase
 from django_digest.test import DigestAuth
 
 from onadata.apps.api.tests.viewsets.test_abstract_viewset import\
     TestAbstractViewSet
 from onadata.apps.api.viewsets.xform_list_api import XFormListApi
+from onadata.libs.permissions import ReadOnlyRole
 
 
-class TestXFormListApi(TestAbstractViewSet):
+class TestXFormListApi(TestAbstractViewSet, TransactionTestCase):
     def setUp(self):
         super(self.__class__, self).setUp()
         self.view = XFormListApi.as_view({
@@ -29,5 +31,53 @@ class TestXFormListApi(TestAbstractViewSet):
 
         with open(path) as f:
             form_list_xml = f.read().strip()
-            content = response.render().content.replace(self.xform.hash, '')
-            self.assertEqual(content, form_list_xml)
+            data = {"hash": self.xform.hash, "pk": self.xform.pk}
+            content = response.render().content
+            self.assertEqual(content, form_list_xml % data)
+
+    def test_get_xform_list_other_user_with_no_role(self):
+        request = self.factory.get('/')
+        response = self.view(request)
+        alice_data = {'username': 'alice', 'email': 'alice@localhost.com'}
+        alice_profile = self._create_user_profile(alice_data)
+
+        self.assertFalse(
+            ReadOnlyRole.user_has_role(alice_profile.user, self.xform)
+        )
+
+        auth = DigestAuth('alice', 'bobbob')
+        request.META.update(auth(request.META, response))
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+        content = response.render().content
+        self.assertNotIn(self.xform.id_string, content)
+        self.assertEqual(
+            content, '<?xml version="1.0" encoding="utf-8"?>\n<xforms '
+            'xmlns="http://openrosa.org/xforms/xformsList"></xforms>')
+
+    def test_get_xform_list_other_user_with_readonly_role(self):
+        request = self.factory.get('/')
+        response = self.view(request)
+        alice_data = {'username': 'alice', 'email': 'alice@localhost.com'}
+        alice_profile = self._create_user_profile(alice_data)
+
+        ReadOnlyRole.add(alice_profile.user, self.xform)
+
+        self.assertTrue(
+            ReadOnlyRole.user_has_role(alice_profile.user, self.xform)
+        )
+
+        auth = DigestAuth('alice', 'bobbob')
+        request.META.update(auth(request.META, response))
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200)
+
+        path = os.path.join(
+            os.path.dirname(__file__),
+            '..', 'fixtures', 'formList.xml')
+
+        with open(path) as f:
+            form_list_xml = f.read().strip()
+            data = {"hash": self.xform.hash, "pk": self.xform.pk}
+            content = response.render().content
+            self.assertEqual(content, form_list_xml % data)
--- onadata/apps/api/viewsets/xform_list_api.py
+++ onadata/apps/api/viewsets/xform_list_api.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 
 from onadata.apps.logger.models.xform import XForm
+from onadata.libs import filters
 from onadata.libs.authentication import DigestAuthentication
 from onadata.libs.renderers.renderers import XFormListRenderer
 from onadata.libs.serializers.xform_serializer import XFormListSerializer
@@ -9,8 +10,9 @@ from onadata.libs.serializers.xform_serializer import XFormListSerializer
 
 class XFormListApi(viewsets.ReadOnlyModelViewSet):
     authentication_classes = (DigestAuthentication,)
-    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (filters.AnonDjangoObjectPermissionFilter,)
     model = XForm
+    permission_classes = (permissions.IsAuthenticated,)
     renderer_classes = (XFormListRenderer,)
     serializer_class = XFormListSerializer
     template_name = 'api/xformsList.xml'
