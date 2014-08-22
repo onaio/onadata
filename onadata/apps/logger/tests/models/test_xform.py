d466ebc6276034a6814db15c55105c71ab1cd307
Ukang'a Dickson <ukanga@gmail.com>
2014-08-22 10:58:59 +0300

    UK: test UnicodeDecodeError is raised when setting title in xform xml

--- onadata/apps/logger/tests/models/test_xform.py
+++ onadata/apps/logger/tests/models/test_xform.py
@@ -1,3 +1,5 @@
+import os
+
 from onadata.apps.main.tests.test_base import TestBase
 from onadata.apps.logger.models import XForm, Instance
 
@@ -18,3 +20,23 @@ class TestXForm(TestBase):
         # the in-memory xform object as one
         self.xform = XForm.objects.get(pk=self.xform.id)
         self.assertEqual(self.xform.submission_count(), 0)
+
+    def test_set_title_in_xml_unicode_error(self):
+        xls_file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "../..",  "fixtures", "tutorial", "tutorial_arabic_labels.xls"
+        )
+        self._publish_xls_file_and_set_xform(xls_file_path)
+
+        self.assertTrue(isinstance(self.xform.xml, unicode))
+
+        # convert xml to str
+        self.xform.xml = self.xform.xml.encode('utf-8')
+        self.assertTrue(isinstance(self.xform.xml, str))
+
+        # change title
+        self.xform.title = u'Random Title'
+
+        with self.assertRaises(UnicodeDecodeError):
+            # set title in xform xml
+            self.xform._set_title()
