bcfc964f6f63aecdfdc4995a570dba4fbf7c0ea2
Ukang'a Dickson <ukanga@gmail.com>
2014-09-02 22:07:50 +0300

    UK: include DigestAuthentication in settings - common.py

--- onadata/settings/common.py
+++ onadata/settings/common.py
@@ -221,6 +221,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'onadata.libs.authentication.DigestAuthentication',
         'oauth2_provider.ext.rest_framework.OAuth2Authentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
