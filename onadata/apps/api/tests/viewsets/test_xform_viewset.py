012d9f2c6fed7921562b53dc16b3a029391a76e4
ekisa <mark.ekisa@gmail.com>
2014-09-12 17:16:19 +0300

    ME: reverted test case to initial state

--- onadata/apps/api/tests/viewsets/test_xform_viewset.py
+++ onadata/apps/api/tests/viewsets/test_xform_viewset.py
@@ -332,7 +332,7 @@ class TestXFormViewSet(TestAbstractViewSet):
             'allows_sms': False,
             'encrypted': False,
             'sms_id_string': u'transportation_2011_07_25',
-            'id_string': u'TRANSPORTATION_2011_07_25',
+            'id_string': u'transportation_2011_07_25',
             'title': u'transportation_2011_07_25',
             'bamboo_dataset': u''
         }
@@ -349,8 +349,6 @@ class TestXFormViewSet(TestAbstractViewSet):
                 'url':
                 'http://testserver/api/v1/forms/%s' % xform.pk
             })
-            # xform id string is converted to lower case before saving
-            data['id_string'] = data['id_string'].lower()
             self.assertDictContainsSubset(data, response.data)
             self.assertTrue(OwnerRole.user_has_role(self.user, xform))
 
