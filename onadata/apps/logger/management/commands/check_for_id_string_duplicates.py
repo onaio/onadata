a04c0142d203b275047384e37794ae70ce64bda2
ekisa <mark.ekisa@gmail.com>
2014-09-15 18:28:02 +0300

    ME: checking for duplicate id_strings using management command

--- onadata/apps/logger/management/commands/check_for_id_string_duplicates.py
+++ onadata/apps/logger/management/commands/check_for_id_string_duplicates.py
@@ -1,43 +1,40 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 fileencoding=utf-8
 
-from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.utils.translation import ugettext_lazy
 from onadata.apps.logger.models.xform import XForm
 from onadata.libs.utils.model_tools import queryset_iterator
+from pprint import pprint
 
 
 class Command(BaseCommand):
-    help = ugettext_lazy("Check for duplicate id_strings in XForms per user")
+    help = ugettext_lazy("Check for duplicate id_strings")
 
     def handle(self, *args, **kwargs):
-        for user in User.objects.all():
-            xforms = XForm.objects.filter(user=user)
-            dict1 = {a.id: a.id_string for a in xforms}
-            list1 = set(map(lambda x: x.lower(), dict1.values()))
+        xforms = XForm.objects.all()
+        list1 = [a.id_string for a in xforms]
+        list2 = set(map(lambda x: x.lower(), list1))
 
-            if len(list1) == len(dict1.values()):
-                continue
-            else:
-                print ">>>>>>>>>>>>>>>>>> DUPLICATES EXIST"
-                flipped = {}
-                for key, value in dict1.items():
-                    if value not in flipped:
-                        flipped[value] = [key]
-                    else:
-                        flipped[value].append(key)
+        if len(list1) == len(list2):
+            print "************** NO id_string DUPLICATES FOUND **************"
+            continue
+        else:
+            counter = {}
+            for xform in queryset_iterator(xforms):
+                id_string = xform.id_string
+                xform_id = xform.id
+                if id_string not in counter:
+                    counter[id_string] = [xform_id]
+                else:
+                    counter[id_string].append(xform_id)
 
-                for duplicate_id_string, ids in flipped.items():
-                    if len(ids) > 1:
-                        from_second = ids[1:]
-                        for xform_id in from_second:
-                            for xform in queryset_iterator(xforms):
-                                try:
-                                    if xform.id == xform_id:
-                                        xform.id_string = "%s_%s" % (
-                                            duplicate_id_string,
-                                            from_second.index(xform_id) + 1)
-                                        xform.save()
-                                except Exception as e:
-                                    print e
+            duplicate_id_strings = [id_string
+                                    for id_string, xform_ids in counter.items()
+                                    if len(xform_ids) > 1]
+
+            duplicates_rows = [(xform.id, xform.user, xform.id_string)
+                               for xform in queryset_iterator(xforms)
+                               if xform.id_string in duplicate_id_strings]
+
+            pprint(duplicates_rows)
