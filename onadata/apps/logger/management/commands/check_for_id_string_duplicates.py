fc17ebca90bd79a0c166e78e41de4b3a1e9ab4a9
ekisa <mark.ekisa@gmail.com>
2014-09-15 21:42:00 +0300

    ME: refactored 'check_for_id_string_duplicates' mngmt cmd

--- onadata/apps/logger/management/commands/check_for_id_string_duplicates.py
+++ onadata/apps/logger/management/commands/check_for_id_string_duplicates.py
@@ -4,7 +4,6 @@
 from django.core.management.base import BaseCommand
 from django.utils.translation import ugettext_lazy
 from onadata.apps.logger.models.xform import XForm
-from onadata.libs.utils.model_tools import queryset_iterator
 from pprint import pprint
 
 
@@ -13,28 +12,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         xforms = XForm.objects.all()
-        list1 = [a.id_string for a in xforms]
+        list1 = [xform.id_string for xform in xforms]
         list2 = set(map(lambda x: x.lower(), list1))
 
-        if len(list1) == len(list2):
-            print "************** NO id_string DUPLICATES FOUND **************"
-            continue
-        else:
+        if len(list1) != len(list2):
             counter = {}
-            for xform in queryset_iterator(xforms):
-                id_string = xform.id_string
-                xform_id = xform.id
-                if id_string not in counter:
-                    counter[id_string] = [xform_id]
-                else:
-                    counter[id_string].append(xform_id)
+            xform_list = [(xform.id, xform.id_string) for xform in xforms]
+            for xform_id, xform_id_string in xform_list:
+                counter.setdefault(xform_id_string, []).append(xform_id)
 
             duplicate_id_strings = [id_string
                                     for id_string, xform_ids in counter.items()
                                     if len(xform_ids) > 1]
 
             duplicates_rows = [(xform.id, xform.user, xform.id_string)
-                               for xform in queryset_iterator(xforms)
+                               for xform in xforms
                                if xform.id_string in duplicate_id_strings]
 
             pprint(duplicates_rows)
