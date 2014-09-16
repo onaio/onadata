74bd90e20e6b38c08edb697450ac08565ce26e55
Dennis Wambua <dennis.wambua@gmail.com>
2014-09-16 10:49:37 +0300

    DW: Used all() function to check for permissions and used an ordered list when comparing

--- onadata/libs/permissions.py
+++ onadata/libs/permissions.py
@@ -26,9 +26,9 @@ CAN_CHANGE_PROJECT = 'change_project'
 CAN_TRANSFER_PROJECT_OWNERSHIP = 'transfer_project'
 CAN_DELETE_PROJECT = 'delete_project'
 
-CAN_ADD_DATA_DIC = 'add_datadictionary'
-CAN_CHANGE_DATA_DIC = 'change_datadictionary'
-CAN_DELETE_DATA_DIC = 'delete_datadictionary'
+CAN_ADD_DATADICTIONARY = 'add_datadictionary'
+CAN_CHANGE_DATADICTIONARY = 'change_datadictionary'
+CAN_DELETE_DATADICTIONARY = 'delete_datadictionary'
 
 
 class Role(object):
@@ -63,8 +63,8 @@ class Role(object):
         :param permissions: A list of permissions.
         :param obj: An object to get the permissions of.
         """
-        return set(permissions)\
-            .issubset(set(cls.class_to_permissions[type(obj)]))
+        return all([l in set(permissions)
+                    for l in set(cls.class_to_permissions[type(obj)])])
 
     @classmethod
     def user_has_role(cls, user, obj):
@@ -151,9 +151,9 @@ class OwnerRole(Role):
         (CAN_VIEW_PROFILE, UserProfile),
         (CAN_VIEW_PROJECT, Project),
         (CAN_VIEW_XFORM, XForm),
-        (CAN_ADD_DATA_DIC, XForm),
-        (CAN_CHANGE_DATA_DIC, XForm),
-        (CAN_DELETE_DATA_DIC, XForm),
+        (CAN_ADD_DATADICTIONARY, XForm),
+        (CAN_CHANGE_DATADICTIONARY, XForm),
+        (CAN_DELETE_DATADICTIONARY, XForm),
         (CAN_ADD_SUBMISSIONS, XForm),
     )
 
@@ -162,6 +162,11 @@ ROLES = {role.name: role for role in [ReadOnlyRole,
                                       EditorRole,
                                       ManagerRole,
                                       OwnerRole]}
+ROLES_ORDERED = [ReadOnlyRole,
+                 DataEntryRole,
+                 EditorRole,
+                 ManagerRole,
+                 OwnerRole]
 
 # Memoize a class to permissions dict.
 for role in ROLES.values():
@@ -170,7 +175,7 @@ for role in ROLES.values():
 
 
 def get_role(permissions, obj):
-    for role in ROLES.values():
+    for role in reversed(ROLES_ORDERED):
         if role.has_role(permissions, obj):
             return role.name
 
