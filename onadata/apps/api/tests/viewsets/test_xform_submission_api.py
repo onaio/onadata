479455b97ecb2103675e16b4d196b14efa4b60c3
Ukang'a Dickson <ukanga@gmail.com>
2014-10-02 15:40:43 +0300

    UK: test for /submission endpoint for authenticated users

--- onadata/apps/api/tests/viewsets/test_xform_submission_api.py
+++ onadata/apps/api/tests/viewsets/test_xform_submission_api.py
@@ -106,6 +106,63 @@ class TestXFormSubmissionApi(TestAbstractViewSet, TransactionTestCase):
                 self.assertEqual(response['Location'],
                                  'http://testserver/submission')
 
+    def test_post_submission_uuid_authenticated_username_not_provided(self):
+        s = self.surveys[0]
+        media_file = "1335783522563.jpg"
+        path = os.path.join(self.main_directory, 'fixtures',
+                            'transportation', 'instances', s, media_file)
+        with open(path) as f:
+            f = InMemoryUploadedFile(f, 'media_file', media_file, 'image/jpg',
+                                     os.path.getsize(path), None)
+            path = os.path.join(
+                self.main_directory, 'fixtures',
+                'transportation', 'instances', s, s + '.xml')
+            path = self._add_uuid_to_submission_xml(path, self.xform)
+
+            with open(path) as sf:
+                data = {'xml_submission_file': sf, 'media_file': f}
+                request = self.factory.post('/submission', data)
+                response = self.view(request)
+                self.assertEqual(response.status_code, 401)
+                auth = DigestAuth('bob', 'bobbob')
+                request.META.update(auth(request.META, response))
+                response = self.view(request)
+                self.assertContains(response, 'Successful submission',
+                                    status_code=201)
+                self.assertTrue(response.has_header('X-OpenRosa-Version'))
+                self.assertTrue(
+                    response.has_header('X-OpenRosa-Accept-Content-Length'))
+                self.assertTrue(response.has_header('Date'))
+                self.assertEqual(response['Content-Type'],
+                                 'text/xml; charset=utf-8')
+                self.assertEqual(response['Location'],
+                                 'http://testserver/submission')
+
+    def test_post_submission_uuid_other_user_username_not_provided(self):
+        alice_data = {'username': 'alice', 'email': 'alice@localhost.com'}
+        self._create_user_profile(alice_data)
+        s = self.surveys[0]
+        media_file = "1335783522563.jpg"
+        path = os.path.join(self.main_directory, 'fixtures',
+                            'transportation', 'instances', s, media_file)
+        with open(path) as f:
+            f = InMemoryUploadedFile(f, 'media_file', media_file, 'image/jpg',
+                                     os.path.getsize(path), None)
+            path = os.path.join(
+                self.main_directory, 'fixtures',
+                'transportation', 'instances', s, s + '.xml')
+            path = self._add_uuid_to_submission_xml(path, self.xform)
+
+            with open(path) as sf:
+                data = {'xml_submission_file': sf, 'media_file': f}
+                request = self.factory.post('/submission', data)
+                response = self.view(request)
+                self.assertEqual(response.status_code, 401)
+                auth = DigestAuth('alice', 'bobbob')
+                request.META.update(auth(request.META, response))
+                response = self.view(request)
+                self.assertEqual(response.status_code, 403)
+
     def test_post_submission_require_auth(self):
         self.user.profile.require_auth = True
         self.user.profile.save()
