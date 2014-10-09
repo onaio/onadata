f069d498a0674d8e8a60cb4e8b57a627ce3851a1
Ukang'a Dickson <ukanga@gmail.com>
2014-10-09 10:49:43 +0300

    UK: replace custom urls with registration.auth_urls, closes #596

--- onadata/apps/main/registration_urls.py
+++ onadata/apps/main/registration_urls.py
@@ -8,8 +8,7 @@ URLConf to include this URLConf for any URL beginning with
 """
 
 
-from django.conf.urls import patterns, url
-from django.contrib.auth import views as auth_views
+from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 from registration.backends.default.views import ActivationView
 
@@ -29,32 +28,6 @@ urlpatterns = patterns(
     url(r'^activate/(?P<activation_key>\w+)/$',
         ActivationView.as_view(),
         name='registration_activate'),
-    url(r'^login/$',
-        auth_views.login,
-        {'template_name': 'registration/login.html'},
-        name='auth_login'),
-    url(r'^logout/$',
-        auth_views.logout,
-        {'template_name': 'registration/logout.html'},
-        name='auth_logout'),
-    url(r'^password/change/$',
-        auth_views.password_change,
-        name='auth_password_change'),
-    url(r'^password/change/done/$',
-        auth_views.password_change_done,
-        name='auth_password_change_done'),
-    url(r'^password/reset/$',
-        auth_views.password_reset,
-        name='auth_password_reset'),
-    url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        auth_views.password_reset_confirm,
-        name='auth_password_reset_confirm'),
-    url(r'^password/reset/complete/$',
-        auth_views.password_reset_complete,
-        name='auth_password_reset_complete'),
-    url(r'^password/reset/done/$',
-        auth_views.password_reset_done,
-        name='auth_password_reset_done'),
     url(r'^register/$',
         FHRegistrationView.as_view(form_class=RegistrationFormUserProfile),
         name='registration_register'),
@@ -62,4 +35,5 @@ urlpatterns = patterns(
         TemplateView.as_view(
             template_name='registration/registration_complete.html'),
         name='registration_complete'),
+    (r'', include('registration.auth_urls')),
 )
