255c5c49255d4a6224c13b19c8cce196b7a41c7a
Ukang'a Dickson <ukanga@gmail.com>
2014-10-28 17:31:08 +0300

    UK: code refactor, use password reset serializers

--- onadata/apps/api/tests/viewsets/test_reset_password_viewset.py
+++ onadata/apps/api/tests/viewsets/test_reset_password_viewset.py
@@ -2,6 +2,8 @@ from mock import patch
 
 from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 from onadata.apps.api.tests.viewsets.test_abstract_viewset import \
     TestAbstractViewSet
@@ -17,30 +19,30 @@ class TestResetPasswordViewSet(TestAbstractViewSet):
             "post": "create"
         })
 
-    @patch('onadata.apps.api.viewsets.reset_password_viewset.send_mail')
+    @patch('onadata.libs.serializers.password_reset_serializer.send_mail')
     def test_request_reset_password(self, mock_send_mail):
         data = {'email': self.user.email,
                 'reset_url': u'http://testdomain.com/reset_form'}
         request = self.factory.post('/', data=data)
         response = self.view(request)
         self.assertTrue(mock_send_mail.called)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 204)
 
         mock_send_mail.called = False
         request = self.factory.post('/')
         response = self.view(request)
         self.assertFalse(mock_send_mail.called)
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 400)
 
     def test_reset_user_password(self):
         token = default_token_generator.make_token(self.user)
         new_password = "bobbob1"
-        data = {'email':  self.user.email,
+        data = {'uid': urlsafe_base64_encode(force_bytes(self.user.pk)),
                 'token': token,
                 'new_password': new_password}
 
         request = self.factory.post('/', data=data)
         response = self.view(request)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 204)
         user = User.objects.get(email=self.user.email)
         self.assertTrue(user.check_password(new_password))
--- onadata/apps/api/viewsets/reset_password_viewset.py
+++ onadata/apps/api/viewsets/reset_password_viewset.py
@@ -1,13 +1,9 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import send_mail
-
-from onadata.settings.common import DEFAULT_FROM_EMAIL
-
 from rest_framework import status
 from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
+from onadata.libs.serializers.password_reset_serializer import \
+    PasswordResetSerializer, PasswordResetChangeSerializer
 
 
 class ResetPasswordViewSet(ViewSet):
@@ -22,10 +18,9 @@ class ResetPasswordViewSet(ViewSet):
 - Sends an email to the user's email with a url that \
 redirects to a reset password form on the API consumer's website.
 - `email` and `reset_url` are expected in the POST payload.
-- Expected url format is `reset_url=https://{{full_domain_name}}/\
-{{reset_password_form_path}}`.
-- Example of url that is sent to user's email is\
-`http://mydomain.com/reset_form/?token=2f3f334g3r3434`.
+- Expected reset_url format is `reset_url=https:/domain/path/to/reset/form`.
+- Example of reset url sent to user's email is\
+`http://mydomain.com/reset_form?uid=Mg&token=2f3f334g3r3434`.
 
 >
 > Example
@@ -35,58 +30,40 @@ redirects to a reset password form on the API consumer's website.
 >
 > Response:
 >
->        HTTP 200 OK
+>        HTTP 204 OK
 
 
 >
 ## Reset user password
 
 - Resets user's password
-- `email`, `token` and `new_password` are expected in the POST payload.
+- `uid`, `token` and `new_password` are expected in the POST payload.
 
 >
 > Example
 >
->       curl -X POST -d email=demouser@mail.com -d token=qndoi209jf02n4 \
+>       curl -X POST -d uid=Mg -d token=qndoi209jf02n4 \
 -d new_password=usernewpass https://ona.io/api/v1/reset
 >
 > Response:
 >
->        HTTP 200 OK
+>        HTTP 204 OK
 """
 
     permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
+        context = {'request': request}
+        data = request.DATA if request.DATA is not None else {}
+        if 'token' in request.DATA:
+            serializer = PasswordResetChangeSerializer(data=data,
+                                                       context=context)
+        else:
+            serializer = PasswordResetSerializer(data=data, context=context)
 
-        email = request.DATA.get('email')
+        if serializer.is_valid():
+            serializer.save()
 
-        try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
-        reset_url = request.DATA.get('reset_url')
-
-        if user and reset_url:
-            reset_token = default_token_generator.make_token(user)
-            reset_url = reset_url + "?token=" + reset_token
-            email_msg = "Click on the provided link to " \
-                        "reset your Ona password\n " + reset_url
-            send_mail("Ona Password Reset",
-                      email_msg,
-                      DEFAULT_FROM_EMAIL,
-                      (user.email, ))
-            return Response(status=status.HTTP_200_OK)
-
-        token = request.DATA.get('token')
-        new_password = request.DATA.get('new_password')
-
-        if user and token and new_password:
-            if default_token_generator.check_token(user, token):
-                user.set_password(new_password)
-                user.save()
-
-                return Response(status=status.HTTP_200_OK)
-
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
