838d75855f1610cae486483a9d4da58419d7ff64
ekisa <mark.ekisa@gmail.com>
2014-10-31 14:22:59 +0300

    ME: added a test for data endpoint xls export

--- onadata/apps/api/tests/viewsets/test_export_viewset.py
+++ onadata/apps/api/tests/viewsets/test_export_viewset.py
@@ -3,6 +3,7 @@ import os
 from django.test import RequestFactory
 
 from onadata.apps.api.viewsets.xform_viewset import XFormViewSet
+from onadata.apps.api.viewsets.data_viewset import DataViewSet
 from onadata.apps.main.tests.test_base import TestBase
 
 
@@ -25,6 +26,35 @@ class TestExportViewSet(TestBase):
         self._make_submissions()
         view = XFormViewSet.as_view({
             'get': 'retrieve'
+        })
+        formid = self.xform.pk
+        # csv
+        request = self.factory.get('/', **self.extra)
+        response = view(request, pk=formid, format='csv')
+        self.assertEqual(response.status_code, 200)
+        headers = dict(response.items())
+        content_disposition = headers['Content-Disposition']
+        filename = self._filename_from_disposition(content_disposition)
+        basename, ext = os.path.splitext(filename)
+        self.assertEqual(headers['Content-Type'], 'application/csv')
+        self.assertEqual(ext, '.csv')
+
+        # xls
+        request = self.factory.get('/', **self.extra)
+        response = view(request, pk=formid, format='xls')
+        self.assertEqual(response.status_code, 200)
+        headers = dict(response.items())
+        content_disposition = headers['Content-Disposition']
+        filename = self._filename_from_disposition(content_disposition)
+        basename, ext = os.path.splitext(filename)
+        self.assertEqual(headers['Content-Type'],
+                         'application/vnd.openxmlformats')
+        self.assertEqual(ext, '.xlsx')
+
+    def test_data_export(self):
+        self._make_submissions()
+        view = DataViewSet.as_view({
+            'get': 'list'
         })
         formid = self.xform.pk
         # csv
