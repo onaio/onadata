3213255eecb18eea542c2bf5311dff72fa66f910
Ukang'a Dickson <ukanga@gmail.com>
2014-11-12 06:23:53 +0300

    UK: lookup user by pk or username

--- onadata/apps/api/tests/viewsets/test_user_viewset.py
+++ onadata/apps/api/tests/viewsets/test_user_viewset.py
@@ -1,5 +1,3 @@
-import json
-
 from onadata.apps.api.tests.viewsets.test_abstract_viewset import\
     TestAbstractViewSet
 from onadata.apps.api.viewsets.user_viewset import UserViewSet
@@ -13,32 +11,37 @@ class TestUserViewSet(TestAbstractViewSet):
 
     def test_user_get(self):
         """Test authenticated user can access user info"""
+        request = self.factory.get('/', **self.extra)
+
         # users list
         view = UserViewSet.as_view({'get': 'list'})
-        request = self.factory.get('/', **self.extra)
         response = view(request)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, [self.data])
 
         # user with username bob
         view = UserViewSet.as_view({'get': 'retrieve'})
-        request = self.factory.get('/', **self.extra)
         response = view(request, username='bob')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, self.data)
 
     def test_user_anon(self):
         """Test anonymous user can access user info"""
+        request = self.factory.get('/')
+
         # users list endpoint
         view = UserViewSet.as_view({'get': 'list'})
-        request = self.factory.get('/')
         response = view(request)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, [self.data])
 
         # user with username bob
         view = UserViewSet.as_view({'get': 'retrieve'})
-        request = self.factory.get('/')
         response = view(request, username='bob')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, self.data)
+
+        # Test with primary key
+        response = view(request, username=self.user.pk)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, self.data)
--- onadata/apps/api/viewsets/user_viewset.py
+++ onadata/apps/api/viewsets/user_viewset.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import User
+from rest_framework.generics import get_object_or_404
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from onadata.libs.serializers.user_serializer import UserSerializer
@@ -53,3 +54,25 @@ This endpoint allows you to list and retrieve user's first and last names.
     serializer_class = UserSerializer
     lookup_field = 'username'
     permission_classes = [permissions.DjangoObjectPermissionsAllowAnon]
+
+    def get_object(self, queryset=None):
+        """Lookup a  username by pk else use lookup_field"""
+        if queryset is None:
+            queryset = self.filter_queryset(self.get_queryset())
+
+        lookup = self.kwargs.get(self.lookup_field)
+        filter_kwargs = {self.lookup_field: lookup}
+
+        try:
+            pk = int(lookup)
+        except ValueError:
+            pass
+        else:
+            filter_kwargs = {'pk': pk}
+
+        obj = get_object_or_404(queryset, **filter_kwargs)
+
+        # May raise a permission denied
+        self.check_object_permissions(self.request, obj)
+
+        return obj
