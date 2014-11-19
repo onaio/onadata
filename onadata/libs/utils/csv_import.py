708967e638a292d3b512bab8289ecac7d09c46bc
James Nzomo <kazikubwa@gmail.com>
2014-11-19 16:53:41 +0300

    JZ: Commits for #684, #693, #695
    Search and shrink multi column gps info to a single geopoint field to sort #684
    Parse csv header for spaces to sort #693
    Reject non utf-8 csv uploads to sort #695

--- onadata/libs/utils/csv_import.py
+++ onadata/libs/utils/csv_import.py
@@ -52,7 +52,7 @@ def dict2xmlsubmission(submission_dict, xform, instance_id, submission_date):
             '</{0}>'.format(
                 json.loads(xform.json).get('name', xform.id_string),
                 xform.id_string, instance_id, submission_date,
-                dict2xml(submission_dict).replace('\n', '')))
+                dict2xml(submission_dict).replace('\n', ''))).encode('utf-8')
 
 
 def submit_csv(username, xform, csv_file):
@@ -68,72 +68,94 @@ def submit_csv(username, xform, csv_file):
     :return: If sucessful, a dict with import summary else dict with error str.
     :rtype: Dict
     """
-    if isinstance(csv_file, (str, unicode)):
+    if isinstance(csv_file, unicode):
         csv_file = cStringIO.StringIO(csv_file)
     elif csv_file is None or not hasattr(csv_file, 'read'):
         return {'error': (u'Invalid param type for `csv_file`. '
-                          'Expected file or String '
+                          'Expected utf-8 encoded file or unicode string '
                           'got {} instead.'.format(type(csv_file).__name__))}
 
+    csv_file.seek(0)
     csv_reader = ucsv.DictReader(csv_file)
+    # check for spaces in headers
+    if any(' ' in header for header in csv_reader.fieldnames):
+        return {'error': u'CSV file fieldnames should not contain spaces'}
+
     rollback_uuids = []
     submission_time = datetime.utcnow().isoformat()
     ona_uuid = {'formhub': {'uuid': xform.uuid}}
     error = None
     additions = inserts = 0
-    for row in csv_reader:
-        # fetch submission uuid before purging row metadata
-        row_uuid = row.get('_uuid')
-        submitted_by = row.get('_submitted_by')
-        submission_date = row.get('_submission_time', submission_time)
+    try:
+        for row in csv_reader:
+            # fetch submission uuid before purging row metadata
+            row_uuid = row.get('_uuid')
+            submitted_by = row.get('_submitted_by')
+            submission_date = row.get('_submission_time', submission_time)
 
-        row.update({'gps': u'{} {} {} {}'.format(
-            row.get('_location_latitude'),
-            row.get('_location_longitude'),
-            row.get('_location_altitude'),
-            row.get('_location_precision'))})
+            location_data = {}
+            for key in row.keys():  # seems faster than a comprehension
+                # remove metadata (keys starting with '_')
+                if key.startswith('_'):
+                    del row[key]
 
-        for key in row.keys():  # seems faster than a comprehension
-            # remove metadata (keys starting with '_')
-            if key.startswith('_'):
-                del row[key]
-            # process nested data e.g x[formhub/uuid] => x[formhub][uuid]
-            if r'/' in key:
-                p, c = key.split('/')
-                row[p] = {c: row[key]}
-                del row[key]
+                # process nested data e.g x[formhub/uuid] => x[formhub][uuid]
+                if r'/' in key:
+                    p, c = key.split('/')
+                    row[p] = {c: row[key]}
+                    del row[key]
 
-        # inject our form's uuid into the submission
-        row.update(ona_uuid)
+                if key.endswith(('.latitude', '.longitude',
+                                '.altitude', '.precision')):
+                    location_data.setdefault(
+                        key.split('.')[0], {}).update(
+                            {key.split('.')[-1]: row.get(key, '0')})
 
-        old_meta = row.get('meta', {})
-        new_meta, update = get_submission_meta_dict(xform, row_uuid)
-        inserts += update
-        old_meta.update(new_meta)
-        row.update({'meta': old_meta})
+            for key in location_data.keys():
+                row.update({key: (u'{latitude} {longitude} '
+                                  '{altitude} {precision}'
+                                  '').format(**location_data.get(key))})
 
-        row_uuid = row.get('meta').get('instanceID')
-        rollback_uuids.append(row_uuid.replace('uuid:', ''))
+            # inject our form's uuid into the submission
+            row.update(ona_uuid)
 
-        xml_file = cStringIO.StringIO(dict2xmlsubmission(row, xform, row_uuid,
-                                      submission_date))
+            old_meta = row.get('meta', {})
+            new_meta, update = get_submission_meta_dict(xform, row_uuid)
+            inserts += update
+            old_meta.update(new_meta)
+            row.update({'meta': old_meta})
 
-        try:
-            error, instance = safe_create_instance(username, xml_file, [],
-                                                   xform.uuid, None)
-        except ValueError as e:
-            error = e
+            row_uuid = row.get('meta').get('instanceID')
+            rollback_uuids.append(row_uuid.replace('uuid:', ''))
 
-        if error:
-            Instance.objects.filter(uuid__in=rollback_uuids,
-                                    xform=xform).delete()
-            return {'error': str(error)}
-        else:
-            additions += 1
-            users = User.objects.filter(
-                username=submitted_by) if submitted_by else []
-            if users:
-                instance.user = users[0]
-                instance.save()
+            xml_file = cStringIO.StringIO(
+                dict2xmlsubmission(row, xform, row_uuid, submission_date))
+
+            try:
+                error, instance = safe_create_instance(username, xml_file, [],
+                                                       xform.uuid, None)
+            except ValueError as e:
+                error = e
+
+            if error:
+                Instance.objects.filter(uuid__in=rollback_uuids,
+                                        xform=xform).delete()
+                return {'error': str(error)}
+            else:
+                additions += 1
+                users = User.objects.filter(
+                    username=submitted_by) if submitted_by else []
+                if users:
+                    instance.user = users[0]
+                    instance.save()
+
+    except UnicodeDecodeError:
+        Instance.objects.filter(uuid__in=rollback_uuids,
+                                xform=xform).delete()
+        return {'error': u'CSV file must be utf-8 encoded'}
+    except Exception as e:
+        Instance.objects.filter(uuid__in=rollback_uuids,
+                                xform=xform).delete()
+        return {'error': str(e)}
 
     return {'additions': additions - inserts, 'updates': inserts}
