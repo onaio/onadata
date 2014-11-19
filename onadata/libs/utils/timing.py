4d8c20f61c6c006c5d51e971bc7aa318210de20a
ekisa <mark.ekisa@gmail.com>
2014-11-19 11:17:22 +0300

    ME: refactored 'get_date' and added 'last_modified_header' and 'merge_dicts' functions

--- onadata/libs/utils/timing.py
+++ onadata/libs/utils/timing.py
@@ -1,5 +1,9 @@
 import time
+from itertools import chain
 from django.utils import timezone
+from onadata.apps.logger.models.attachment import Attachment
+from onadata.apps.main.models.meta_data import MetaData
+from onadata.apps.main.models.user_profile import UserProfile
 
 
 def print_time(func):
@@ -26,8 +30,25 @@ def get_header_date_format(date_modified):
 
 def get_date(_object=None, which_date=None):
     if _object is None:
-        return timezone.now()
+        return get_header_date_format(timezone.now())
+    if isinstance(_object, Attachment):
+        _object = _object.instance
+    elif isinstance(_object, UserProfile):
+        _object = _object.user
+    elif isinstance(_object, MetaData):
+        _object = _object.xform
+
     if which_date == 'joined':
-        return _object.date_joined
+        _date = _object.date_joined
     elif which_date == 'modified':
-        return _object.date_modified
+        _date = _object.date_modified
+
+    return get_header_date_format(_date)
+
+
+def last_modified_header(last_modified_date):
+    return {'Last-Modified': last_modified_date}
+
+
+def merge_dicts(*args):
+    return dict(chain(*[d.items() for d in args]))
