6571e4580ba70bde935bb70ddefd121d7ec1b2fa
ekisa <mark.ekisa@gmail.com>
2014-11-20 17:36:10 +0300

    ME: added TempToken model

--- onadata/apps/api/models/__init__.py
+++ onadata/apps/api/models/__init__.py
@@ -1,4 +1,6 @@
-from onadata.apps.api.models.organization_profile import OrganizationProfile  # flake8: noqa
+# flake8: noqa
+from onadata.apps.api.models.organization_profile import OrganizationProfile
 from onadata.apps.api.models.project import Project
 from onadata.apps.api.models.project_xform import ProjectXForm
 from onadata.apps.api.models.team import Team
+from onadata.apps.api.models.temp_token import TempToken
