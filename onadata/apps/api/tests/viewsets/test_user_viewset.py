59af3058593bc967bee50c44f870943f72f44d43
Dennis Wambua <dennis.wambua@gmail.com>
2014-11-20 14:39:56 +0300

    DW: Added an endpoint for searching a user using an email

--- onadata/apps/api/tests/viewsets/test_user_viewset.py
+++ onadata/apps/api/tests/viewsets/test_user_viewset.py
@@ -48,3 +48,48 @@ class TestUserViewSet(TestAbstractViewSet):
         response = view(request, username=self.user.pk)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, self.data)
+
+    def test_get_user_using_email(self):
+        alice_data = {'username': 'alice', 'email': 'alice@localhost.com',
+                      'name': u'Alice'}
+        alice_profile = self._create_user_profile(alice_data)
+        data = {'id': alice_profile.user.pk, 'username': u'alice',
+                'first_name': u'Alice', 'last_name': u''}
+        get_params = {
+            'email': alice_profile.user.email,
+        }
+        view = UserViewSet.as_view(
+            {'get': 'list'}
+        )
+        request = self.factory.get('/', data=get_params)
+
+        response = view(request)
+        self.assertEquals(response.status_code, 401)
+        error = {'detail': 'Authentication credentials were not provided.'}
+        self.assertEqual(response.data, error)
+
+        # authenticated
+        request = self.factory.get('/', data=get_params, **self.extra)
+        response = view(request)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.data, data)
+
+        get_params = {
+            'email': 'invalidemail',
+        }
+
+        request = self.factory.get('/', data=get_params, **self.extra)
+        response = view(request)
+
+        self.assertEquals(response.status_code, 400)
+        self.assertEquals(response.data, {'details': 'Invalid email'})
+
+        get_params = {
+            'email': 'doesnotexist@email.com',
+        }
+
+        request = self.factory.get('/', data=get_params, **self.extra)
+        response = view(request)
+        self.assertEquals(response.status_code, 404)
+        self.assertEquals(response.data, {'detail': 'User not found'})
--- onadata/apps/api/viewsets/user_viewset.py
+++ onadata/apps/api/viewsets/user_viewset.py
@@ -2,10 +2,44 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from rest_framework.generics import get_object_or_404
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 from onadata.libs.serializers.user_serializer import UserSerializer
 from onadata.apps.api import permissions
 from onadata.libs.utils.timing import last_modified_header, get_date
+
+
+def is_valid_email(email):
+    try:
+        validate_email(email)
+    except ValidationError:
+        return False
+
+    return True
+
+
+def _search_user(request):
+    # needs to be authenticated
+    if not request.user.is_authenticated():
+        error = {'detail': 'Authentication credentials were not provided.'}
+        return Response(data=error,
+                        status=status.HTTP_401_UNAUTHORIZED)
+    # validate email
+    if not is_valid_email(request.GET.get('email')):
+        return Response(data={'detail': 'Invalid email'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    # search user using email
+    users = User.objects.filter(email=request.GET.get('email'))
+    if users:
+        serializer = UserSerializer(users[0])
+        return Response(serializer.data)
+    else:
+        return Response(data={'detail': 'User not found'},
+                        status=status.HTTP_404_NOT_FOUND)
 
 
 class UserViewSet(ReadOnlyModelViewSet):
@@ -51,6 +85,20 @@ This endpoint allows you to list and retrieve user's first and last names.
 >           "last_name": "Last"
 >       }
 
+
+## Search for a users using email
+> Example
+>
+>       curl -X GET https://ona.io/api/v1/users?email=demo@email.com
+
+> Response:
+
+>       {
+>           "username": "demo",
+>           "first_name": "First",
+>           "last_name": "Last"
+>       }
+
 """
     queryset = User.objects.exclude(pk=settings.ANONYMOUS_USER_ID)
     default_response_headers = last_modified_header(
@@ -80,3 +128,9 @@ This endpoint allows you to list and retrieve user's first and last names.
         self.check_object_permissions(self.request, obj)
 
         return obj
+
+    def list(self, request, *args, **kwargs):
+        if request.GET.get('email'):
+            return _search_user(request)
+        else:
+            return super(UserViewSet, self).list(request, *args, **kwargs)
