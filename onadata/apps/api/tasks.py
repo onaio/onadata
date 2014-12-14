962a2925fc6a1e5353626e5401abd67fa759958c
James Nzomo <kazikubwa@gmail.com>
2014-12-14 15:41:59 +0300

    JZ: Recreate tmpfile fn for large xform uploads

--- onadata/apps/api/tasks.py
+++ onadata/apps/api/tasks.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from celery import task
 from celery.result import AsyncResult
 from django.core.files.uploadedfile import (InMemoryUploadedFile,
@@ -7,21 +9,30 @@ from io import BytesIO
 from onadata.apps.api import tools
 
 
+def recreate_tmp_file(name, path, mime_type):
+    tmp_file = TemporaryUploadedFile(name, mime_type, 0, None)
+    tmp_file.file = open(path)
+    tmp_file.size = os.fstat(tmp_file.fileno()).st_size
+    return tmp_file
+
+
 @task()
 def publish_xlsform_async(user, post_data, owner, file_data):
     try:
-        files = MultiValueDict(
-            {u'xls_file':
-             (InMemoryUploadedFile(BytesIO(file_data.get('data')), None,
-                                   file_data.get('name'),
-                                   u'application/vnd.ms-excel',
-                                   len(file_data.get('data')), None)
-              if file_data.get('data') else
-              TemporaryUploadedFile(file_data.get('path'),
-                                    u'application/vnd.ms-excel', 0))})
+        files = MultiValueDict()
+        files[u'xls_file'] = \
+            (InMemoryUploadedFile(
+                BytesIO(file_data.get('data')), None,
+                file_data.get('name'), u'application/octet-stream',
+                len(file_data.get('data')), None)
+             if file_data.get('data') else
+             recreate_tmp_file(
+                file_data.get('name'), file_data.get('path'),
+                u'application/octet-stream'))
 
         return tools.do_publish_xlsform(user, post_data, files, owner)
-    except e:
+    except:
+        e = sys.exc_info()[0]
         return {u'error': str(e)}
 
 
@@ -32,7 +43,7 @@ def get_async_creation_status(job_uuid):
         return {u'error': u'Empty job uuid'}
 
     job = AsyncResult(job_uuid)
-    result = (job.result or job.state)
+    result = job.result or job.state
     if isinstance(result, basestring):
         return {'JOB_STATUS': result}
 
