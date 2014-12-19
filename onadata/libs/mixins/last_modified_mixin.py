48efd69cb0ca9db860af027ca435e303f8fd62fe
Ukang'a Dickson <ukanga@gmail.com>
2014-12-19 13:28:12 +0300

    UK/GM: add a last_modified_date option in LastModifiedMixin

--- onadata/libs/mixins/last_modified_mixin.py
+++ onadata/libs/mixins/last_modified_mixin.py
@@ -4,21 +4,26 @@ from onadata.libs.utils.timing import last_modified_header, get_date
 class LastModifiedMixin(object):
 
     last_modified_field = 'modified'
+    last_modified_date = None
 
     def finalize_response(self, request, response, *args, **kwargs):
         if request.method == 'GET':
-            obj = None
-            if hasattr(self, 'object_list'):
-                obj = self.object_list.last()
+            if self.last_modified_date is not None:
+                self.headers.update(
+                    last_modified_header(self.last_modified_date))
+            else:
+                obj = None
+                if hasattr(self, 'object_list'):
+                    obj = self.object_list.last()
 
-            if hasattr(self, 'object'):
-                obj = self.object
+                if hasattr(self, 'object'):
+                    obj = self.object
 
-            if not obj:
-                obj = self.queryset.last()
+                if not obj:
+                    obj = self.queryset.last()
 
-            if response.status_code < 400:
-                self.headers.update(last_modified_header(get_date(obj)))
+                if response.status_code < 400:
+                    self.headers.update(last_modified_header(get_date(obj)))
 
         return super(LastModifiedMixin, self).finalize_response(
             request, response, *args, **kwargs)
