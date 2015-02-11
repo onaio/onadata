1c6e545e25a283c1321c208586351f4b0e636044
ekisa <mark.ekisa@gmail.com>
2015-02-11 16:45:01 +0300

    ME: renamed file and added soft delete helper function

--- onadata/libs/utils/cache_tools.py
+++ onadata/libs/utils/cache_tools.py
@@ -1,3 +1,9 @@
+from django.core.cache import cache
+
+
+def safe_delete(key):
+    cache.get(key) and cache.delete(key)
+
 # Cache names used in project serializers
 PROJ_PERM_CACHE = 'ps-project_permissions-'
 PROJ_NUM_DATASET_CACHE = 'ps-num_datasets-'
