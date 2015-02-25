f4f5630c4e3faf890b7c953fbbb79bd74a4d29a8
ekisa <mark.ekisa@gmail>
2015-02-25 17:55:18 +0300

    ME: increased the max length of media_file to 255

--- onadata/apps/logger/models/attachment.py
+++ onadata/apps/logger/models/attachment.py
@@ -17,7 +17,7 @@ def upload_to(instance, filename):
 class Attachment(models.Model):
     OSM = 'osm'
     instance = models.ForeignKey(Instance, related_name="attachments")
-    media_file = models.FileField(upload_to=upload_to)
+    media_file = models.FileField(max_length=255, upload_to=upload_to)
     mimetype = models.CharField(
         max_length=50, null=False, blank=True, default='')
     extension = models.CharField(max_length=10, null=False, blank=False,
