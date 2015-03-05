a5c0d508c5869cddb30d95d6b663dbaadc83e11f
pld <peter@helioid.com>
2015-03-05 11:34:30 -0500

    PLD: use request kr theme

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ from django.conf import settings
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('_themes'))
 settings.configure()
 
 # -- General configuration ------------------------------------------------
@@ -106,7 +107,9 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'nature'
+html_theme_path = ['_themes']
+html_theme = 'kr'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
