c8873e66a4c955ac7420870221d6ccfea6ed36cd
Ukang'a Dickson <ukanga@gmail.com>
2015-03-06 15:11:55 +0300

    UK: redirect /api to docs, api/v1 still goes to api/v1

--- onadata/apps/main/urls.py
+++ onadata/apps/main/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns(
     (r'^i18n/', include('django.conf.urls.i18n')),
     url('^api/v1/', include(router.urls)),
     url(r'^api-docs/', RedirectView.as_view(url=settings.STATIC_DOC)),
-    url(r'^api/', RedirectView.as_view(url='/api/v1/')),
+    url(r'^api/', RedirectView.as_view(url=settings.STATIC_DOC)),
     url(r'^api/v1', RedirectView.as_view(url='/api/v1/')),
 
     # django default stuff
