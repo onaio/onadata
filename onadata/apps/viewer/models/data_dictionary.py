5a4e51b8fd3074b50d759fff09d7bb141abe276b
Ukang'a Dickson <ukanga@gmail.com>
2015-03-11 10:27:56 +0300

    UK: updates for _submitted_by

--- onadata/apps/viewer/models/data_dictionary.py
+++ onadata/apps/viewer/models/data_dictionary.py
@@ -22,7 +22,7 @@ from onadata.apps.logger.models.xform import XForm
 from onadata.apps.logger.xform_instance_parser import clean_and_parse_xml
 from onadata.apps.viewer.models.parsed_instance import _encode_for_mongo
 from onadata.libs.utils.common_tags import UUID, SUBMISSION_TIME, TAGS, NOTES,\
-    VERSION, DURATION
+    VERSION, DURATION, SUBMITTED_BY
 from onadata.libs.utils.export_tools import question_types_to_exclude,\
     DictOrganizer
 from onadata.libs.utils.model_tools import queryset_iterator, set_uuid
@@ -367,7 +367,8 @@ class DataDictionary(XForm):
             return '/'.join(l[2:])
 
         header_list = [shorten(xpath) for xpath in self.xpaths()]
-        header_list += [UUID, SUBMISSION_TIME, TAGS, NOTES, VERSION, DURATION]
+        header_list += [UUID, SUBMISSION_TIME, TAGS, NOTES, VERSION, DURATION,
+                        SUBMITTED_BY]
         if include_additional_headers:
             header_list += self._additional_headers()
         return header_list
