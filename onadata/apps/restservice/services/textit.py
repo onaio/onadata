e476e9756f90fffd6ce89373d5b8f925b773a8cf
Dennis Wambua <dennis.wambua@gmail.com>
2015-03-13 16:24:51 +0300

    DW: Sanitize the json payload

--- onadata/apps/restservice/services/textit.py
+++ onadata/apps/restservice/services/textit.py
@@ -17,13 +17,7 @@ class ServiceDefinition(RestServiceInterface):
         :param parsed_instance:
         :return:
         """
-        extra_data = parsed_instance.instance.json
-        # Ensure both key and value are string
-        for key in extra_data:
-            value = extra_data[key]
-
-            if not isinstance(value, basestring):
-                extra_data[key] = str(value)
+        extra_data = self.clean_keys_of_slashes(parsed_instance.instance.json)
 
         meta = MetaData.textit(parsed_instance.instance.xform)
 
@@ -40,3 +34,27 @@ class ServiceDefinition(RestServiceInterface):
         resp, content = http.request(uri=url, method='POST',
                                      headers=headers,
                                      body=json.dumps(post_data))
+
+    def clean_keys_of_slashes(self, record):
+        """
+        Replaces the slashes found in a dataset keys with underscores
+        :param record: list containing a couple of dictionaries
+        :return: record with keys without slashes
+        """
+        for key in record:
+            value = record[key]
+            # Ensure both key and value are string
+            if not isinstance(value, basestring):
+                record[key] = str(value)
+
+            if '/' in key:
+                # replace with _
+                record[key.replace('/', '_')]\
+                    = record.pop(key)
+            # Check if the value is a list containing nested dict and apply same
+            if value:
+                if isinstance(value, list) and isinstance(value[0], dict):
+                    for v in value:
+                        self.clean_keys_of_slashes(v)
+
+        return record
