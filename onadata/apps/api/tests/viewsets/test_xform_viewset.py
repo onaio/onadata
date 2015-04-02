6f73015f766cec7866b77b269653ac960bddf267
ivermac <mark.ekisa@gmail.com>
2015-04-02 11:53:58 +0300

    ME: reverted back to using 'format' instead of 'fmt'

--- onadata/apps/api/tests/viewsets/test_xform_viewset.py
+++ onadata/apps/api/tests/viewsets/test_xform_viewset.py
@@ -1806,7 +1806,7 @@ server=http://testserver/%s/&id=transportation_2011_07_25' %
             with HTTMock(external_mock):
                 # External export
                 request = self.factory.get(
-                    '/', data={"fmt": "xls", "meta": metadata.pk},
+                    '/', data={"format": "xls", "meta": metadata.pk},
                     **self.extra)
                 response = view(request, pk=formid)
 
@@ -1853,7 +1853,7 @@ server=http://testserver/%s/&id=transportation_2011_07_25' %
             with HTTMock(external_mock):
                 # External export
                 request = self.factory.get(
-                    '/', data={"fmt": "xls", "meta": metadata.pk,
+                    '/', data={"format": "xls", "meta": metadata.pk,
                                'data_id': self.xform.instances.all()[0].pk},
                     **self.extra)
                 response = view(request, pk=formid)
