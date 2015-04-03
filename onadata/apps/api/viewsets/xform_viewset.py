2b4a4d2f9dd216aed88e32bc22d2b58cc4ce3eab
Ukang'a Dickson <ukanga@gmail.com>
2015-04-03 19:10:05 +0300

    UK: query should be None, not defaulted to an empty dict

--- onadata/apps/api/viewsets/xform_viewset.py
+++ onadata/apps/api/viewsets/xform_viewset.py
@@ -576,7 +576,7 @@ class XFormViewSet(AnonymousUserPublicFormsMixin,
     def export_async(self, request, *args, **kwargs):
         job_uuid = request.QUERY_PARAMS.get('job_uuid')
         export_type = request.QUERY_PARAMS.get('format')
-        query = request.GET.get("query", {})
+        query = request.GET.get("query")
         xform = self.get_object()
 
         token = request.QUERY_PARAMS.get('token')
