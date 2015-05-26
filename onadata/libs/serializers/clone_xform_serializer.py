bd4e3c507a1c3bd33b610492e8a6cbdc9b6de02f
ivermac <mark.ekisa@gmail.com>
2015-05-26 10:50:29 +0300

    ME: validate project

--- onadata/libs/serializers/clone_xform_serializer.py
+++ onadata/libs/serializers/clone_xform_serializer.py
@@ -5,18 +5,20 @@ from django.utils.translation import ugettext as _
 from rest_framework import serializers
 from onadata.libs.models.clone_xform import CloneXForm
 from onadata.libs.serializers.fields.xform_field import XFormField
+from onadata.libs.serializers.fields.project_field import ProjectField
+from onadata.apps.logger.models.project import Project
 
 
 class CloneXFormSerializer(serializers.Serializer):
     xform = XFormField()
     username = serializers.CharField(max_length=255)
-    project_id = serializers.IntegerField(default=-1, required=False)
+    project = ProjectField(required=False)
 
     def restore_object(self, attrs, instance=None):
         if instance is not None:
             instance.xform = attrs.get('xform', instance.xform)
             instance.username = attrs.get('username', instance.username)
-            instance.project_id = attrs.get('project_id', instance.project_id)
+            instance.project = attrs.get('project', instance.project)
 
             return instance
 
@@ -31,4 +33,16 @@ class CloneXFormSerializer(serializers.Serializer):
             raise ValidationError(_(u"User '%(value)s' does not exist."
                                     % {"value": value}))
 
+        return attrs
+
+    def validate_project(self, attrs, source):
+        """Check that projects exists"""
+        value = attrs[source]
+        if value is not None and not isinstance(value, Project):
+            try:
+                Project.objects.get(pk=int(value))
+            except Project.DoesNotExist:
+                raise ValidationError(
+                    _(u"Project with id '%(value)s' does not exist." %
+                        {"value": value}))
         return attrs
