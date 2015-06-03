83f8e59fa347126e28bcb935a9083ee766b078b1
ivermac <mark.ekisa@gmail.com>
2015-06-03 10:56:51 +0300

    ME: code refactor

--- onadata/libs/serializers/fields/project_field.py
+++ onadata/libs/serializers/fields/project_field.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ParseError
+from django.core.validators import ValidationError
+from django.utils.translation import ugettext as _
 from onadata.apps.logger.models.project import Project
 
 
@@ -8,10 +10,13 @@ class ProjectField(serializers.WritableField):
         return obj.pk
 
     def from_native(self, data):
-        try:
-            project = Project.objects.get(pk=data)
-        except Project.DoesNotExist:
-            project = data
-        except ValueError as e:
-            raise ParseError(unicode(e))
-        return project
+        if data is not None:
+            try:
+                project = Project.objects.get(pk=data)
+            except Project.DoesNotExist:
+                raise ValidationError(
+                    _(u"Project with id '%(value)s' does not exist." %
+                        {"value": data}))
+            except ValueError as e:
+                raise ParseError(unicode(e))
+            return project
