fb8fa19c9367bad5d03fda29323bd7b45417b1fd
Ukang'a Dickson <ukanga@gmail.com>
2015-07-23 14:36:42 +0300

    remove restore_object and now using create() and updated() methods - textit serializer

--- onadata/libs/serializers/textit_serializer.py
+++ onadata/libs/serializers/textit_serializer.py
@@ -15,21 +15,26 @@ class TextItSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=50, required=True)
     service_url = serializers.URLField(required=True)
 
-    def restore_object(self, attrs, instance=None):
+    def update(self, instance, validated_data):
+        meta = MetaData.textit(instance.xform)
+        values = meta.data_value.split(settings.METADATA_SEPARATOR)
+        if len(values) < 3:
+            values = ['', '', '']
+        xform = validated_data.get('xform', instance.xform)
+        auth_token = validated_data.get('auth_token', values[0])
+        flow_uuid = validated_data.get('flow_uuid', values[1])
+        contacts = validated_data.get('contacts', values[2])
+        name = validated_data.get('name', instance.name)
+        service_url = validated_data.get('service_url', instance.service_url)
 
-        if instance:
-            meta = MetaData.textit(instance.xform)
-            values = meta.data_value.split(settings.METADATA_SEPARATOR)
-            if len(values) < 3:
-                values = ['', '', '']
-            xform = attrs.get('xform', instance.xform)
-            auth_token = attrs.get('auth_token', values[0])
-            flow_uuid = attrs.get('flow_uuid', values[1])
-            contacts = attrs.get('contacts', values[2])
-            name = attrs.get('name', instance.name)
-            service_url = attrs.get('service_url', instance.service_url)
-
-            return TextItService(xform, service_url, name, auth_token,
+        instance = TextItService(xform, service_url, name, auth_token,
                                  flow_uuid, contacts, instance.pk)
+        instance.save()
 
-        return TextItService(**attrs)
+        return instance
+
+    def create(self, validated_data):
+        instance = TextItService(**validated_data)
+        instance.save()
+
+        return instance
