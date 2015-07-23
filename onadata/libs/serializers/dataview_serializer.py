10304d332ca0b02c0be6b93045811557215ecb22
Ukang'a Dickson <ukanga@gmail.com>
2015-07-23 11:44:40 +0300

    raise serializers.ValidationError

--- onadata/libs/serializers/dataview_serializer.py
+++ onadata/libs/serializers/dataview_serializer.py
@@ -1,4 +1,3 @@
-from django.core.validators import ValidationError
 from django.utils.translation import ugettext as _
 from django.core.cache import cache
 
@@ -37,18 +36,26 @@ class DataViewSerializer(serializers.HyperlinkedModelSerializer):
         if query:
             for q in query:
                 if 'column' not in q:
-                    raise ValidationError(_(u"`column` not set in query"))
+                    raise serializers.ValidationError(_(
+                        u"`column` not set in query"
+                    ))
 
                 if 'filter' not in q:
-                    raise ValidationError(_(u"`filter` not set in query"))
+                    raise serializers.ValidationError(_(
+                        u"`filter` not set in query"
+                    ))
 
                 if 'value' not in q:
-                    raise ValidationError(_(u"`value` not set in query"))
+                    raise serializers.ValidationError(_(
+                        u"`value` not set in query"
+                    ))
 
                 comp = q.get('filter')
 
                 if comp not in SUPPORTED_FILTERS:
-                    raise ValidationError(_(u"Filter not supported"))
+                    raise serializers.ValidationError(_(
+                        u"Filter not supported"
+                    ))
 
         return attrs
 
@@ -56,7 +63,9 @@ class DataViewSerializer(serializers.HyperlinkedModelSerializer):
         columns = attrs.get('columns')
 
         if not isinstance(columns, list):
-            raise ValidationError(_(u"`columns` should be a list of columns"))
+            raise serializers.ValidationError(_(
+                u"`columns` should be a list of columns"
+            ))
 
         return attrs
 
