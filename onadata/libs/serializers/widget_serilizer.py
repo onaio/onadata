903474178790cf648f662ee804bb62ee15f39fea
Ukang'a Dickson <ukanga@gmail.com>
2015-08-03 11:05:31 +0300

    a custom generic related field to support dataview and xform relations with the widget serializer

--- onadata/libs/serializers/widget_serilizer.py
+++ onadata/libs/serializers/widget_serilizer.py
@@ -1,7 +1,10 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.urlresolvers import resolve, get_script_prefix, Resolver404
 from django.utils.translation import ugettext as _
-
-from rest_framework import serializers
 from guardian.shortcuts import get_users_with_perms
+from rest_framework import serializers
+from rest_framework.compat import urlparse
+from rest_framework.reverse import reverse
 
 from onadata.apps.logger.models.xform import XForm
 from onadata.apps.logger.models.data_view import DataView
@@ -9,28 +12,76 @@ from onadata.apps.logger.models.widget import Widget
 from onadata.libs.utils.string import str2bool
 
 
-class GenericRelatedField(serializers.RelatedField):
+class GenericRelatedField(serializers.HyperlinkedRelatedField):
+    default_error_messages = {
+        'incorrect_match': _('`{input}` is not a valid relation.')
+    }
+
+    def __init__(self, *args, **kwargs):
+        self.view_names = ['xform-detail', 'dataviews-detail']
+        self.resolve = resolve
+        self.reverse = reverse
+        super(serializers.RelatedField, self).__init__(*args, **kwargs)
+
+    def _setup_field(self, view_name):
+        self.lookup_url_kwarg = self.lookup_field
+
+        if view_name == 'xform-detail':
+            self.queryset = XForm.objects.all()
+
+        if view_name == 'dataviews-detail':
+            self.queryset = DataView.objects.all()
+
     def to_representation(self, value):
         if isinstance(value, XForm):
-            return value.url
+            self.view_name = 'xform-detail'
         elif isinstance(value, DataView):
-            return value.url
+            self.view_name = 'dataviews-detail'
+        else:
+            raise Exception(_(u"Uknown type for content_object."))
 
-        raise Exception(_(u"Uknown tyoe for content_object"))
+        self._setup_field(self.view_name)
+
+        return super(GenericRelatedField, self).to_representation(value)
+
+    def to_internal_value(self, data):
+        try:
+            http_prefix = data.startswith(('http:', 'https:'))
+        except AttributeError:
+            self.fail('incorrect_type', data_type=type(data).__name__)
+        input_data = data
+        if http_prefix:
+            # If needed convert absolute URLs to relative path
+            data = urlparse.urlparse(data).path
+            prefix = get_script_prefix()
+            if data.startswith(prefix):
+                data = '/' + data[len(prefix):]
+
+        try:
+            match = self.resolve(data)
+        except Resolver404:
+            self.fail('no_match')
+
+        if match.view_name not in self.view_names:
+            self.fail('incorrect_match', input=input_data)
+
+        self._setup_field(match.view_name)
+
+        try:
+            return self.get_object(match.view_name, match.args, match.kwargs)
+        except (ObjectDoesNotExist, TypeError, ValueError):
+            self.fail('does_not_exist')
+
+        return data
 
 
 class WidgetSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='widgets-detail')
-    key = serializers.CharField(max_length=255, read_only=True)
-    title = serializers.CharField(max_length=255, required=False)
-    description = serializers.CharField(max_length=255, required=False)
-
-    widget_type = serializers.ChoiceField(choices=Widget.WIDGETS_TYPES)
-    view_type = serializers.CharField(max_length=50)
-    column = serializers.CharField(max_length=50)
-    group_by = serializers.CharField(max_length=50, required=False)
-
-    content_object = GenericRelatedField(queryset=[])
+    url = serializers.HyperlinkedIdentityField(
+        view_name='widgets-detail',
+        lookup_field='pk'
+    )
+    content_object = GenericRelatedField()
+    key = serializers.CharField(read_only=True)
 
     data = serializers.SerializerMethodField()
 
@@ -71,9 +122,9 @@ class WidgetSerializer(serializers.HyperlinkedModelSerializer):
             data_dictionary = xform.data_dictionary()
 
             if column not in data_dictionary.get_headers():
-                raise serializers.ValidationError(_(
-                    u"'{}' not in the form".format(column)
-                ))
+                raise serializers.ValidationError({
+                    'column': _(u"'{}' not in the form.".format(column))
+                })
 
         return attrs
 
@@ -86,7 +137,7 @@ class WidgetSerializer(serializers.HyperlinkedModelSerializer):
 
         if request.user not in users:
             raise serializers.ValidationError(_(
-                u"You don't have permission to the XForm"
+                u"You don't have permission to the XForm."
             ))
 
         return value
