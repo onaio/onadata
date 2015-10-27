b308f71678989fa831a3186796ffb75e37e863f3
Dennis Wambua <dennis.wambua@gmail.com>
2015-10-27 11:34:27 +0300

    DW: added a management command

---
+++ onadata/apps/logger/management/commands/update_moved_forms.py
@@ -0,0 +1,30 @@
+from django.core.management import BaseCommand
+from django.utils.translation import ugettext_lazy
+
+from onadata.apps.logger.models.project import Project
+from onadata.libs.utils.model_tools import queryset_iterator
+
+
+class Command(BaseCommand):
+    help = ugettext_lazy("Ensures all the forms are owned by the project"
+                         " owner")
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write("Updating forms owner", ending='\n')
+
+        for project in queryset_iterator(Project.objects.all()):
+            for xform in project.xform_set.all():
+                try:
+                    if xform.user != project.organization:
+                        self.stdout.write(
+                        "Processing: {} - {}".format(xform.id_string,
+                                                     xform.user.username)
+                    )
+                        xform.user = project.organization
+                        xform.save()
+                except:
+                    self.stdout.write(
+                        "Error processing: {} - {}".format(xform.id_string,
+                                                     xform.user.username)
+                    )
+                    pass
