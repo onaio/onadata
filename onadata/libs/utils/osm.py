293d1459769b0311229ce009d11ddbb29c7cec59
Ukang'a Dickson <ukanga@gmail.com>
2015-11-09 11:15:43 +0300

    restore get_combined_osm

--- onadata/libs/utils/osm.py
+++ onadata/libs/utils/osm.py
@@ -5,10 +5,48 @@ from django.contrib.gis.geos import Point
 from django.contrib.gis.geos import Polygon
 from django.contrib.gis.geos import GeometryCollection
 
+from lxml import etree
+
 from onadata.apps.logger.models.osmdata import OsmData
 from onadata.apps.logger.models.attachment import Attachment
 
-from lxml import etree
+
+def get_combined_osm(files):
+    """
+    Combines a list of osm files
+    :param files - list of osm file objects
+
+    :return string: osm xml string of the combined files
+    """
+    def _parse_osm_file(f):
+        try:
+            return etree.parse(f)
+        except:
+            return None
+
+    xml = u""
+    if len(files) and isinstance(files, list):
+        osm = None
+        for f in files:
+            _osm = _parse_osm_file(f)
+            if _osm is None:
+                continue
+
+            if osm is None:
+                osm = _osm
+                continue
+
+            for child in _osm.getroot().getchildren():
+                osm.getroot().append(child)
+
+        if osm:
+            xml = etree.tostring(osm, encoding='utf-8', xml_declaration=True)
+
+    elif isinstance(files, dict):
+        if 'detail' in files:
+            xml = u'<error>' + files['detail'] + '</error>'
+
+    return xml
 
 
 def _get_xml_obj(xml):
