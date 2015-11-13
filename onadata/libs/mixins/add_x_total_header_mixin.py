94ec8811c78d3846bfb998417fa5d8020b29bc30
ivermac <mark.ekisa@gmail.com>
2015-11-13 11:26:42 +0300

    ME: renamed total header mixin file

--- onadata/libs/mixins/add_x_total_header_mixin.py
+++
@@ -1,8 +0,0 @@
-class AddXTotalHeaderMixin(object):
-
-    def finalize_response(self, request, response, *args, **kwargs):
-        if self.x_total_count is not None:
-            self.headers.update({'X-total': self.x_total_count})
-
-        return super(AddXTotalHeaderMixin, self).finalize_response(
-            request, response, *args, **kwargs)
