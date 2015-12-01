f4f6774e98ca6f3439882ac54ba35917b4bc9ec1
Dennis Wambua <dennis.wambua@gmail.com>
2015-12-01 17:52:09 +0300

    DW: Fix tests

--- onadata/apps/viewer/tests/test_parsed_instance.py
+++ onadata/apps/viewer/tests/test_parsed_instance.py
@@ -13,11 +13,11 @@ class TestParsedInstance(TestBase):
     def test_get_where_clause_with_string_query(self):
         query = 'bla'
         where, where_params = ParsedInstance._get_where_clause(query)
-        self.assertEqual(where, [u"json::text like %s"])
-        self.assertEqual(where_params, ["%%bla%%"])
+        self.assertEqual(where, [u"json::text ~* %s"])
+        self.assertEqual(where_params, ["bla"])
 
     def test_get_where_clause_with_integer(self):
         query = '11'
         where, where_params = ParsedInstance._get_where_clause(query)
-        self.assertEqual(where, [u"json::text like %s"])
-        self.assertEqual(where_params, ["%%11%%"])
+        self.assertEqual(where, [u"json::text ~* %s"])
+        self.assertEqual(where_params, [11])
