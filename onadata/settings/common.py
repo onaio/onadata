2c7e9f8f6cb4e17b6d0c729dfd79b44281cfdf25
ukanga <ukanga+osx@ona.io>
2015-12-27 13:34:04 +0300

    update restframework third party packages

--- onadata/settings/common.py
+++ onadata/settings/common.py
@@ -238,7 +238,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.JSONPRenderer',
+        'rest_framework_jsonp.renderers.JSONPRenderer',
         'rest_framework_csv.renderers.CSVRenderer',
     ),
 }
