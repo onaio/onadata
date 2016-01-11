9132cff86225b409a05bfc6a85658ee7b957d403
Okal Otieno <okal@justokal.com>
2016-01-11 14:49:22 +0300

    OO: Add test for ExportViewSet

---
+++ onadata/apps/api/tests/viewsets/test_export_viewset.py
@@ -0,0 +1,36 @@
+import os
+
+from django.conf import settings
+from onadata.apps.api.viewsets.export_viewset import ExportViewSet
+from onadata.apps.main.tests.test_base import TestBase
+from onadata.apps.viewer.models.export import Export
+from onadata.apps.api.models.temp_token import TempToken
+
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+from tempfile import NamedTemporaryFile
+
+
+class TestExportViewSet(TestBase):
+
+    def setUp(self):
+        super(self.__class__, self).setUp()
+        self._create_user_and_login()
+        self._publish_transportation_form()
+        self.factory = APIRequestFactory()
+        self.view = ExportViewSet.as_view({'get': 'retrieve'})
+
+    def test_generates_expected_response(self):
+        temp_dir = settings.MEDIA_ROOT
+        dummy_export_file = NamedTemporaryFile(suffix='.xlsx', dir=temp_dir)
+        filename = os.path.basename(dummy_export_file.name)
+        filedir = os.path.dirname(dummy_export_file.name)
+        export = Export.objects.create(xform=self.xform,
+                                       filename=filename,
+                                       filedir=filedir)
+        export.save()
+        request = self.factory.get('/export')
+        force_authenticate(request, user=self.user)
+        response = self.view(request, pk=export.pk)
+        self.assertIn(filename,
+                      response.get('Content-Disposition'))
