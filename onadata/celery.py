83df8ac9b22bec4709015be2c65ced90ce18a80f
ivermac <mark.ekisa@gmail.com>
2016-01-15 11:29:59 +0300

    ME: fixing flake8 issues

--- onadata/celery.py
+++ onadata/celery.py
@@ -2,13 +2,11 @@ from __future__ import absolute_import
 
 import os
 
+from django.conf import settings
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onadata.settings.common')
-
-from django.conf import settings
-
 
 app = Celery('onadata.celery')
 
