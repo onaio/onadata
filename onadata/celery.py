3356794c38dd5ee8d8e4bf57c1bc22754ba0fdb3
Dennis Wambua <dennis.wambua@gmail.com>
2016-01-18 13:59:03 +0300

    DW: removed unused import

--- onadata/celery.py
+++ onadata/celery.py
@@ -4,7 +4,6 @@ import os
 
 from django.conf import settings
 from celery import Celery
-from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onadata.settings.common')
