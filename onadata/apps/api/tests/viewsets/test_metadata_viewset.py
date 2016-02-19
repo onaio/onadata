79170d5ac46d70226b794f009029705d0564e16a
geoffreymuchai <geoffreymuchai@gmail.com>
2016-02-19 16:57:01 +0300

    GM: Add tests for Project MetaData Support in MetaDataViewSet

--- onadata/apps/api/tests/viewsets/test_metadata_viewset.py
+++ onadata/apps/api/tests/viewsets/test_metadata_viewset.py
@@ -8,7 +8,7 @@ from onadata.apps.api.tests.viewsets.test_abstract_viewset import (
 from onadata.apps.api.viewsets.metadata_viewset import MetaDataViewSet
 from onadata.apps.api.viewsets.project_viewset import ProjectViewSet
 from onadata.apps.api.viewsets.xform_viewset import XFormViewSet
-from onadata.apps.main.models.meta_data import MetaData
+from onadata.apps.main.models.meta_data import MetaData, ProjectMetaData
 from onadata.libs.serializers.xform_serializer import XFormSerializer
 
 
@@ -28,6 +28,37 @@ class TestMetaDataViewSet(TestAbstractViewSet):
             "transportation"
         )
         self.path = os.path.join(self.fixture_dir, self.data_value)
+
+    def _add_project_metadata(self, project, data_type, data_value, path=None):
+        data = {
+            'data_type': data_type,
+            'data_value': data_value,
+            'project': project.id
+        }
+
+        if path and data_value:
+            with open(path) as media_file:
+                data.update({
+                    'data_file': media_file,
+                })
+                self._post_to_viewset(data)
+        else:
+            self._post_to_viewset(data)
+
+    def _post_to_viewset(self, data, test=True):
+        count = ProjectMetaData.objects.count()
+        view = MetaDataViewSet.as_view({"post": "create"})
+        request = self.factory.post('/', data, **self.extra)
+        response = view(request)
+
+        if test:
+            self.assertEqual(response.status_code, 201)
+            another_count = ProjectMetaData.objects.count()
+            self.assertEqual(another_count, count + 1)
+            self.project_metadata = ProjectMetaData.objects.get(
+                pk=response.data['id'])
+
+        return response
 
     def test_add_metadata_with_file_attachment(self):
         for data_type in ['supporting_doc', 'media', 'source']:
@@ -208,3 +239,37 @@ class TestMetaDataViewSet(TestAbstractViewSet):
         request = self.factory.get('/', data, **self.extra)
         response = self.view(request)
         self.assertEqual(response.status_code, 400)
+
+    def test_project_metadata_has_project_field(self):
+        self._add_project_metadata(
+            self.project, 'supporting_doc', self.data_value, self.path)
+
+        # Test json of project metadata
+        request = self.factory.get('/', **self.extra)
+        response = self.view(request, pk=self.project_metadata.pk)
+
+        data = dict(response.data)
+        self.assertEqual(data['project'], self.project_metadata.object_id)
+
+    def test_should_return_both_xform_and_project_metadata(self):
+        # delete all existing metadata
+        MetaData.objects.all().delete()
+
+        self._add_project_metadata(
+            self.project, 'media', "check.png", self.path)
+
+        self._add_form_metadata(
+            self.xform, 'supporting_doc', "bla.png", self.path)
+
+        view = MetaDataViewSet.as_view({'get': 'list'})
+        request = self.factory.get("/", **self.extra)
+        response = view(request)
+
+        project_metadata = dict(response.data[0])
+        xform_metadata = dict(response.data[1])
+
+        self.assertEquals(project_metadata.get('project'), self.project.id)
+        self.assertIsNone(project_metadata.get('xform'))
+
+        self.assertEquals(xform_metadata.get('xform'), self.xform.id)
+        self.assertIsNone(xform_metadata.get('project'))
