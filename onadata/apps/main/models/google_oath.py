6030841797ab1a363ed1ed0d9cf165c722bcf09f
Dennis Wambua <dennis.wambua@gmail.com>
2016-02-23 17:29:09 +0300

    DW: used latest oauth2 client lib from google

--- onadata/apps/main/models/google_oath.py
+++ onadata/apps/main/models/google_oath.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
+from oauth2client.django_orm import CredentialsField
 
 
 class TokenStorageModel(models.Model):
     id = models.ForeignKey(User, primary_key=True, related_name='google_id')
-    token = models.TextField()
+    credential = CredentialsField()
 
     class Meta:
         app_label = 'main'
