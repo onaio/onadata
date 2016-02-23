bfdb141ed65d83790a020e380daf4458fa83e8be
geoffreymuchai <geoffreymuchai@gmail.com>
2016-02-23 12:41:25 +0300

    GM|ME Fix failing test_xform_show test

--- onadata/apps/main/models/meta_data.py
+++ onadata/apps/main/models/meta_data.py
@@ -379,7 +379,7 @@ class ProjectMetaData(MetaData):
 
 post_save.connect(clear_cached_metadata_instance_object, sender=MetaData,
                   dispatch_uid='clear_cached_metadata_instance_object')
-post_save.connect(update_attached_xform, sender=MetaData,
+post_save.connect(update_attached_xform, sender=XFormMetaData,
                   dispatch_uid='update_attached_xform')
 post_delete.connect(clear_cached_metadata_instance_object, sender=MetaData,
                     dispatch_uid='clear_cached_metadata_instance_delete')
