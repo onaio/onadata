32815c1fefa7acad0db272429e510faffe9a1e39
ivermac <mark.ekisa@gmail.com>
2016-03-09 10:59:20 +0300

    ME: updated test to check for id_string_changed field value

--- onadata/apps/api/tests/viewsets/test_xform_viewset.py
+++ onadata/apps/api/tests/viewsets/test_xform_viewset.py
@@ -1052,7 +1052,8 @@ class TestXFormViewSet(TestAbstractViewSet):
                 xform = self.user.xforms.all()[0]
                 data.update({
                     'url':
-                    'http://testserver/api/v1/forms/%s' % xform.pk
+                    'http://testserver/api/v1/forms/%s' % xform.pk,
+                    'id_string_changed': False,
                 })
 
                 self.assertDictContainsSubset(data, response.data)
@@ -1081,7 +1082,8 @@ class TestXFormViewSet(TestAbstractViewSet):
                     'url': 'http://testserver/api/v1/forms/%s' % xform.pk,
                     'id_string': u'Transportation_2011_07_25_1',
                     'title': u'Transportation_2011_07_25',
-                    'sms_id_string': u'Transportation_2011_07_25'
+                    'sms_id_string': u'Transportation_2011_07_25',
+                    'id_string_changed': True,
                 })
 
                 self.assertDictContainsSubset(data, response.data)
