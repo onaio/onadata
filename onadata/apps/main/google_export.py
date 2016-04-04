75fd744d5c821eaff14a55e08628fff2db7aecad
Dennis Wambua <dennis.wambua@gmail.com>
2016-04-04 15:22:33 +0300

    DW: Fetches existing spreadsheet and updates with the new records

--- onadata/apps/main/google_export.py
+++ onadata/apps/main/google_export.py
@@ -1,7 +1,4 @@
-import gspread
-
 from oauth2client.contrib.django_orm import Storage
-from oauth2client import client as google_client
 
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
@@ -10,33 +7,6 @@ from django.http import HttpResponseRedirect
 from onadata.apps.main.models import TokenStorageModel
 from onadata.apps.main.views import home
 from onadata.libs.utils.google import google_flow
-
-
-def google_oauth2_request(request):
-    token = None
-    if request.user.is_authenticated():
-        try:
-            storage = Storage(TokenStorageModel, 'id', request.user,
-                              'credential')
-            credential = storage.get()
-        except TokenStorageModel.DoesNotExist:
-            pass
-        else:
-            token = credential.access_token
-    elif request.session.get('access_token'):
-        token = request.session.get('access_token')
-    if token is not None:
-        google_creds = google_client.OAuth2Credentials.from_json(token)
-        gc = gspread.authorize(google_creds)
-
-        docs_feed = gc.get_spreadsheets_feed()
-        _l = '<ul>'
-        for entry in docs_feed.entry:
-            _l += '<li>%s</li>' % entry.title.text
-            print entry.title.text
-        _l += '</ul>'
-        return HttpResponse(_l)
-    return HttpResponseRedirect(google_flow.step1_get_authorize_url())
 
 
 def google_auth_return(request):
