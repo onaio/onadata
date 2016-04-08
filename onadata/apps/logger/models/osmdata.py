2d64d8e081d94adc6aa3850fffa3c0ca3f0a899d
Ukang'a Dickson <ukanga@gmail.com>
2016-04-08 15:30:54 +0300

    use JSONB_OBJECT_KEYS, we have switched to jsonb fields

--- onadata/apps/logger/models/osmdata.py
+++ onadata/apps/logger/models/osmdata.py
@@ -28,7 +28,7 @@ class OsmData(models.Model):
     @classmethod
     def get_tag_keys(cls, xform, field_path, include_prefix=False):
         query = OsmData.objects.raw(
-            '''SELECT DISTINCT JSON_OBJECT_KEYS(tags) as id FROM "logger_osmdata" INNER JOIN "logger_instance" ON ( "logger_osmdata"."instance_id" = "logger_instance"."id" ) WHERE "logger_instance"."xform_id" = %s AND field_name = %s''',  # noqa
+            '''SELECT DISTINCT JSONB_OBJECT_KEYS(tags) as id FROM "logger_osmdata" INNER JOIN "logger_instance" ON ( "logger_osmdata"."instance_id" = "logger_instance"."id" ) WHERE "logger_instance"."xform_id" = %s AND field_name = %s''',  # noqa
             [xform.pk, field_path]
         )
         prefix = field_path + u':' if include_prefix else u''
