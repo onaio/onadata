68f33f85752ecc53ba5483dd20de71ee8a649f9f
Dennis Wambua <dennis.wambua@gmail.com>
2016-04-18 15:33:57 +0300

    DW: use get params in request

--- onadata/apps/main/google_export.py
+++ onadata/apps/main/google_export.py
@@ -10,18 +10,18 @@ from onadata.libs.utils.google import google_flow
 
 
 def google_auth_return(request):
-    if 'code' not in request.REQUEST:
+    if 'code' not in request.GET:
         return HttpResponse(u"Invalid Request")
     if request.user.is_authenticated():
 
         storage = Storage(TokenStorageModel, 'id', request.user,
                           'credential')
-        code = request.REQUEST.get('code')
+        code = request.GET.get('code')
         google_creds = google_flow.step2_exchange(code)
         google_creds.set_store(storage)
         storage.put(google_creds)
     else:
-        code = request.REQUEST.get('code')
+        code = request.GET.get('code')
         google_creds = google_flow.step2_exchange(code)
         request.session["access_token"] = google_creds.to_json()
     if request.session.get('google_redirect_url'):
