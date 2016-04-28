ba723479d28b350360c03a526e935d205c93ab1a
ivermac <mark.ekisa@gmail.com>
2016-04-28 14:04:01 +0300

    ME: added periodic task for google sheet service

--- onadata/apps/restservice/tasks.py
+++ onadata/apps/restservice/tasks.py
@@ -1,7 +1,18 @@
-from celery import task
-from onadata.apps.restservice.utils import call_service
+from datetime import timedelta
 
-from onadata.apps.logger.models.xform import XForm
+from celery import task
+# from celery.task.schedules import crontab
+from celery.decorators import periodic_task
+from celery.utils.log import get_task_logger
+
+from onadata.apps.restservice.utils import call_service
+from onadata.apps.restservice.models import RestService
+from onadata.apps.logger.models import (
+    Instance,
+    XForm
+)
+
+logger = get_task_logger(__name__)
 
 
 @task()
@@ -17,6 +28,33 @@ def call_service_async(instance_pk):
         pass
     else:
         call_service(instance)
+
+
+@periodic_task(
+    # Execute every 12 hours i.e twice a day
+    # run_every=(crontab(minute=0, hour='*/12')),
+    run_every=timedelta(seconds=30),
+    name="call_google_sheet_service ",
+    ignore_result=True
+)
+def call_google_sheet_service(instance_pk):
+    logger.info(">>> call google sheet service")
+    return
+    # lookup service
+    instance = Instance.objects.get(pk=instance_pk)
+    service = RestService.objects.filter(
+        xform_id=instance.xform_id, name='googlesheets'
+    ).first()
+
+    # call service send with url and data parameters
+    try:
+        service = service.get_service_definition()()
+        service.send(service.service_url, instance)
+        # TODO: check that the sent service is successful so that the instance
+        # bucket can be updated
+    except Exception:
+        # TODO: Handle gracefully | requeue/resend
+        pass
 
 
 @task()
