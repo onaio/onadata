07c8e321307da9e31d388680eb621632a3a296b0
ivermac <mark.ekisa@gmail.com>
2016-05-09 14:28:34 +0300

    ME: test that image error is handled by returning 400 status code

--- onadata/apps/api/tests/viewsets/test_media_viewset.py
+++ onadata/apps/api/tests/viewsets/test_media_viewset.py
@@ -1,3 +1,4 @@
+from mock import patch
 from onadata.apps.api.tests.viewsets.test_abstract_viewset import \
     TestAbstractViewSet
 from onadata.apps.api.viewsets.media_viewset import MediaViewSet
@@ -25,44 +26,52 @@ class TestMediaViewSet(TestAbstractViewSet):
 
     def test_retrieve_view(self):
         request = self.factory.get('/', {
-            'filename': self.attachment.media_file.name},  **self.extra)
+            'filename': self.attachment.media_file.name}, **self.extra)
         response = self.retrieve_view(request, self.attachment.pk)
         self.assertEqual(response.status_code, 302)
         self.assertTrue(response['Location'], attachment_url(self.attachment))
 
+    @patch('onadata.apps.api.viewsets.media_viewset.image_url')
+    def test_handle_image_exception(self, mock_image_url):
+        mock_image_url.side_effect = Exception()
+        request = self.factory.get(
+            '/',
+            {'filename': self.attachment.media_file.name, 'suffix': 'large'},
+            **self.extra
+        )
+        response = self.retrieve_view(request, self.attachment.pk)
+        self.assertEqual(response.status_code, 400)
+
     def test_retrieve_view_small(self):
-        request = self.factory.get('/', {
-            'filename': self.attachment.media_file.name,
-            'suffix': 'small'
-        },  **self.extra)
+        request = self.factory.get(
+            '/',
+            {'filename': self.attachment.media_file.name, 'suffix': 'small'},
+            **self.extra
+        )
         response = self.retrieve_view(request, self.attachment.pk)
         self.assertEqual(response.status_code, 302)
         self.assertTrue(response['Location'],
                         attachment_url(self.attachment, 'small'))
 
     def test_retrieve_view_invalid_suffix(self):
-        request = self.factory.get('/',
-                                   {
-                                       'filename':
-                                           self.attachment.media_file.name,
-                                       'suffix': 'TK'
-                                   },
-                                   **self.extra)
+        request = self.factory.get(
+            '/',
+            {'filename': self.attachment.media_file.name, 'suffix': 'TK'},
+            **self.extra
+        )
         response = self.retrieve_view(request, self.attachment.pk)
         self.assertEqual(response.status_code, 404)
 
     def test_retrieve_view_invalid_pk(self):
-        request = self.factory.get('/',
-                                   {
-                                       'filename':
-                                           self.attachment.media_file.name,
-                                       'suffix': 'small'
-                                   },
-                                   **self.extra)
-        response = self.retrieve_view(request,  'INVALID')
+        request = self.factory.get(
+            '/',
+            {'filename': self.attachment.media_file.name, 'suffix': 'small'},
+            **self.extra
+        )
+        response = self.retrieve_view(request, 'INVALID')
         self.assertEqual(response.status_code, 404)
 
     def test_retrieve_view_no_filename_param(self):
-        request = self.factory.get('/',  **self.extra)
+        request = self.factory.get('/', **self.extra)
         response = self.retrieve_view(request, self.attachment.pk)
         self.assertEqual(response.status_code, 404)
