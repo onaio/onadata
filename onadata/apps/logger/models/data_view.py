a46e6cfb412aaa4500d9dacc1b56e05092b239e2
pld <peetldee@gmail.com>
2016-05-24 10:37:15 +0300

    PLD: move sorting utils to a sorting lib

--- onadata/apps/logger/models/data_view.py
+++ onadata/apps/logger/models/data_view.py
@@ -1,6 +1,4 @@
 import datetime
-import json
-import six
 
 from django.utils.translation import ugettext as _
 from django.contrib.gis.db import models
@@ -10,6 +8,8 @@ from django.db.models.signals import post_delete, post_save
 
 from onadata.apps.logger.models.xform import XForm
 from onadata.apps.logger.models.project import Project
+from onadata.libs.models.sorting import (
+    json_order_by, json_order_by_params, sort_from_mongo_sort_str)
 from onadata.libs.utils.common_tags import (
     ATTACHMENTS,
     EDITED,
@@ -81,51 +81,6 @@ def has_attachments_fields(data_view):
                     return True
 
     return False
-
-
-def sort_from_mongo_sort_str(sort_str):
-    sort_values = []
-    if isinstance(sort_str, six.string_types):
-        if sort_str.startswith('{'):
-            sort_dict = json.loads(sort_str)
-            for k, v in sort_dict.items():
-                try:
-                    v = int(v)
-                except ValueError:
-                    pass
-                if v < 0:
-                    k = u'-{}'.format(k)
-                sort_values.append(k)
-        else:
-            sort_values.append(sort_str)
-
-    return sort_values
-
-
-def json_order_by(sort_list):
-    _list = []
-
-    for field in sort_list:
-        _str = u" json->>%s"
-        if field.startswith('-'):
-            _str += u" DESC"
-        else:
-            _str += u" ASC"
-        _list.append(_str)
-
-    if len(_list) > 0:
-        return u"ORDER BY {}".format(u",".join(_list))
-
-    return u""
-
-
-def json_order_by_params(sort_list):
-    params = []
-
-    for field in sort_list:
-        params.append(field.lstrip('-'))
-
-    return params
 
 
 class DataView(models.Model):
