7152a3177c38169dabafe46f8d4c702b8f3107e5
Ukang'a Dickson <ukanga@gmail.com>
2016-07-07 15:31:32 +0300

    do not set etags for list endpoints, we will use last modified instead

--- onadata/libs/mixins/etags_mixin.py
+++ onadata/libs/mixins/etags_mixin.py
@@ -1,41 +1,9 @@
-import json
-import types
-
 from django.utils.timezone import now
 from hashlib import md5
 
 MODELS_WITH_DATE_MODIFIED = ('XForm', 'Instance', 'Project', 'Attachment',
                              'MetaData', 'Note', 'OrganizationProfile',
                              'UserProfile', 'Team')
-
-
-def get_etag_value(obj, object_list):
-    etag_value = '{}'.format(now())
-
-    if isinstance(object_list, list):
-        etag_value = json.dumps([
-            '{}'.format(
-                o.date_modified if hasattr(o, 'date_modified') else o.pk
-            ) for o in object_list
-        ])
-    elif object_list.model.__name__ in MODELS_WITH_DATE_MODIFIED:
-        object_list = object_list
-
-        if object_list.query.can_filter():
-            object_list = \
-                object_list.order_by('-date_modified')
-
-        etag_value = json.dumps([
-            '{}'.format(o) for o in object_list.values_list(
-                'date_modified', flat=True)
-        ])
-    elif obj:
-        etag_value = json.dumps([
-            '{}'.format(o) for o in object_list.values_list(
-                'pk', flat=True)
-            ])
-
-    return etag_value
 
 
 class ETagsMixin(object):
@@ -48,14 +16,11 @@ class ETagsMixin(object):
 
     def finalize_response(self, request, response, *args, **kwargs):
         if request.method == 'GET' and not response.streaming and \
-                response.status_code in [200, 201, 202]:
-            etag_value = obj = None
+                response.status_code in [200, 201, 202] and \
+                not hasattr(self, 'object_list'):
+            etag_value = None
             if hasattr(self, 'etag_data') and self.etag_data:
                 etag_value = str(self.etag_data)
-            elif hasattr(self, 'object_list'):
-                if not isinstance(self.object_list, types.GeneratorType) and \
-                        self.object_list:
-                    etag_value = get_etag_value(obj, self.object_list)
             elif hasattr(self, 'object'):
                 if self.object.__class__.__name__ in MODELS_WITH_DATE_MODIFIED:
                     etag_value = self.object.date_modified
