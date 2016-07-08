371c3832be3ff6875e80922e383aa4ae6594b3e8
Dennis Wambua <dennis.wambua@gmail.com>
2016-07-08 17:33:12 +0300

    DW: handle flow exchange error

--- onadata/apps/main/google_export.py
+++ onadata/apps/main/google_export.py
@@ -3,10 +3,12 @@ from oauth2client.contrib.django_orm import Storage
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.http import HttpResponseBadRequest
 
 from onadata.apps.main.models import TokenStorageModel
 from onadata.apps.main.views import home
 from onadata.libs.utils.google import google_flow
+from oauth2client.client import FlowExchangeError
 
 
 def google_auth_return(request):
@@ -17,7 +19,10 @@ def google_auth_return(request):
         storage = Storage(TokenStorageModel, 'id', request.user,
                           'credential')
         code = request.GET.get('code')
-        google_creds = google_flow.step2_exchange(code)
+        try:
+            google_creds = google_flow.step2_exchange(code)
+        except FlowExchangeError as e:
+            return HttpResponseBadRequest(str(e))
         google_creds.set_store(storage)
         storage.put(google_creds)
     else:
