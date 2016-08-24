0c0e9e7fafed2545d3ad0bc68f80b4b5724bffd3
hngenda <hngenda@ona.io>
2016-08-24 15:05:44 +0300

    Fixed validation exception by adding length check validation on the serializer
    Signed-off-by: hngenda <hngenda@ona.io>

--- onadata/apps/api/tests/viewsets/test_user_profile_viewset.py
+++ onadata/apps/api/tests/viewsets/test_user_profile_viewset.py
@@ -650,3 +650,23 @@ class TestUserProfileViewSet(TestAbstractViewSet):
         response = self.view(request, user=user.username)
 
         self.assertEqual(response.status_code, 400)
+
+
+    def test_profile_create_fails_with_long_first_and_last_names(self):
+        data = {
+            'username': u'machicimo',
+            'email': u'mike@columbia.edu',
+            'city': u'Denoville',
+            'country': u'US',
+            'last_name':
+                u'undeomnisistenatuserrorsitvoluptatem',
+            'first_name':
+                u'quirationevoluptatemsequinesciunt'
+        }
+        request = self.factory.post(
+            '/api/v1/profiles', data=json.dumps(data),
+            content_type="application/json", **self.extra)
+        response = self.view(request)
+        self.assertIsNotNone(response.data['first_name'])
+        self.assertIsNotNone(response.data['last_name'])
+        self.assertEqual(response.status_code, 400)
