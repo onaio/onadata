cef75d2d03dac446f5e16f94728123a93a41f94a
hngenda <hngenda@ona.io>
2016-09-06 17:21:40 +0300

    Added export listing by form id
    Signed-off-by: hngenda <hngenda@ona.io>

--- onadata/libs/serializers/export_serializer.py
+++ onadata/libs/serializers/export_serializer.py
@@ -3,6 +3,20 @@ from onadata.apps.viewer.models.export import Export
 
 
 class ExportSerializer(serializers.HyperlinkedModelSerializer):
+    status = serializers.SerializerMethodField()
+    type = serializers.SerializerMethodField()
+
     class Meta:
         model = Export
-        fields = ('id', 'xform')
+        fields = ('id', 'status', 'type', 'task_id', 'xform')
+
+    def get_status(self, obj):
+        if obj.internal_status == Export.PENDING:
+            return 'PENDING'
+        elif obj.internal_status == Export.SUCCESSFUL:
+            return 'SUCCESSFUL'
+        else:
+            return 'FAILED'
+
+    def get_type(self, obj):
+        return obj.export_type
