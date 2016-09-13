196b0a41905f420f6d91bea7bebd2219f7517ebd
hngenda <hngenda@ona.io>
2016-09-13 15:31:54 +0300

    Refactored status message to be consistent
    Signed-off-by: hngenda <hngenda@ona.io>

--- onadata/libs/serializers/export_serializer.py
+++ onadata/libs/serializers/export_serializer.py
@@ -1,22 +1,25 @@
 from rest_framework import serializers
 from onadata.apps.viewer.models.export import Export
+from onadata.libs.utils.api_export_tools import (EXPORT_FAILED,
+                                                 EXPORT_SUCCESS,
+                                                 EXPORT_PENDING)
 
 
 class ExportSerializer(serializers.HyperlinkedModelSerializer):
-    status = serializers.SerializerMethodField()
+    job_status = serializers.SerializerMethodField()
     type = serializers.SerializerMethodField()
 
     class Meta:
         model = Export
-        fields = ('id', 'status', 'type', 'task_id', 'xform')
+        fields = ('id', 'job_status', 'type', 'task_id', 'xform')
 
-    def get_status(self, obj):
+    def get_job_status(self, obj):
         if obj.internal_status == Export.PENDING:
-            return 'PENDING'
+            return EXPORT_PENDING
         elif obj.internal_status == Export.SUCCESSFUL:
-            return 'SUCCESSFUL'
+            return EXPORT_SUCCESS
         else:
-            return 'FAILED'
+            return EXPORT_FAILED
 
     def get_type(self, obj):
         return obj.export_type
