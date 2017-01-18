60dad7d95d05335bb7b0b443e4e7982aff73471c
ivermac <mark.ekisa@gmail.com>
2017-01-18 16:33:53 +0300

    ME: sort lists being compared

--- onadata/apps/api/tests/viewsets/test_user_profile_viewset.py
+++ onadata/apps/api/tests/viewsets/test_user_profile_viewset.py
@@ -75,8 +75,8 @@ class TestUserProfileViewSet(TestAbstractViewSet):
         })
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual([dict(d) for d in response.data],
-                         [self.user_profile_data(), deno_profile_data])
+        self.assertEqual(sorted([dict(d) for d in response.data]),
+                         sorted([self.user_profile_data(), deno_profile_data]))
         self.assertEqual(len(response.data), 2)
 
     def test_user_profile_list_with_and_without_users_param(self):
