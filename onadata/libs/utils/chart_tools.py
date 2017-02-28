680c9babb51b97776a74162c9e4e6ac8df916d18
Dickson Ukang'a <ukanga@gmail.com>
2017-02-28 11:50:22 +0300

    flake8 cleanup

--- onadata/libs/utils/chart_tools.py
+++ onadata/libs/utils/chart_tools.py
@@ -1,23 +1,23 @@
 import re
-import six
 
+import six
 from django.db.utils import DataError
 from django.http import Http404
-
-from onadata.libs.data.query import \
-    get_form_submissions_aggregated_by_select_one, \
-    get_form_submissions_grouped_by_field, \
-    get_form_submissions_grouped_by_select_one
-from onadata.libs.utils import common_tags
-
-from onadata.apps.logger.models.xform import XForm
-from onadata.apps.logger.models.data_view import DataView
 from rest_framework.exceptions import ParseError
 
+from onadata.apps.logger.models.data_view import DataView
+from onadata.apps.logger.models.xform import XForm
+from onadata.libs.data.query import (
+    get_form_submissions_aggregated_by_select_one,
+    get_form_submissions_grouped_by_field,
+    get_form_submissions_grouped_by_select_one)
+from onadata.libs.utils import common_tags
 
 # list of fields we can chart
-CHART_FIELDS = ['select one', 'integer', 'decimal', 'date', 'datetime',
-                'start', 'end', 'today']
+CHART_FIELDS = [
+    'select one', 'integer', 'decimal', 'date', 'datetime', 'start', 'end',
+    'today'
+]
 # numeric, categorized
 DATA_TYPE_MAP = {
     'integer': 'numeric',
@@ -32,17 +32,14 @@ DATA_TYPE_MAP = {
 
 FIELD_DATA_MAP = {
     common_tags.SUBMISSION_TIME:
-        ('Submission Time', '_submission_time', 'datetime'),
-    common_tags.SUBMITTED_BY:
-        ('Submission By', '_submitted_by', 'text'),
-    common_tags.DURATION:
-        ('Duration', '_duration', 'integer')
+    ('Submission Time', '_submission_time', 'datetime'),
+    common_tags.SUBMITTED_BY: ('Submission By', '_submitted_by', 'text'),
+    common_tags.DURATION: ('Duration', '_duration', 'integer')
 }
 
 CHARTS_PER_PAGE = 20
 
 POSTGRES_ALIAS_LENGTH = 63
-
 
 timezone_re = re.compile(r'(.+)\+(\d+)')
 
@@ -117,8 +114,9 @@ def get_choice_label(choices, string):
             labels.append(label)
         else:
             # Try to get labels by splitting the string
-            labels = [find_choice_label(choices, name)
-                      for name in string.split(" ")]
+            labels = [
+                find_choice_label(choices, name) for name in string.split(" ")
+            ]
 
             # If any split string does not have a label it is not a multiselect
             # but a missing label, use string
@@ -134,34 +132,42 @@ def _flatten_multiple_dict_into_one(field_name, group_by_name, data):
     # truncate field name to 63 characters to fix #354
     truncated_field_name = field_name[0:POSTGRES_ALIAS_LENGTH]
     truncated_group_by_name = group_by_name[0:POSTGRES_ALIAS_LENGTH]
-    final = [{truncated_field_name: b, 'items': []}
-             for b in list({a.get(truncated_field_name) for a in data})]
+    final = [{
+        truncated_field_name: b,
+        'items': []
+    } for b in list({a.get(truncated_field_name)
+                     for a in data})]
 
     for a in data:
         for b in final:
             if a.get(truncated_field_name) == b.get(truncated_field_name):
                 b['items'].append({
-                    truncated_group_by_name: a.get(truncated_group_by_name),
-                    'count': a.get('count')
-                    })
+                    truncated_group_by_name:
+                    a.get(truncated_group_by_name),
+                    'count':
+                    a.get('count')
+                })
 
     return final
 
 
-def _use_labels_from_field_name(field_name, field, data_type, data,
+def _use_labels_from_field_name(field_name,
+                                field,
+                                data_type,
+                                data,
                                 choices=None):
     # truncate field name to 63 characters to fix #354
     truncated_name = field_name[0:POSTGRES_ALIAS_LENGTH]
     truncated_name = truncated_name.encode('utf-8')
 
-    if data_type == 'categorized'and field_name != common_tags.SUBMITTED_BY:
+    if data_type == 'categorized' and field_name != common_tags.SUBMITTED_BY:
         if data:
             if field.children:
                 choices = field.children
 
             for item in data:
-                item[truncated_name] = get_choice_label(
-                    choices, item[truncated_name])
+                item[truncated_name] = get_choice_label(choices,
+                                                        item[truncated_name])
 
     # replace truncated field names in the result set with the field name key
     field_name = field_name.encode('utf-8')
@@ -169,12 +175,15 @@ def _use_labels_from_field_name(field_name, field, data_type, data,
     for item in data:
         if field_name != truncated_name:
             item[field_name] = item[truncated_name]
-            del(item[truncated_name])
+            del (item[truncated_name])
 
     return data
 
 
-def _use_labels_from_group_by_name(field_name, field, data_type, data,
+def _use_labels_from_group_by_name(field_name,
+                                   field,
+                                   data_type,
+                                   data,
                                    choices=None):
     # truncate field name to 63 characters to fix #354
     truncated_name = field_name[0:POSTGRES_ALIAS_LENGTH]
@@ -188,8 +197,8 @@ def _use_labels_from_group_by_name(field_name, field, data_type, data,
             for item in data:
                 if 'items' in item:
                     for i in item.get('items'):
-                        i[truncated_name] = get_choice_label(
-                            choices, i[truncated_name])
+                        i[truncated_name] = get_choice_label(choices,
+                                                             i[truncated_name])
                 else:
                     item[truncated_name] = \
                         get_choice_label(choices, item[truncated_name])
@@ -202,17 +211,21 @@ def _use_labels_from_group_by_name(field_name, field, data_type, data,
             for i in item.get('items'):
                 if field_name != truncated_name:
                     i[field_name] = i[truncated_name]
-                    del(i[truncated_name])
+                    del (i[truncated_name])
         else:
             if field_name != truncated_name:
                 item[field_name] = item[truncated_name]
-                del(item[truncated_name])
+                del (item[truncated_name])
 
     return data
 
 
-def build_chart_data_for_field(xform, field, language_index=0, choices=None,
-                               group_by=None, data_view=None):
+def build_chart_data_for_field(xform,
+                               field,
+                               language_index=0,
+                               choices=None,
+                               group_by=None,
+                               data_view=None):
     # check if its the special _submission_time META
     if isinstance(field, basestring):
         field_label, field_xpath, field_type = FIELD_DATA_MAP.get(field)
@@ -242,8 +255,7 @@ def build_chart_data_for_field(xform, field, language_index=0, choices=None,
             result = get_form_submissions_grouped_by_select_one(
                 xform, field_xpath, group_by_name, field_name, data_view)
 
-            result = _flatten_multiple_dict_into_one(field_name,
-                                                     group_by_name,
+            result = _flatten_multiple_dict_into_one(field_name, group_by_name,
                                                      result)
 
         elif field_type in common_tags.NUMERIC_LIST \
@@ -253,18 +265,21 @@ def build_chart_data_for_field(xform, field, language_index=0, choices=None,
         else:
             raise ParseError(u'Cannot group by %s' % group_by_name)
     else:
-        result = get_form_submissions_grouped_by_field(
-            xform, field_xpath, field_name, data_view)
+        result = get_form_submissions_grouped_by_field(xform, field_xpath,
+                                                       field_name, data_view)
 
-    result = _use_labels_from_field_name(field_name, field, data_type, result,
-                                         choices=choices)
+    result = _use_labels_from_field_name(
+        field_name, field, data_type, result, choices=choices)
 
     if group_by and not isinstance(group_by, six.string_types):
         group_by_data_type = DATA_TYPE_MAP.get(group_by.type, 'categorized')
         grp_choices = get_field_choices(group_by, xform)
-        result = _use_labels_from_group_by_name(group_by_name, group_by,
-                                                group_by_data_type, result,
-                                                choices=grp_choices)
+        result = _use_labels_from_group_by_name(
+            group_by_name,
+            group_by,
+            group_by_data_type,
+            result,
+            choices=grp_choices)
 
     if not group_by:
         result = sorted(result, key=lambda d: d['count'])
@@ -305,8 +320,8 @@ def calculate_ranges(page, items_per_page, total_items):
 def build_chart_data(xform, language_index=0, page=0):
     # only use chart-able fields
 
-    fields = filter(
-        lambda f: f.type in CHART_FIELDS, [e for e in xform.survey_elements])
+    fields = filter(lambda f: f.type in CHART_FIELDS,
+                    [e for e in xform.survey_elements])
 
     # prepend submission time
     fields[:0] = [common_tags.SUBMISSION_TIME]
@@ -315,8 +330,10 @@ def build_chart_data(xform, language_index=0, page=0):
     start, end = calculate_ranges(page, CHARTS_PER_PAGE, len(fields))
     fields = fields[start:end]
 
-    return [build_chart_data_for_field(xform, field, language_index)
-            for field in fields]
+    return [
+        build_chart_data_for_field(xform, field, language_index)
+        for field in fields
+    ]
 
 
 def build_chart_data_from_widget(widget, language_index=0):
@@ -335,13 +352,12 @@ def build_chart_data_from_widget(widget, language_index=0):
         field = common_tags.SUBMISSION_TIME
     else:
         # use specified field to get summary
-        fields = filter(
-            lambda f: f.name == field_name,
-            [e for e in xform.survey_elements])
+        fields = filter(lambda f: f.name == field_name,
+                        [e for e in xform.survey_elements])
 
         if len(fields) == 0:
-            raise ParseError(
-                "Field %s does not not exist on the form" % field_name)
+            raise ParseError("Field %s does not not exist on the form" %
+                             field_name)
 
         field = fields[0]
     choices = xform.survey.get('choices')
@@ -367,13 +383,12 @@ def get_field_from_field_name(field_name, xform):
         field = common_tags.DURATION
     else:
         # use specified field to get summary
-        fields = filter(
-            lambda f: f.name == field_name,
-            [e for e in xform.survey_elements])
+        fields = filter(lambda f: f.name == field_name,
+                        [e for e in xform.survey_elements])
 
         if len(fields) == 0:
-            raise Http404(
-                "Field %s does not not exist on the form" % field_name)
+            raise Http404("Field %s does not not exist on the form" %
+                          field_name)
 
         field = fields[0]
 
@@ -390,13 +405,12 @@ def get_field_from_field_xpath(field_xpath, xform):
         field = common_tags.DURATION
     else:
         # use specified field to get summary
-        fields = filter(
-            lambda f: f.get_abbreviated_xpath() == field_xpath,
-            [e for e in xform.survey_elements])
+        fields = filter(lambda f: f.get_abbreviated_xpath() == field_xpath,
+                        [e for e in xform.survey_elements])
 
         if len(fields) == 0:
-            raise Http404(
-                "Field %s does not not exist on the form" % field_xpath)
+            raise Http404("Field %s does not not exist on the form" %
+                          field_xpath)
 
         field = fields[0]
 
@@ -415,8 +429,12 @@ def get_field_label(field, language_index=0):
     return field_label
 
 
-def get_chart_data_for_field(field_name, xform, accepted_format, group_by,
-                             field_xpath=None, data_view=None):
+def get_chart_data_for_field(field_name,
+                             xform,
+                             accepted_format,
+                             group_by,
+                             field_xpath=None,
+                             data_view=None):
     """
     Get chart data for a given xlsform field.
     """
@@ -435,7 +453,10 @@ def get_chart_data_for_field(field_name, xform, accepted_format, group_by,
 
     try:
         data = build_chart_data_for_field(
-            xform, field, choices=choices, group_by=group_by,
+            xform,
+            field,
+            choices=choices,
+            group_by=group_by,
             data_view=data_view)
     except DataError as e:
         raise ParseError(unicode(e))
@@ -447,8 +468,6 @@ def get_chart_data_for_field(field_name, xform, accepted_format, group_by,
                 if isinstance(item[field_name], list):
                     item[field_name] = u', '.join(item[field_name])
 
-        data.update({
-            'xform': xform
-        })
+        data.update({'xform': xform})
 
     return data
