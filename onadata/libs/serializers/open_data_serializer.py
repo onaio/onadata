3b971746c84545215d05b4d04b637e59535d3aba
ivermac <mark.ekisa@gmail.com>
2017-03-03 15:35:27 +0300

    moved get_data to serializer

--- onadata/libs/serializers/open_data_serializer.py
+++ onadata/libs/serializers/open_data_serializer.py
@@ -1,3 +1,5 @@
+import collections
+
 from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 
@@ -5,9 +7,41 @@ from onadata.apps.logger.models import OpenData, XForm
 from django.shortcuts import get_object_or_404
 
 
+def get_data(request_data, update=False):
+    '''
+    return a namedtuple with error, message and data values.
+    '''
+    fields = ['object_id', 'data_type', 'name']
+    results = collections.namedtuple('results', 'error message data')
+    if not update:
+        if not set(fields).issubset(request_data.keys()):
+            return results(
+                error=True,
+                message="Fields object_id, data_type and name are required.",
+                data=None
+            )
+
+    fields.append('active')
+
+    # check if invalid fields are provided
+    if any(a not in fields for a in request_data.keys()):
+        return results(
+            error=True,
+            message="Valid fields are object_id, data_type and name.",
+            data=None
+        )
+
+    data = {}
+    for key in fields:
+        available = request_data.get(key) is not None
+        available and data.update({key: request_data.get(key)})
+
+    return results(error=False, message=None, data=data)
+
+
 class OpenDataSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=255, required=True)
-    clazz = serializers.CharField(max_length=50, required=False)
+    data_type = serializers.CharField(max_length=50, required=False)
     object_id = serializers.IntegerField(required=False)
     active = serializers.BooleanField(required=False)
 
