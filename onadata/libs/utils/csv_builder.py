7ec250730e9c38e8f5b77c1791e18b192a6ebac2
ivermac <mark.ekisa@gmail.com>
2017-03-04 07:16:31 +0300

    initialize pattern and replacer variables

--- onadata/libs/utils/csv_builder.py
+++ onadata/libs/utils/csv_builder.py
@@ -473,6 +473,7 @@ class CSVDataFrameBuilder(AbstractDataFrameBuilder):
                               record, self.dd, include_images=image_xpaths)
 
     def _format_for_dataframe(self, cursor, key_replacement_obj=None):
+        pattern, replacer = None, None
         if key_replacement_obj:
             pattern = key_replacement_obj.get('pattern')
             replacer = key_replacement_obj.get('replacer')
