a3ce681baee87e4292d954c185175cff31e42ef6
Dickson Ukang'a <ukanga@gmail.com>
2017-04-10 19:53:27 +0300

    management command textit_v1_to_v2 to convert v1 urls to v2 urls for starting flows

---
+++ onadata/apps/restservice/management/commands/textit_v1_to_v2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import re
+
+from django.core.management.base import BaseCommand
+from django.utils.translation import ugettext as _
+
+from onadata.apps.restservice.models import RestService
+from onadata.libs.utils.common_tags import TEXTIT
+
+
+class Command(BaseCommand):
+    help = _("Migrate TextIt/RapidPro v1 URLS to v2 URLS.")
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--apply', default=False, help=_("Apply changes to database."))
+
+    def handle(self, *args, **options):
+        services = RestService.objects.filter(name=TEXTIT)
+        force = options.get('apply')
+        if force and force.lower() != 'true':
+            self.stderr.write("--apply expects 'true' as a parameter value.")
+
+            return
+
+        v1 = re.compile(r'\/v1/runs')
+        v2 = '/v2/flow_starts'
+
+        for service in services:
+            if v1.findall(service.service_url):
+                original = service.service_url
+                new_uri = re.sub(v1, v2, service.service_url)
+                if force.lower() == 'true':
+                    service.service_url = new_uri
+                    service.save()
+                    self.stdout.write(
+                        _("Changed %s to %s" % (original, new_uri)))
+                else:
+                    self.stdout.write(
+                        _("Will change %s to %s" % (original, new_uri)))
