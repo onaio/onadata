9569665a1c4262b443a93df757ff4d4b7ecbb26d
ivermac <mark.ekisa@gmail.com>
2017-06-16 15:19:30 +0300

    fix typo

--- onadata/apps/api/viewsets/open_data_viewset.py
+++ onadata/apps/api/viewsets/open_data_viewset.py
@@ -220,7 +220,6 @@ class OpenDataViewSet(ETagsMixin, CacheControlMixin, TotalHeaderMixin,
                         status=status.HTTP_200_OK)
             else:
                 raise PermissionDenied(
-                    _((u"You do not haveYou do not have permission "
-                       "to perform this action.")))
+                    _((u"You do not have permission to perform this action.")))
 
         return Response(status=status.HTTP_404_NOT_FOUND)
