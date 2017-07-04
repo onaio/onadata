3082245526c3c1e0c8a32fa23f229d6b56911082
Dickson Ukang'a <ukanga@gmail.com>
2017-07-04 09:28:17 +0300

    XForm whitespace cleanup should leave a whitespace between output variable
    matches to recent changes in pyxform

--- onadata/apps/logger/models/xform.py
+++ onadata/apps/logger/models/xform.py
@@ -215,13 +215,13 @@ class XFormMixin(object):
         # hack
         # http://ronrothman.com/public/leftbraned/xml-dom-minidom-toprettyxml-\
         # and-silly-whitespace/
-        text_re = re.compile('>\n\s+([^<>\s].*?)\n\s+</', re.DOTALL)
+        text_re = re.compile('(>)\n\s*(\s[^<>\s].*?)\n\s*(\s</)', re.DOTALL)
         output_re = re.compile('\n.*(<output.*>)\n(  )*')
-        prettyXml = text_re.sub('>\g<1></', self.xml)
-        inlineOutput = output_re.sub('\g<1>', prettyXml)
-        inlineOutput = re.compile('<label>\s*\n*\s*\n*\s*</label>').sub(
-            '<label></label>', inlineOutput)
-        self.xml = inlineOutput
+        pretty_xml = text_re.sub(lambda m: ''.join(m.group(1, 2, 3)), self.xml)
+        inline_output = output_re.sub('\g<1>', pretty_xml)
+        inline_output = re.compile('<label>\s*\n*\s*\n*\s*</label>').sub(
+            '<label></label>', inline_output)
+        self.xml = inline_output
 
     class Meta:
         app_label = "viewer"
