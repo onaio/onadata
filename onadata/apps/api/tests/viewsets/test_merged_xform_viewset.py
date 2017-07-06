09d9bf1cfbf9bdbbdc24089107c4d1ac0e932133
Ona KE code machine <tech+ke@ona.io>
2017-07-06 18:26:52 +0300

    NM/UK: Test merged datasets get list and retrieve
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- onadata/apps/api/tests/viewsets/test_merged_xform_viewset.py
+++ onadata/apps/api/tests/viewsets/test_merged_xform_viewset.py
@@ -19,7 +19,7 @@ class TestMergedXFormViewSet(TestAbstractViewSet):
     def setUp(self):
         super(self.__class__, self).setUp()
 
-    def test_create_merged_dataset(self):
+    def _create_merged_dataset(self):
         view = MergedXFormViewSet.as_view({'post': 'create', })
         xform1 = self._publish_md(MD, self.user, id_string='a')
         xform2 = self._publish_md(MD, self.user, id_string='b')
@@ -36,6 +36,46 @@ class TestMergedXFormViewSet(TestAbstractViewSet):
         request = self.factory.post('/', data=data)
         response = view(request)
         self.assertEqual(response.status_code, 201)
+        self.assertIn('id', response.data)
+        self.assertIn('title', response.data)
+        self.assertIn('xforms', response.data)
+
+        return response.data
+
+    def test_create_merged_dataset(self):
+        self._create_merged_dataset()
+
+    def test_merged_datasets_list(self):
+        view = MergedXFormViewSet.as_view({'get': 'list', })
+        request = self.factory.get('/')
+
+        # Empty list when there are no merged datasets
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.data, list)
+        self.assertEqual([], response.data)
+
+        # A list containing the merged datasets
+        merged_dataset = self._create_merged_dataset()
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.data, list)
+        self.assertIn(merged_dataset, response.data)
+
+    def test_merged_datasets_retrieve(self):
+        merged_dataset = self._create_merged_dataset()
+        view = MergedXFormViewSet.as_view({'get': 'retrieve', })
+        request = self.factory.get('/')
+
+        # status_code is 404 when the pk doesn't exist
+        response = view(request, pk=(1000 * merged_dataset['id']))
+        self.assertEqual(response.status_code, 404)
+
+        # status_code is 200 when the pk exists
+        response = view(request, pk=merged_dataset['id'])
+        self.assertEqual(response.status_code, 200)
+
+        # data has expected fields
         self.assertIn('id', response.data)
         self.assertIn('title', response.data)
         self.assertIn('xforms', response.data)
