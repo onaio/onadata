6a2c7dc9dc78508a78d8d9f7b034c687186b3d04
Dickson Ukang'a <ukanga@gmail.com>
2017-07-22 17:19:38 +0300

    add raven configuration for celery module

--- onadata/celery.py
+++ onadata/celery.py
@@ -2,13 +2,29 @@ from __future__ import absolute_import
 
 import os
 
+import celery
 from django.conf import settings
-from celery import Celery
+
+import raven
+from raven.contrib.celery import register_logger_signal, register_signal
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onadata.settings.common')
 
-app = Celery('onadata.celery')
+
+class Celery(celery.Celery):
+    def on_configure(self):
+        if getattr(settings, 'RAVEN_CONFIG', None):
+            client = raven.Client(settings.RAVEN_CONFIG['dsn'])
+
+            # register a custom filter to filter out duplicate logs
+            register_logger_signal(client)
+
+            # hook into the Celery error handler
+            register_signal(client)
+
+
+app = Celery(__name__)
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
