fd8b91ecae4fa1f1c4dfd3b64975cdb3c4f5b6eb
Dickson Ukang'a <ukanga@gmail.com>
2017-08-14 00:10:25 +0300

    refactor get_or_create_opendata() as a helper function

--- onadata/apps/logger/models/open_data.py
+++ onadata/apps/logger/models/open_data.py
@@ -1,11 +1,21 @@
-from django.contrib.gis.db import models
+# -*- coding: utf-8 -*-
+"""
+OpenData model represents a way to access private datasets without
+authentication using the unique uuid.
+"""
+
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.gis.db import models
 
 from onadata.libs.utils.common_tools import getUUID
 
 
 class OpenData(models.Model):
+    """
+    OpenData model represents a way to access private datasets without
+    authentication using the unique uuid.
+    """
     name = models.CharField(max_length=255)
     uuid = models.CharField(max_length=32, default=getUUID, unique=True)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
@@ -21,3 +31,22 @@ class OpenData(models.Model):
 
     class Meta:
         app_label = 'logger'
+
+
+def get_or_create_opendata(xform):
+    """
+    Looks up an OpenData object with the given xform, creates one if it does
+    not exist.
+    Returns a tuple of (object, created), where created is a boolean specifing
+    whether an object was created.
+    """
+    content_type = ContentType.objects.get_for_model(xform)
+
+    return OpenData.objects.get_or_create(
+        object_id=xform.id,
+        defaults={
+            'name': xform.id_string,
+            'content_type': content_type,
+            'content_object': xform,
+        }
+    )
