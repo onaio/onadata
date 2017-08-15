92df14b83398ce996c8739af73ff2f00d35eacb8
Kelvin Jayanoris <kjayanoris@ona.io>
2017-08-15 06:07:30 +0000

    Keep track of whether all attachments have been submitted
    Fixes: #1049
    
    Add new fields to the model instance to keep track of:
    
    - How many attachments, if any are expected in a submission
    - How many attachments have been actually received
    - Whether all attachments have been received

--- onadata/libs/utils/logger_tools.py
+++ onadata/libs/utils/logger_tools.py
@@ -224,6 +224,15 @@ def save_attachments(xform, instance, media_files):
             extension=extension)
     update_instance_attachment_tracking(instance)
 
+    instance.total_media = len([m for m in instance.get_dict().keys() if m in
+                                xform.get_media_survey_xpaths()])
+    instance.media_count = instance.attachments.count()
+    instance.media_all_received = instance.media_count == \
+        instance.total_media
+    instance.save(update_fields=['total_media',
+                                 'media_count',
+                                 'media_all_received'])
+
 
 def save_submission(xform, xml, media_files, new_uuid, submitted_by, status,
                     date_created_override):
