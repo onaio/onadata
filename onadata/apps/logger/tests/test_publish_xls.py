6f6f9ca0ed7f4bd74e890bc13c7aea44fb8e226b
Kelvin Jayanoris <kjayanoris@ona.io>
2017-09-23 06:36:08 +0000

    Test that hash is created and updated successfully

--- onadata/apps/logger/tests/test_publish_xls.py
+++ onadata/apps/logger/tests/test_publish_xls.py
@@ -1,9 +1,11 @@
 import os
 import sys
 
+from django.contrib.auth.models import User
 from django.core.management import call_command
 from django.core.management.base import CommandError
 
+from onadata.apps.main.models import UserProfile
 from onadata.apps.main.tests.test_base import TestBase
 from onadata.apps.logger.models.xform import XForm
 from onadata.libs.utils.common_tools import report_exception
@@ -45,6 +47,39 @@ class TestPublishXLS(TestBase):
         is_updated_form = len([e.name for e in self.xform.survey_elements
                                if e.name == u'preferred_means']) > 0
         self.assertTrue(is_updated_form)
+
+    def test_xform_hash(self):
+        md = """
+        | survey |       |        |       |
+        |        | type  | name   | label |
+        |        | image | image1 | Photo |
+        """
+        self._create_user_and_login()
+        self.xform = self._publish_markdown(md, self.user)
+        # make sure the has is created and is not empty
+        self.assertFalse(self.xform.hash == "" or self.xform.hash is None)
+        self.assertEqual(self.xform.hash, self.xform.get_hash())
+        old_hash = self.xform.hash
+        # publish a second form
+        md2 = """
+        | survey |                        |          |           |
+        |        | type                   | name     | label     |
+        |        | select_multiple yes_no | expensed | Expensed? |
+
+        | choices |           |      |       |
+        |         | list name | name | label |
+        |         | yes_no    | 1    | Yes   |
+        |         | yes_no    | 09   | No    |
+        """
+        self.new_user = User.objects.create(username='mosh')
+        self.new_profile = UserProfile.objects.create(user=self.new_user)
+        self.xform2 = self._publish_markdown(md2, self.new_user)
+        # chane the xml value of form1
+        self.xform.xml = self.xform2.xml
+        self.xform.save()
+        # assert that the hash has been updated and is not empty
+        self.assertFalse(self.xform.hash == "" or self.xform.hash is None)
+        self.assertFalse(self.xform.hash == old_hash)
 
     def test_report_exception_with_exc_info(self):
         e = Exception("A test exception")
