ce4b739ba7e07c717181c525b959dbab709eecbb
Dickson Ukang'a <ukanga@gmail.com>
2017-12-21 15:29:57 +0300

    Assign superuser to onadata role.
    Allows test database to br created when running tests.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,13 +2,14 @@
 
 sleep 20
 
-psql -h db -U postgres -c "CREATE ROLE onadata WITH LOGIN PASSWORD 'onadata';"
+psql -h db -U postgres -c "CREATE ROLE onadata WITH SUPERUSER LOGIN PASSWORD 'onadata';"
 psql -h db -U postgres -c "CREATE DATABASE onadata OWNER onadata;"
 psql -h db -U postgres onadata -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;"
 
 . /srv/.virtualenv/bin/activate
 
 cd /srv/onadata
+pip install -r requirements/base.pip
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
