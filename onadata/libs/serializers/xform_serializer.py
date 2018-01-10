28c39d78f87cbdf877dab49f5221220014f3b908
Dickson Ukang'a <ukanga@gmail.com>
2018-01-10 14:18:45 +0300

    Display form version in OpenRosa Form List API
    - also removes majorMinorVersion
    - closes #1195

--- onadata/libs/serializers/xform_serializer.py
+++ onadata/libs/serializers/xform_serializer.py
@@ -323,15 +323,11 @@ class XFormCreateSerializer(XFormSerializer):
 class XFormListSerializer(serializers.Serializer):
     formID = serializers.ReadOnlyField(source='id_string')
     name = serializers.ReadOnlyField(source='title')
-    majorMinorVersion = serializers.SerializerMethodField('get_version')
-    version = serializers.SerializerMethodField()
+    version = serializers.ReadOnlyField()
     hash = serializers.ReadOnlyField()
     descriptionText = serializers.ReadOnlyField(source='description')
     downloadUrl = serializers.SerializerMethodField('get_url')
     manifestUrl = serializers.SerializerMethodField('get_manifest_url')
-
-    def get_version(self, obj):
-        return None
 
     @check_obj
     def get_url(self, obj):
