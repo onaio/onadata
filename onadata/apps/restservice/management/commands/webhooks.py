ebf3b8e1f87866fd23d61ef53e90d60b617c6f16
Dickson Ukang'a <ukanga@gmail.com>
2018-01-13 09:33:59 +0300

    google

--- onadata/apps/restservice/management/commands/webhooks.py
+++ onadata/apps/restservice/management/commands/webhooks.py
@@ -24,18 +24,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         action = options.get('action')
-        if action == 'resend':
-            xform = XForm.objects.get(pk=options.get('formid'))
-            self.stdout.write(
-                _("Going to resend %(no_of_submissions)d submissions for the"
-                  " form %(xform)s." % {
-                      'no_of_submissions': xform.instances.count(),
-                      'xform': xform
-                  }))
+        xform = XForm.objects.get(pk=options.get('formid'))
+        self.stdout.write(
+            _("Going to resend %(no_of_submissions)d submissions for the"
+              " form %(xform)s." % {
+                  'no_of_submissions': xform.instances.count(),
+                  'xform': xform
+              }))
 
-            submissions = xform.instances.filter(deleted_at__isnull=True)
+        submissions = xform.instances.filter(deleted_at__isnull=True)
+        if action == 'resend':
             for i in queryset_iterator(submissions):
                 call_service_async.delay(i)
+        elif action == 'googlesheets':
+            try:
+                from google_export.signals import google_sync_submissions
+            except ImportError:
+                pass
+            else:
+                for i in queryset_iterator(submissions):
+                    google_sync_submissions(i.parsed_instance, False)
         else:
             self.stdout.write(
                 _("Unknown action %(action)s." % {'action': action}))
