600b0bd0ee5a484000202cc94b3e6eb117970142
Dickson Ukang'a <ukanga@gmail.com>
2018-01-21 19:58:15 +0300

    fix_attachments_counts

---
+++ onadata/apps/logger/management/commands/fix_attachments_counts.py
@@ -0,0 +1,96 @@
+# -*- coding=utf-8 -*-
+"""
+Fix submission media count command.
+"""
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy
+
+from onadata.apps.logger.models.attachment import get_original_filename
+from onadata.apps.logger.models.xform import XForm
+from onadata.libs.utils.dict_tools import get_values_matching_key
+from onadata.libs.utils.model_tools import queryset_iterator
+
+
+def get_expected_media(instance):
+    """
+    Returns a list of expected media files from the submission data.
+    """
+    if not hasattr(instance, '_expected_media'):
+        data = instance.get_dict()
+        media_list = []
+        if 'encryptedXmlFile' in data and instance.xform.encrypted:
+            media_list.append(data['encryptedXmlFile'])
+            if 'media' in data:
+                media_list.extend([i['media/file'] for i in data['media']])
+        else:
+            media_xpaths = instance.xform.get_media_survey_xpaths()
+            for media_xpath in media_xpaths:
+                media_list.extend(
+                    get_values_matching_key(data, media_xpath))
+        # pylint: disable=protected-access
+        instance._expected_media = list(set(media_list))
+
+    return instance._expected_media  # pylint: disable=protected-access
+
+
+def num_of_media(instance):
+    """
+    Returns number of media attachments expected in the submission.
+    """
+    if not hasattr(instance, '_num_of_media'):
+        # pylint: disable=protected-access
+        instance._num_of_media = len(get_expected_media(instance))
+
+    return instance._num_of_media  # pylint: disable=protected-access
+
+
+def update_attachment_tracking(instance):
+    """
+    Takes an Instance object and updates attachment tracking fields
+    """
+    for attachment in instance.attachments.all():
+        attachment.name = get_original_filename(attachment.media_file.name)
+        attachment.save()
+
+    instance.total_media = num_of_media(instance)
+    instance.media_count = instance.attachments.filter(
+        name__in=get_expected_media(instance)
+    ).distinct('name').order_by('name').count()
+    instance.media_all_received = instance.media_count == instance.total_media
+    instance.save(update_fields=['total_media',
+                                 'media_count',
+                                 'media_all_received'])
+
+
+class Command(BaseCommand):
+    """
+    Fix attachments count command.
+    """
+    args = 'username'
+    help = ugettext_lazy("Fix attachments count.")
+
+    def add_arguments(self, parser):
+        parser.add_argument('username')
+
+    def handle(self, *args, **options):
+        try:
+            username = options['username']
+        except KeyError:
+            raise CommandError(
+                _("You must provide the username to publish the form to."))
+        # make sure user exists
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise CommandError(_("The user '%s' does not exist.") % username)
+
+        xforms = XForm.objects.filter(user=user, deleted_at__isnull=True)
+        for xform in queryset_iterator(xforms):
+            submissions = xform.instances.filter(media_all_received=False)
+            self.stdout.write("%s to process %s submissions" % (
+                xform, submissions.count()))
+            for submission in queryset_iterator(submissions):
+                update_attachment_tracking(submission)
