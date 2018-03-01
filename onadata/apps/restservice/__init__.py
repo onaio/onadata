4ab077580898d2d131f896ce8c3d2e00042cefae
Rémi Desgrange <remi.desgrange@fibrea.net>
2018-03-01 10:38:39 +0100

    WIP add slack support

--- onadata/apps/restservice/__init__.py
+++ onadata/apps/restservice/__init__.py
@@ -1,4 +1,5 @@
 SERVICE_CHOICES = ((u'f2dhis2', u'f2dhis2'), (u'generic_json', u'JSON POST'),
                    (u'generic_xml', u'XML POST'), (u'bamboo', u'bamboo'),
                    (u'textit', u'TextIt POST'),
-                   (u'google_sheets', u'Google Sheet'))
+                   (u'google_sheets', u'Google Sheet'),
+                   (u'slack', u'slack'))
