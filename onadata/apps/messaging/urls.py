6755f3ae583d353135d47641760842abc9d8b4be
Ona KE code machine <tech+ke@ona.io>
2018-03-14 17:18:49 +0300

    Add messaging urls module
    Co-authored-by: Kelvin Jayanoris <kjayanoris@gmail.com>
    Co-authored-by: Ukang'a Dickson <ukanga@gmail.com>

---
+++ onadata/apps/messaging/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Messaging urls module.
+"""
+from django.conf.urls import url, include
+from rest_framework import routers
+from onadata.apps.messaging.viewsets import MessagingViewSet
+
+router = routers.DefaultRouter(trailing_slash=False)  # pylint: disable=C0103
+router.register(r'messaging', MessagingViewSet)
+
+urlpatterns = [  # pylint: disable=C0103
+    url(r'^api/v1/', include(router.urls)),
+]
