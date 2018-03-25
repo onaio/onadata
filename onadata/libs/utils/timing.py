fd27c631674f7aca446355f55d0642e907fdb411
pld <peetldee@gmail.com>
2018-03-25 17:58:40 -0400

    fix python3 syntax errors

--- onadata/libs/utils/timing.py
+++ onadata/libs/utils/timing.py
@@ -1,26 +1,8 @@
 import datetime
 import six
-import time
 
 from itertools import chain
 from django.utils import timezone
-
-
-def print_time(func):
-    """
-    @print_time
-
-    Put this decorator around a function to see how many seconds each
-    call of this function takes to run.
-    """
-    def wrapped_func(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        seconds = end - start
-        print "SECONDS:", seconds, func.__name__, kwargs
-        return result
-    return wrapped_func
 
 
 def get_header_date_format(date_modified):
