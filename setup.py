482daf37303d82e4af3f1d40c0b119309fff26ba
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-03-28 12:22:42 +0300

    Use setupy.py to track dependencies
    - Add dependencies to `install_requires` option of setup.py
    - Make `base.in` use dependencies from `setup.py`

--- setup.py
+++ setup.py
@@ -1,25 +1,112 @@
 """
 Setup file for onadata
 
-Ona is a social enterprise that builds the data infrastructure to drive change.
-We believe technology affords new opportunities for governments and development
-organizations to be increasingly data driven, collaborative and accountable.
-Our goal is never simply to build a great product, but to support great
-outcomes.
+Onadata is a Django application that provides APIs for data collection and
+aggregation.
+
+It provides:
+- ODK aggregate server API
+- ODK briefcase API
+- REST APIs to interact with data that is used by ona.io
 
 See:
 https://github.com/onaio/onadata
 https://ona.io
+https://opendatakit.org
 """
 
 from setuptools import setup, find_packages
 
-setup(
+config = dict(
     name="onadata",
     version="1.13.0",
     description="Collect Analyze and Share Data!",
     author="Ona Systems Inc",
     author_email="support@ona.io",
     license="Copyright (c) 2014 Ona Systems Inc All rights reserved.",
+    project_urls={
+        'Source': 'https://github.com/onaio/onadata',
+    },
     packages=find_packages(exclude=['docs', 'tests']),
-    install_requires=['Django==1.11.11'])
+    install_requires=[
+        "Django>=1.11<2",
+        "django-guardian",
+        "django-registration-redux",
+        "django-templated-email",
+        "django-reversion",
+        "django-filter",
+        # generic relation
+        "django-query-builder",
+        "celery",
+        "django-celery",
+        "librabbitmq",
+        # cors
+        "django-cors-headers",
+        "django-debug-toolbar",
+        # digest authentication
+        "python-digest",
+        # oauth2 support
+        "django-oauth-toolkit",
+        "oauth2client",
+        "jsonpickle",
+        # jwt
+        "PyJWT",
+        # captcha
+        "recaptcha-client",
+        # API support
+        "djangorestframework",
+        "djangorestframework-csv",
+        "djangorestframework-gis",
+        "djangorestframework-jsonapi",
+        "djangorestframework-jsonp",
+        "djangorestframework-xml",
+        # geojson
+        "geojson",
+        # tagging
+        "django-taggit",
+        # database
+        "psycopg2>2.7.1",
+        "pymongo",
+        # sms support
+        "dict2xml",
+        "lxml",
+        # pyxform
+        "pyxform",
+        # spss
+        "savreaderwriter",
+        # tests
+        "mock",
+        "httmock",
+        # JSON data type support, keeping it around for previous migration
+        "jsonfield<1.0",
+        # memcached support
+        "pylibmc",
+        "python-memcached",
+        # docs
+        "sphinx",
+        "Markdown",
+        # others
+        "unicodecsv",
+        "xlrd",
+        "xlwt",
+        "openpyxl",
+        "dpath",
+        "elaphe",
+        "httplib2",
+        "modilabs-python-utils",
+        "numpy",
+        "Pillow",
+        "python-dateutil",
+        "pytz",
+        "requests",
+        "simplejson",
+        "google-api-python-client",
+        "uwsgi",
+        "flake8",
+        "raven",
+        "django-activity-stream",
+        "paho-mqtt",
+    ]
+)
+
+setup(**config)
