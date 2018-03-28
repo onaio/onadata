5afb2ba1777921b67e298eb03309aee02ab824f6
pld <peetldee@gmail.com>
2018-03-28 15:38:28 -0400

    decode b64 encoded strings

--- onadata/libs/utils/mongo.py
+++ onadata/libs/utils/mongo.py
@@ -1,6 +1,8 @@
+from __future__ import unicode_literals
+
 import base64
-from functools import reduce
 import re
+from functools import reduce
 
 
 key_whitelist = ['$or', '$and', '$exists', '$in', '$gt', '$gte',
@@ -8,8 +10,8 @@ key_whitelist = ['$or', '$and', '$exists', '$in', '$gt', '$gte',
 
 
 def _decode_from_mongo(key):
-    re_dollar = re.compile(r'^%s' % base64.b64encode(b'$'))
-    re_dot = re.compile(r'\%s' % base64.b64encode(b'.'))
+    re_dollar = re.compile(r'^%s' % base64.b64encode(b'$').decode('utf-8'))
+    re_dot = re.compile(r'%s' % base64.b64encode(b'.').decode('utf-8'))
     return reduce(lambda s, c: c[0].sub(c[1], s),
                   [(re_dollar, '$'), (re_dot, '.')], key)
 
