dc92d7ba414e5ff17596b6c867671146fa9192d3
pld <peetldee@gmail.com>
2018-03-28 11:53:37 -0400

    add line breaks

--- onadata/libs/utils/common_tools.py
+++ onadata/libs/utils/common_tools.py
@@ -203,16 +203,22 @@ def cmp_to_key(mycmp):
     class K(object):
         def __init__(self, obj, *args):
             self.obj = obj
+
         def __lt__(self, other):
             return mycmp(self.obj, other.obj) < 0
+
         def __gt__(self, other):
             return mycmp(self.obj, other.obj) > 0
+
         def __eq__(self, other):
             return mycmp(self.obj, other.obj) == 0
+
         def __le__(self, other):
             return mycmp(self.obj, other.obj) <= 0
+
         def __ge__(self, other):
             return mycmp(self.obj, other.obj) >= 0
+
         def __ne__(self, other):
             return mycmp(self.obj, other.obj) != 0
     return K
