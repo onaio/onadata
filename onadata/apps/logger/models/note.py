f12ff6af69cb5053e2b73ea1276abae0a642427a
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-03-29 17:09:47 +0300

    Use settings.AUTH_USER_MODEL in Note model

--- onadata/apps/logger/models/note.py
+++ onadata/apps/logger/models/note.py
@@ -3,15 +3,14 @@ from __future__ import absolute_import
 from django.conf import settings
 from django.db import models
 
-USER = settings.AUTH_USER_MODEL
-
 
 class Note(models.Model):
     note = models.TextField()
     instance = models.ForeignKey(
         'logger.Instance', related_name='notes')
     instance_field = models.TextField(null=True, blank=True)
-    created_by = models.ForeignKey(USER, null=True, blank=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
+                                   blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
