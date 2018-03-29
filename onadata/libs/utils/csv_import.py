ec36c96484582c0aef3e97acf5a0587b42bde362
pld <peetldee@gmail.com>
2018-03-29 19:37:15 -0400

    open as bytes convert result to text

--- onadata/libs/utils/csv_import.py
+++ onadata/libs/utils/csv_import.py
@@ -351,7 +351,7 @@ def get_async_csv_submission_status(job_uuid):
 
         if isinstance(result, (Exception)):
             return async_status(
-                celery_state_to_status(job.state), job.result.message)
+                celery_state_to_status(job.state), text(job.result))
 
         if isinstance(result, basestring):
             return async_status(celery_state_to_status(job.state))
