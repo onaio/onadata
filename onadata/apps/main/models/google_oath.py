221403fc1ee1289dff64d2e4de04eb332cdc5ab7
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-03-29 14:42:18 +0300

    pylint disable=C0103 for User

--- onadata/apps/main/models/google_oath.py
+++ onadata/apps/main/models/google_oath.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.db import models
 from oauth2client.contrib.django_util.models import CredentialsField
 
-USER = settings.AUTH_USER_MODEL
+User = settings.AUTH_USER_MODEL    # pylint: disable=C0103
 
 
 class TokenStorageModel(models.Model):
@@ -14,7 +14,7 @@ class TokenStorageModel(models.Model):
     Google Auth Token storage model
     """
 
-    id = models.OneToOneField(USER, primary_key=True, related_name='google_id')
+    id = models.OneToOneField(User, primary_key=True, related_name='google_id')
     credential = CredentialsField()
 
     class Meta:
