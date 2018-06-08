2281eb045198212e7d9d17b5f5b9d3fd30084cb9
Njagi Mwaniki <njagi@urbanslug.com>
2018-06-08 13:13:04 +0300

    activate virtalenv
    If deps are installed in the Dockerfile no need to redo it here.
    
    Signed-off-by: Njagi Mwaniki <njagi@urbanslug.com>

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,11 +6,8 @@ psql -h db -U postgres -c "CREATE ROLE onadata WITH SUPERUSER LOGIN PASSWORD 'on
 psql -h db -U postgres -c "CREATE DATABASE onadata OWNER onadata;"
 psql -h db -U postgres onadata -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;"
 
-virtualenv /srv/onadata/.virtualenv
-. /srv/onadata/.virtualenv/bin/activate
-
+source /srv/.virtualenv/bin/activate
 cd /srv/onadata
-pip install -r requirements/base.pip
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
