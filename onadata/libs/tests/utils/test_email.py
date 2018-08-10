8c97f0a54dfbca3ecc94d32318c2dbac88a17ffa
ivermac <mark.ekisa@gmail.com>
2018-08-10 17:44:36 +0300

    Update email test: test get_verification_email_data function
    Signed-off-by: ivermac <mark.ekisa@gmail.com>

--- onadata/libs/tests/utils/test_email.py
+++ onadata/libs/tests/utils/test_email.py
@@ -1,10 +1,11 @@
-from django.test import RequestFactory
+import urllib
 
+from django.test import RequestFactory
 from onadata.apps.main.tests.test_base import TestBase
 from onadata.libs.utils.email import get_verification_email_data
 
 
-def get_kwargs(verification_url=None):
+def get_kwargs(verification_url=None, redirect_url=None):
     request_factory = RequestFactory()
     request = request_factory.get('/path', data={'name': u'test'})
 
@@ -13,6 +14,7 @@ def get_kwargs(verification_url=None):
         "username": "johndoe",
         "verification_key": "123abc",
         "verification_url": verification_url,
+        "redirect_url": redirect_url,
         "request": request
     }
 
@@ -25,7 +27,7 @@ class TestEmail(TestBase):
 
         self.assertIn(
             ('http://testserver/api/v1/profiles/verify_email?'
-             'verification_key=123abc'),
+             'verification_key=%s' % kwargs.get('verification_key')),
             email_data.get('message_txt')
         )
 
@@ -34,6 +36,23 @@ class TestEmail(TestBase):
         kwargs = get_kwargs(verification_url)
         email_data = get_verification_email_data(**kwargs)
         self.assertIn(
-            '%s?verification_key=123abc' % verification_url,
+            '{}?verification_key={}'.format(
+                verification_url,
+                kwargs.get('verification_key')
+            ),
             email_data.get('message_txt')
+        )
+
+    def test_get_verification_email_data_with_verification_and_redirect_urls(
+            self):
+        verification_url = "http://ab.cd.ef"
+        redirect_url = "http://red.ir.ect"
+        kwargs = get_kwargs(verification_url, redirect_url)
+        email_data = get_verification_email_data(**kwargs)
+        encoded_url = urllib.urlencode({
+            'verification_key': kwargs.get('verification_key'),
+            'redirect_url': redirect_url
+        })
+        self.assertIn(
+            encoded_url.replace('&', '&amp;'), email_data.get('message_txt')
         )
