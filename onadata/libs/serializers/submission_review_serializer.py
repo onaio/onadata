979ca17a294a7715c6b9c1e89f30d2ab73da0f60
DavisRayM <davisraymondmuro@outlook.com>
2018-08-17 12:26:50 +0300

    Add SubmissionReviewSerializer

---
+++ onadata/libs/serializers/submission_review_serializer.py
@@ -0,0 +1,41 @@
+"""
+Submission Review Serializer Module
+"""
+from rest_framework import serializers, exceptions
+
+from onadata.apps.logger.models import SubmissionReview
+
+
+class SubmissionReviewSerializer(serializers.ModelSerializer):
+    """
+    SubmissionReviewSerializer Class
+    """
+
+    class Meta:
+        """
+        Meta Options for SubmissionReviewSerializer
+        """
+        model = SubmissionReview
+        fields = (
+            'id',
+            'instance',
+            'note',
+            'created_by',
+            'status',
+            'date_created',
+            'date_modified')
+        read_only_fields = ['note_text']
+
+    def validate(self, attrs):
+        """
+        Custom Validate Method for SubmissionReviewSerializer
+        """
+        status = attrs.get('status')
+        note = attrs.get('note')
+
+        if status == SubmissionReview.REJECTED:
+            if note is None:
+                raise exceptions.ValidationError(
+                    {"note": "Can\'t reject a submission without a comment."}
+                )
+        return attrs
