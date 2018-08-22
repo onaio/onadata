49d2a87d7c215575c20572f8c7f1bf03709a7b5a
DavisRayM <davisraymondmuro@outlook.com>
2018-08-22 14:08:59 +0300

    Update TestSubmissionReviewViewset - Add Tests for SubmissionReviewViewset filtering

--- onadata/apps/api/tests/viewsets/test_submission_review_viewset.py
+++ onadata/apps/api/tests/viewsets/test_submission_review_viewset.py
@@ -3,12 +3,14 @@ Submission Review ViewSet Tests Module
 """
 from __future__ import unicode_literals
 
+from guardian.shortcuts import assign_perm
 from rest_framework.test import APIRequestFactory
 
 from onadata.apps.api.viewsets.submission_review_viewset import \
     SubmissionReviewViewSet
 from onadata.apps.logger.models import SubmissionReview
 from onadata.apps.main.tests.test_base import TestBase
+from onadata.libs.permissions import CAN_CHANGE_XFORM
 
 
 class TestSubmissionReviewViewSet(TestBase):
@@ -35,7 +37,7 @@ class TestSubmissionReviewViewSet(TestBase):
         """
         Utility method creates Submission Review
         """
-        instance_id = self._first_xform_instance.pk
+        instance_id = self._first_xform_instance.id
         submission_data = {
             'note_text': "Supreme Overload!",
             'instance': instance_id
@@ -182,3 +184,100 @@ class TestSubmissionReviewViewSet(TestBase):
 
         self.assertEqual(200, response.status_code)
         self.assertEqual(0, len(response.data))
+
+    def test_submission_review_instance_filter(self):
+        """
+        Test can filter Submission Reviews By Instance
+        """
+        review_one_data = self._create_submission_review()
+
+        review_two_data = {
+            'note_text': "Sup ?",
+            'instance': self.xform.instances.all().order_by('pk')[1].id
+        }
+
+        view = SubmissionReviewViewSet.as_view({
+            'post': 'create',
+            'get': 'list'
+        })
+        request = self.factory.post('/', data=review_two_data, **self.extra)
+        response = view(request=request)
+
+        self.assertEqual(201, response.status_code)
+        self.assertEqual(2, len(SubmissionReview.objects.all()))
+
+        # Can filter submission review list by instance
+        request = self.factory.get(
+            '/', {'instance': review_one_data['instance']}, **self.extra)
+        response = view(request=request)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(1, len(response.data))
+        self.assertEqual(review_one_data['id'], response.data[0]['id'])
+
+    def test_submission_review_status_filter(self):
+        """
+        Test can filter Submission Reviews By Status
+        """
+        review_one_data = self._create_submission_review()
+
+        review_two_data = {
+            'note_text': "Sup ?",
+            'instance': review_one_data['instance'],
+            'status': SubmissionReview.APPROVED
+        }
+
+        view = SubmissionReviewViewSet.as_view({
+            'post': 'create',
+            'get': 'list'
+        })
+        request = self.factory.post('/', data=review_two_data, **self.extra)
+        response = view(request=request)
+
+        self.assertEqual(201, response.status_code)
+        self.assertEqual(2, len(SubmissionReview.objects.all()))
+
+        # Can filter submission review list by instance
+        request = self.factory.get('/', {'status': SubmissionReview.PENDING},
+                                   **self.extra)
+        response = view(request=request)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(1, len(response.data))
+        self.assertEqual(review_one_data['id'], response.data[0]['id'])
+
+    def test_submission_review_created_by_filter(self):
+        """
+        Test we can filter by created_by
+        """
+        review_one_data = self._create_submission_review()
+        submission_review = SubmissionReview.objects.get(
+            id=review_one_data['id'])
+        self._create_user_and_login('dave', '1234')
+        extra = {'HTTP_AUTHORIZATION': 'Token %s' % self.user.auth_token}
+
+        assign_perm(CAN_CHANGE_XFORM, self.user,
+                    submission_review.instance.xform)
+
+        review_two_data = {
+            'note_text': "Sup ?",
+            'instance': review_one_data['instance'],
+            'status': SubmissionReview.APPROVED
+        }
+
+        view = SubmissionReviewViewSet.as_view({
+            'post': 'create',
+            'get': 'list'
+        })
+        request = self.factory.post('/', data=review_two_data, **extra)
+        response = view(request=request)
+
+        self.assertEqual(201, response.status_code)
+        review_two_data = response.data
+        self.assertEqual(2, len(SubmissionReview.objects.all()))
+
+        # Can filter submission review list by created_by
+        request = self.factory.get('/', {'created_by': self.user.id},
+                                   **self.extra)
+        response = view(request=request)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(1, len(response.data))
+        self.assertEqual(review_two_data['id'], response.data[0]['id'])
