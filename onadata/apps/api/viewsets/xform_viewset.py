bf543d8c5bbade8a11f49ae7743f9fb47040f286
WinnyTroy <kiraguwinnie@gmail.com>
2019-01-09 12:57:16 +0300

    Include viewset to expose single submit url.

--- onadata/apps/api/viewsets/xform_viewset.py
+++ onadata/apps/api/viewsets/xform_viewset.py
@@ -72,7 +72,8 @@ from onadata.libs.utils.string import str2bool
 from onadata.libs.utils.viewer_tools import (enketo_url,
                                              generate_enketo_form_defaults,
                                              get_enketo_preview_url,
-                                             get_form_url)
+                                             get_form_url,
+                                             get_single_submit_url)
 from onadata.libs.exceptions import EnketoError
 from onadata.settings.common import XLS_EXTENSIONS, CSV_EXTENSION
 
@@ -413,6 +414,26 @@ class XFormViewSet(AnonymousUserPublicFormsMixin,
                 data = {"enketo_url": url, "enketo_preview_url": preview_url}
 
         return Response(data, http_status)
+
+    @action(methods=['GET'], detail=True)
+    def submit_url_preview(self, request, **kwargs):
+        """Expose single submit url."""
+        self.object = self.get_object()
+        form_id = self.object.id_string
+        username = self.object.username
+        xform_pk = self.object.pk
+        try:
+            submission_url = get_single_submit_url(
+                request, username, form_id, xform_pk)
+
+        except EnketoError as e:
+            data = {'message': _(u"Enketo error: %s" % e)}
+        else:
+            if submission_url:
+                data = {"single_url": submission_url}
+
+        return Response(data)
+
 
     @action(methods=['POST', 'GET'], detail=False)
     def survey_preview(self, request, **kwargs):
