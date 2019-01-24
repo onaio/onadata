b3a24e2adce3d06cd768a2998ba2dedc8415631c
WinnyTroy <kiraguwinnie@gmail.com>
2019-01-24 15:47:42 +0300

    Include constant in settings file

--- onadata/apps/api/tests/viewsets/test_xform_viewset.py
+++ onadata/apps/api/tests/viewsets/test_xform_viewset.py
@@ -733,6 +733,7 @@ class TestXFormViewSet(TestAbstractViewSet):
                     response.status_code, status.HTTP_400_BAD_REQUEST)
                 self.assertEqual(response.data, data)
 
+    @override_settings(TESTING_MODE=False)
     def test_enketo_url(self):
         """Test functionality to expose enketo urls."""
         with HTTMock(enketo_preview_url_mock, enketo_url_mock,
