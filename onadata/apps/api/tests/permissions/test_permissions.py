1cdb336481457584bbb3f3b3b8a0f8bb7588220b
Brian Marika <mwaura.marika@gmail.com>
2019-02-03 02:22:21 +0300

    Fix faiing permission in permissions.

--- onadata/apps/api/tests/permissions/test_permissions.py
+++ onadata/apps/api/tests/permissions/test_permissions.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User
-from mock import MagicMock
+from mock import MagicMock, patch
 from onadata.apps.main.tests.test_base import TestBase
 
 from onadata.apps.logger.models import Instance, XForm
-from onadata.apps.api.permissions import MetaDataObjectPermissions
+from onadata.apps.api.permissions import (
+    MetaDataObjectPermissions,
+    AlternateHasObjectPermissionMixin
+)
 
 
 class TestPermissions(TestBase):
@@ -20,11 +23,16 @@ class TestPermissions(TestBase):
             self.permissions.has_object_permission(
                 request, self.view, obj))
 
-    def test_delete_instance_metadata_without_perms(self):
+    @patch.object(AlternateHasObjectPermissionMixin, '_has_object_permission')
+    def test_delete_instance_metadata_without_perms(self, has_perms_mock):
+        """
+        Test that a user cannot delete an instance if they are not allowed
+        through the XForm or the Project
+        """
+        has_perms_mock.return_value = False
         user = User(username="test")
-        instance = Instance()
+        instance = Instance(user=User(username="username"))
         instance.xform = XForm()
-        # user.has_perms.return_value = False
         request = MagicMock(user=user, method='DELETE')
         obj = MagicMock(content_object=instance)
         self.assertFalse(
