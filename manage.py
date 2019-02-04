ef07c1171cf2fc76e5924e93bce48c67d817f69b
Brian Marika <mwaura.marika@gmail.com>
2019-02-04 16:17:59 +0300

    Issue a Deprecation  warnning for sites running Django < 2.

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import logging
 import os
 import sys
+import warnings
 
 south_logger = logging.getLogger('south')
 south_logger.setLevel(logging.INFO)
@@ -15,6 +16,16 @@ if __name__ == "__main__":
                               "onadata.settings.common")
         print('Your environment is:"{}"'.format(
             os.environ['DJANGO_SETTINGS_MODULE']))
+
+    import django
+    if django.VERSION[0] == 2:
+        message = "This version of onadata is no-longer supported and no new"\
+            "features will be added. However, bugs that will be found will be"\
+            "fixed. Please consider upgrading your onadata version to the "\
+            "latest one. https://github.com/onaio/onadata/releases"
+        RED = '\033[91m'
+        CEND = '\033[0m'
+        print(RED + "{}".format(message) + CEND)
 
     from django.core.management import execute_from_command_line
 
