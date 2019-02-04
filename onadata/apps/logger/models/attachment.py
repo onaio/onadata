06806febe692b326dcc22c5811a279226bd0fa76
Lincoln Simba <lincolncmba@gmail.com>
2019-02-04 14:41:45 +0300

    Save osm xml as string not binary.
    Signed-off-by: Lincoln Simba <lincolncmba@gmail.com>

--- onadata/apps/logger/models/attachment.py
+++ onadata/apps/logger/models/attachment.py
@@ -40,7 +40,8 @@ class Attachment(models.Model):
     OSM = 'osm'
 
     instance = models.ForeignKey(
-        'logger.Instance',related_name="attachments", on_delete=models.CASCADE)
+        'logger.Instance', related_name="attachments",
+        on_delete=models.CASCADE)
     media_file = models.FileField(max_length=255, upload_to=upload_to)
     mimetype = models.CharField(
         max_length=100, null=False, blank=True, default='')
