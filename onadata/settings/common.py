b3fa6515ffb35d294aa36b32f001edc49f5f66e3
Brian Marika <mwaura.marika@gmail.com>
2019-02-04 12:17:57 +0300

    Fix attachment_url errors in tests.

--- onadata/settings/common.py
+++ onadata/settings/common.py
@@ -558,3 +558,6 @@ XLS_EXTENSIONS = ['xls', 'xlsx']
 CSV_EXTENSION = 'csv'
 
 PROJECT_QUERY_CHUNK_SIZE = 5000
+
+# Prevents "The number of GET/POST parameters exceeded" exception
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000000
