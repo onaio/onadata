120456d63223663e5b93f42eed2e8d61cfa35a58
WinnyTroy <kiraguwinnie@gmail.com>
2019-12-30 08:48:28 +0300

    Update failing tests

--- onadata/apps/api/management/commands/delete_users.py
+++ onadata/apps/api/management/commands/delete_users.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import User
 from onadata.apps.logger.models import XForm
 from onadata.apps.logger.models import Project
 from onadata.apps.logger.models import Instance
-from django.db.utils import IntegrityError
 from django.core.management.base import BaseCommand
 
 
@@ -15,61 +14,59 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         users = kwargs['user']
         if users:
-            usernames = []
+            positive_reponses = ["Yes", "yes", "y", "Y"]
+            negative_reponses = ["No", "no", "n", "N"]
             for user in users:
                 user_details = user.split(':')
-                import ipdb; ipdb.set_trace()
                 username = user_details[0]
-                email = user_details[1]
                 user_projects = self.get_user_projects(username)
-                user_forms = self.get_user_forms(username)
-                user_input = input("User has {} projects, {} forms with {} submissions. \
-                    Do you wish to continue deleting this account?".format(user_projects, user_forms, user_forms))
+                user_forms = len(self.get_user_forms(username))
 
-            positive_reponses = ["Yes","yes","y","Y"]
-            negative_reponses = ["No","no","n","N"]
+                user_input = input("User has {} projects, {} forms. \
+                        Do you wish to continue deleting this account?".format(
+                        user_projects, user_forms))
 
             if user_input in positive_reponses:
-                import ipdb; ipdb.set_trace()
-                usernames.append(username)
+                try:
+                    self.delete_user(username)
+                except User.DoesNotExist:
+                    self.stdout.write('User %s does not exist.' % username)
+
             elif user_input in negative_reponses:
                 pass
             else:
                 input('Enter space-delimited list of users: ')
 
-            self.delete_user(usernames)
         else:
-            raw_input('Enter space-delimited list of users: ').split(',')
-            
-            
-        
+            input('Enter space-delimited list of users: ').split(',')
 
-    def get_user_projects(self, username):
+    def get_user_projects(self, username):  # pylint: disable=R0201
         user = User.objects.get(username=username)
 
         user_projects = Project.objects.filter(created_by=user).count()
 
         return user_projects
 
-    def get_user_forms(self, username):
+    def get_user_forms(self, username):  # pylint: disable=R0201
         user = User.objects.get(username=username)
 
-        user_forms = XForm.objects.filter(user=user).count()
-        forms = XForm.objects.filter(user=user)
+        user_forms = XForm.objects.filter(user=user)
 
-        for form in forms:
-            form_name = form.name
-            form_sumbissions = Instance.objects.filter(xform=form_name).count()
+        for form in user_forms:
+            form_name = form.title
+            form_sumbissions = Instance.objects.filter(xform=form).count()
 
-        return user_forms
+            result = {
+                form_name: form_sumbissions,
+            }
 
-    def delete_user(self, usernames):
-        import ipdb; ipdb.set_trace()
-        for username in usernames:
-            try:
-                user = User.objects.get(username=username)
-                user.delete()
-                self.stdout.write(
-                    'User %s deleted with success!' % (username))
-            except User.DoesNotExist:
-                self.stdout.write('User %sdoes not exist.' % username)
+        return result
+
+    def delete_user(self, username):
+        try:
+            user = User.objects.get(username=username)
+            user.delete()
+            self.stdout.write(
+                'User %s deleted with success!' % (username))
+        except User.DoesNotExist:
+            self.stdout.write('User %sdoes not exist.' % username)
--- onadata/apps/api/tests/management/commands/test_delete_users.py
+++ onadata/apps/api/tests/management/commands/test_delete_users.py
@@ -1,29 +1,19 @@
-from io import StringIO
+from six import StringIO
+
 from django.core.management import call_command
-from django.contrib.auth.models import User
 from onadata.apps.main.tests.test_base import TestBase
 
 
 class DeleteUserTest(TestBase):
     def test_delete_users(self):
+        self._publish_transportation_form_and_submit_instance()
+        username = self.xform.user.username
+        self.xform.user.email = 'bob@gmail.com'
+        email = self.xform.user.email
+
+        user_details = username+':'+email
         out = StringIO()
-        user1, created = User.objects.get_or_create(username='scott')
-        user1.email = 'scott@gmail.com'
-        user1.save()
-        user2, created = User.objects.get_or_create(username='scotty')
-        user2.email = 'scotty@gmail.com'
-        user2.save()
-
-        users = [user1, user2]
-
-        for user in users:
-            username = user.username
-            email = user.email
-            user_details = username+':'+email
-        
-            call_command('delete_users', user_details, stdout=out)
-            self.assertIn('User scott deleted with success!', out.getvalue())
-        
-        
-
-
+        call_command(
+                'delete_users', user_details, stdout=out)
+        self.assertIn(
+            'User bob deleted with success!', out.getvalue())
