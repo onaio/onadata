e1e5194dfa9c693db4480e7492c1cc966b10bc23
Davis Raymond Muro <davisraymondmuro@outlook.com>
2020-02-24 11:41:30 +0300

    Revert "Upgrade pyxform version to 0.15.1"
    This reverts commit 18fcc2cdf1b7e41a8f625fccc203ceafdc88183e.

--- onadata/apps/main/tests/test_form_errors.py
+++ onadata/apps/main/tests/test_form_errors.py
@@ -93,10 +93,9 @@ class TestFormErrors(TestBase):
         self.xform.save()
         xls_path = os.path.join(self.this_directory, "fixtures",
                                 "transportation", "tutorial .xls")
-        msg = ("The name 'tutorial ' is an invalid XML tag, it contains an"
-               " invalid character ' '. Names must begin with a letter, colon,"
-               " or underscore, subsequent characters can include numbers, "
-               "dashes, and periods")
+        msg = u"The name 'tutorial ' is an invalid xml tag. Names must begin"\
+            u" with a letter, colon, or underscore, subsequent characters "\
+            u"can include numbers, dashes, and periods"
         self.assertRaisesMessage(
             PyXFormError, msg, self._publish_xls_file, xls_path)
         self.assertEquals(XForm.objects.count(), count)
