8762f1db4c0c565b10035ed1e06a91b831a86721
Davis Raymond Muro <davisraymondmuro@outlook.com>
2020-02-28 10:20:41 +0300

    Update 'test_choice_duplicate_error' test
    Update the 'test_choice_duplicate_error' test to expect the correct
    error message raised by PyXForm.

--- onadata/apps/main/tests/test_form_errors.py
+++ onadata/apps/main/tests/test_form_errors.py
@@ -111,13 +111,14 @@ class TestFormErrors(TestBase):
         xls_path = os.path.join(
             self.this_directory, 'fixtures', 'cascading_selects',
             'duplicate_choice_form.xls')
-        msg = ("There does not seem to be"
-               " a `allow_choice_duplicates`"
-               " column header defined in your settings sheet."
-               " You must have set `allow_choice_duplicates`"
-               " setting in your settings sheet"
-               " to have duplicate choice list names"
-               " in your choices sheet")
+        msg = ("The name column for the 'counties' choice list"
+               " contains these duplicates: 'king'. Duplicate "
+               "names will be impossible to identify in "
+               "analysis unless a previous value in a "
+               "cascading select differentiates them. If this "
+               "is intentional, you can set the "
+               "allow_choice_duplicates setting to 'yes'. "
+               "Read more: https://xlsform.org/#choice-names.")
         self.assertRaisesMessage(
             PyXFormError, msg, self._publish_xls_file, xls_path)
         self.assertEquals(XForm.objects.count(), count)
