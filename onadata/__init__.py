dcc95672c34885953ae1a2005d0e7e34c5ddb4e0
Davis Raymond Muro <davisraymondmuro@outlook.com>
2020-05-19 15:43:53 +0300

    Bump onadata version to v2.3.3
    - Update Changelogs

--- onadata/__init__.py
+++ onadata/__init__.py
@@ -6,7 +6,7 @@ visualization.
 """
 from __future__ import absolute_import, unicode_literals
 
-__version__ = "2.3.2"
+__version__ = "2.3.3"
 
 
 # This will make sure the app is always imported when
