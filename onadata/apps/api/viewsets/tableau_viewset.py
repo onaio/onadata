cdac7137e231ceb7059dd99282d17d122c07b513
Davis Raymond Muro <davisraymondmuro@outlook.com>
2020-10-13 14:29:43 +0300

    Return acceptable tableau column headers

--- onadata/apps/api/viewsets/tableau_viewset.py
+++ onadata/apps/api/viewsets/tableau_viewset.py
@@ -1,5 +1,6 @@
+import json
 import re
-from typing import Dict
+from typing import Dict, List
 
 from rest_framework import status
 from collections import defaultdict
@@ -11,7 +12,8 @@ from onadata.libs.renderers.renderers import pairing
 from onadata.apps.logger.models import Instance
 from onadata.apps.logger.models.xform import XForm
 from onadata.apps.viewer.models.data_dictionary import DataDictionary
-from onadata.apps.api.viewsets.open_data_viewset import OpenDataViewSet
+from onadata.apps.api.viewsets.open_data_viewset import (
+    OpenDataViewSet, IGNORED_FIELD_TYPES, remove_metadata_fields)
 from onadata.libs.serializers.data_serializer import TableauDataSerializer
 from onadata.libs.utils.common_tags import (
     ID, MULTIPLE_SELECT_TYPE, REPEAT_SELECT_TYPE)
@@ -107,6 +109,8 @@ def process_tableau_data(data, xform):
 
 
 class TableauViewSet(OpenDataViewSet):
+    flattened_dict = defaultdict(list)
+
     @action(methods=['GET'], detail=True)
     def data(self, request, **kwargs):
         self.object = self.get_object()
@@ -152,46 +156,111 @@ class TableauViewSet(OpenDataViewSet):
 
         return Response(data)
 
+    def flatten_xform_columns(
+            self, json_of_columns_fields, table: str = None):
+        '''
+        Flattens a json of column fields while splitting columns into separate
+        table names for each repeat and then sets the result to a class
+        variable
+        '''
+        for a in json_of_columns_fields:
+            table_name = table or 'data'
+            if a.get('type') != 'repeat':
+                self.flattened_dict[table_name].append(
+                    {
+                        'name': a.get('name'),
+                        'type': self.get_tableau_type(a.get('type'))})
+            else:
+                table_name = a.get('name')
+
+            # using IGNORED_FIELD_TYPES so that choice values are not included.
+            if a.get('children') and a.get('type') not in IGNORED_FIELD_TYPES:
+                self.flatten_xform_columns(a.get('children'), table=table_name)
+
+    def clean_xform_headers(self, headers: list) -> list:
+        ret = []
+        for h in headers:
+            if re.search(r"\[+\d+\]", h):
+                repeat_count = len(re.findall(r"\[+\d+\]", h))
+                h = h.split('/')[repeat_count].replace('[1]', '')
+
+            # Replace special character with underscore
+            h = re.sub(r"\W", r"_", h)
+            ret.append(h)
+        return ret
+
+    def get_tableau_column_headers(self):
+        """
+        Retrieve column headers that are valid in tableau
+        """
+        tableau_column_headers = defaultdict(list)
+
+        def append_to_tableau_column_headers(
+                header, question_type=None, table=None):
+            table_name = table or 'data'
+            quest_type = question_type or 'string'
+
+            # alias can be updated in the future to question labels
+            tableau_column_headers[table_name].append({
+                'id': header,
+                'dataType': quest_type,
+                'alias': header
+            })
+
+        # Remove metadata fields from the column headers
+        # Calling set to remove duplicates in group data
+        xform_headers = set(remove_metadata_fields(self.xform_headers))
+
+        for header in xform_headers:
+            for table_name, fields in self.flattened_dict.items():
+                for field in fields:
+                    if header == field["name"]:
+                        append_to_tableau_column_headers(
+                            header, field["type"], table_name)
+                        break
+            else:
+                if header == '_id':
+                    append_to_tableau_column_headers(header, "int")
+                elif header.startswith('meta'):
+                    append_to_tableau_column_headers(header)
+
+        # Add repeat parent fields
+        for table_name in self.flattened_dict.keys():
+            if table_name != 'data':
+                append_to_tableau_column_headers(
+                    "_id", "int", table_name)
+                append_to_tableau_column_headers(
+                    "__parent_id", "int", table_name)
+                append_to_tableau_column_headers(
+                    "__parent_table", table=table_name)
+
+        return tableau_column_headers
+
+    def get_tableau_table_schemas(
+            self, column_headers: dict) -> List[dict]:
+        ret = []
+        project = self.xform.project_id
+        id_str = self.xform.id_string
+        for table_name, headers in column_headers.items():
+            table_schema = {}
+            table_schema['table_alias'] = table_name
+            table_schema['connection_name'] = f"{project}_{id_str}"
+            table_schema['column_headers'] = headers
+            if table_name != 'data':
+                table_schema['connection_name'] += f"_{table_name}"
+            ret.append(table_schema)
+        return ret
+
     @action(methods=['GET'], detail=True)
     def schema(self, request, **kwargs):
         self.object = self.get_object()
         if isinstance(self.object.content_object, XForm):
-            xform = self.object.content_object
-            headers = xform.get_headers(repeat_iterations=1)
-            schemas = defaultdict(dict)
-
-            repeat_parent_fields = ['__parent_id', '__parent_table']
-            for child in headers:
-                # Use regex to identify number of repeats
-                repeat_count = len(re.findall(r"\[+\d+\]", child))
-                if re.search(r"\[+\d+\]", child):
-                    table_name = child.split('/')[repeat_count - 1]
-                    table_name = table_name.replace('[1]', '')
-                    if not schemas[table_name].get('headers'):
-                        schemas[table_name]['headers'] = []
-                        schemas[table_name]['headers'].extend(
-                            repeat_parent_fields)
-                    schemas[table_name]['headers'].append(child.split(
-                        '/')[repeat_count])
-                    if not schemas[table_name].get('connection_name'):
-                        schemas[table_name]['connection_name'] = (
-                            f"{xform.project_id}_"
-                            f"{xform.id_string}_{table_name}")
-                    if not schemas[table_name].get('table_alias'):
-                        schemas[table_name]['table_alias'] = table_name
-                else:
-                    if not schemas['data'].get('headers'):
-                        schemas['data']['headers'] = []
-                    if not schemas['data'].get('connection_name'):
-                        schemas['data']['connection_name'] =\
-                            f"{xform.project_id}_{xform.id_string}"
-                    if not schemas['data'].get('table_alias'):
-                        schemas['data']['table_alias'] = "data"
-
-                    # No need to split the repeats down
-                    schemas['data']['headers'].append(child)
-            response_data = [
-                v for k, v in schemas.items()]
-            return Response(data=response_data, status=status.HTTP_200_OK)
-
+            self.xform = self.object.content_object
+            xform_json = json.loads(self.xform.json)
+            headers = self.xform.get_headers(repeat_iterations=1)
+            self.flatten_xform_columns(xform_json.get('children'))
+            self.xform_headers = self.clean_xform_headers(headers)
+            column_headers = self.get_tableau_column_headers()
+            data = self.get_tableau_table_schemas(column_headers)
+            return Response(data=data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_404_NOT_FOUND)
