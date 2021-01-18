03be3dd4022ac9bffe35723d51ab64867f8e0b31
RayceeM <mwatelaraycee33@gmail.com>
2021-01-18 17:02:46 +0300

    Pass file_url

--- onadata/libs/utils/presigned_download_url.py
+++ onadata/libs/utils/presigned_download_url.py
@@ -5,7 +5,7 @@ from botocore.exceptions import ClientError
 from django.core.files.storage import get_storage_class
 
 
-def generate_presigned_download_url(file_path: str, expiration: int = 3600):
+def generate_presigned_download_url(file_path: str, file_url: str, expiration: int = 3600):
  # Generate a presigned URL for the S3 object
     s3 = get_storage_class('storages.backends.s3boto3.S3Boto3Storage')()
     bucket_name = s3.bucket.name
