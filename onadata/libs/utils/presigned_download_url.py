02960ba1a1450cc4ffe5eb091124bb6046948263
RayceeM <mwatelaraycee33@gmail.com>
2021-01-18 17:04:32 +0300

    Check if default storage is equal to s3 storage and return s3 pre-signed url

--- onadata/libs/utils/presigned_download_url.py
+++ onadata/libs/utils/presigned_download_url.py
@@ -1,28 +1,34 @@
-import logging
 import boto3
 from botocore.exceptions import ClientError
+import logging
 
 from django.core.files.storage import get_storage_class
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from wsgiref.util import FileWrapper
 
 
-def generate_presigned_download_url(file_path: str, file_url: str, expiration: int = 3600):
- # Generate a presigned URL for the S3 object
+def generate_media_download_url(file_path: str, expiration: int = 3600):
     default_storage = get_storage_class()()
     filename = file_path.split("/")[-1]
     s3 = get_storage_class('storages.backends.s3boto3.S3Boto3Storage')()
-    bucket_name = s3.bucket.name
-    s3_client = boto3.client('s3')
 
-    try:
-        response = s3_client.generate_presigned_url('get_object',
-                                                    Params={'Bucket': bucket_name,
-                                                            'Key': file_path,
-                                                            'ResponseContentDisposition': 'attachment;filename={}'.format(filename),
-                                                            'ResponseContentType': 'application/octet-stream',},
-                                                    ExpiresIn=expiration)
-    except ClientError as e:
-        logging.error(e)
-        return None
+    if default_storage.__class__ != s3.__class__:
+        return file_url
+    else:
+        try:
+            bucket_name = s3.bucket.name
+            s3_client = boto3.client('s3')
+            response = s3_client.generate_presigned_url('get_object',
+                                                        Params={'Bucket': bucket_name,
+                                                                'Key': file_path,
+                                                                'ResponseContentDisposition': 'attachment;filename={}'.format(filename),
+                                                                'ResponseContentType': 'application/octet-stream', },
+                                                        ExpiresIn=expiration)
+        except ClientError as e:
+            logging.error(e)
+            return None
 
-    # The response contains the presigned URL
-    return response
+        # The response contains the presigned URL
+        return response
