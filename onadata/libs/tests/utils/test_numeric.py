02d7e5323c79e887fd85f8e81e76514274c3775b
Mark Ekisa <mark.ekisa@gmail.com>
2021-01-20 09:37:40 +0300

    Update error msgs in tests and add new test
    Signed-off-by: Mark Ekisa <mark.ekisa@gmail.com>

--- onadata/libs/tests/utils/test_numeric.py
+++ onadata/libs/tests/utils/test_numeric.py
@@ -4,24 +4,28 @@ from rest_framework.exceptions import ParseError
 
 
 class TestNumeric(TestCase):
+    def test_int_or_parse_error_with_valid_value(self):
+        valid_val = "100"
+        returned_val = int_or_parse_error(
+            valid_val, u"Invalid value for formid"
+        )
+
+        self.assertIsNone(returned_val)
+
     def test_int_or_parse_error_with_url(self):
         url = "http://api.ona.iovrocndwm.detectify.io"
         with self.assertRaises(ParseError) as err:
-            int_or_parse_error(url, u"Invalid value for formid %s.")
+            int_or_parse_error(url, u"Invalid value for formid")
 
         self.assertEqual(
-            err.exception.args[0],
-            ('Invalid value for formid '
-             'http%3A//api.ona.iovrocndwm.detectify.io.')
+            err.exception.args[0], 'Invalid value for formid'
         )
 
     def test_int_or_parse_error_with_html_str(self):
         html_str = "<p>thisishtml<p>"
         with self.assertRaises(ParseError) as err:
-            int_or_parse_error(html_str, u"Invalid value for formid %s.")
+            int_or_parse_error(html_str, u"Invalid value for formid")
 
         self.assertEqual(
-            err.exception.args[0],
-            ('Invalid value for formid '
-             '&lt;p&gt;thisishtml&lt;p&gt;.')
+            err.exception.args[0], 'Invalid value for formid'
         )
