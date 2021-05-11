df68cd44c62038150dc0d2ac60131900e0ff4e5e
Davis Raymond Muro <davisraymondmuro@outlook.com>
2021-05-11 16:15:50 +0300

    Modify CSV Import to be fully asynchronous

--- onadata/apps/api/viewsets/xform_viewset.py
+++ onadata/apps/api/viewsets/xform_viewset.py
@@ -628,26 +628,19 @@ class XFormViewSet(AnonymousUserPublicFormsMixin,
                 overwrite = request.query_params.get('overwrite')
                 overwrite = True \
                     if overwrite and overwrite.lower() == 'true' else False
-                size_threshold = settings.CSV_FILESIZE_IMPORT_ASYNC_THRESHOLD
-                try:
-                    csv_size = csv_file.size
-                except AttributeError:
-                    csv_size = csv_file.__sizeof__()
-                if csv_size < size_threshold:
-                    resp.update(submit_csv(request.user.username,
-                                           self.object, csv_file, overwrite))
+
+                csv_file.seek(0)
+                upload_to = os.path.join(
+                    request.user.username, 'csv_imports', csv_file.name)
+                file_name = default_storage.save(upload_to, csv_file)
+                task = submit_csv_async.delay(
+                    request.user.username, self.object.pk, file_name,
+                    overwrite)
+
+                if task is None:
+                    raise ParseError('Task not found')
                 else:
-                    csv_file.seek(0)
-                    upload_to = os.path.join(request.user.username,
-                                             'csv_imports', csv_file.name)
-                    file_name = default_storage.save(upload_to, csv_file)
-                    task = submit_csv_async.delay(request.user.username,
-                                                  self.object.pk, file_name,
-                                                  overwrite)
-                    if task is None:
-                        raise ParseError('Task not found')
-                    else:
-                        resp.update({u'task_id': task.task_id})
+                    resp.update({u'task_id': task.task_id})
 
         return Response(
             data=resp,
@@ -685,22 +678,18 @@ class XFormViewSet(AnonymousUserPublicFormsMixin,
                 overwrite = request.query_params.get('overwrite')
                 overwrite = True \
                     if overwrite and overwrite.lower() == 'true' else False
-                size_threshold = settings.CSV_FILESIZE_IMPORT_ASYNC_THRESHOLD
-                if csv_file.size < size_threshold:
-                    resp.update(submit_csv(request.user.username,
-                                           self.object, csv_file, overwrite))
+
+                csv_file.seek(0)
+                upload_to = os.path.join(
+                    request.user.username, 'csv_imports', csv_file.name)
+                file_name = default_storage.save(upload_to, csv_file)
+                task = submit_csv_async.delay(
+                    request.user.username, self.object.pk, file_name,
+                    overwrite)
+                if task is None:
+                    raise ParseError('Task not found')
                 else:
-                    csv_file.seek(0)
-                    upload_to = os.path.join(request.user.username,
-                                             'csv_imports', csv_file.name)
-                    file_name = default_storage.save(upload_to, csv_file)
-                    task = submit_csv_async.delay(request.user.username,
-                                                  self.object.pk, file_name,
-                                                  overwrite)
-                    if task is None:
-                        raise ParseError('Task not found')
-                    else:
-                        resp.update({u'task_id': task.task_id})
+                    resp.update({u'task_id': task.task_id})
 
         return Response(
             data=resp,
