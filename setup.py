b8be01c43d2c4d37d202be65eac3628da18ac6f2
Davis Raymond Muro <davisraymondmuro@outlook.com>
2021-05-18 08:38:31 +0300

    Remove version lock on Celery dependency

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "django-ordered-model",
         # generic relation
         "django-query-builder",
-        "celery<5",
+        "celery",
         # cors
         "django-cors-headers",
         "django-debug-toolbar",
