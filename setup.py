b92b165427670c44690d5c94549002070ee00c70
Davis Raymond Muro <davisraymondmuro@outlook.com>
2021-06-02 11:59:41 +0300

    Add `django-redis` as a requirement

--- setup.py
+++ setup.py
@@ -115,6 +115,8 @@ setup(
         "appoptics-metrics",
         # Deprecation tagging
         "deprecated",
+        # Redis cache
+        "django-redis",
     ],
     dependency_links=[
         'https://github.com/onaio/python-digest/tarball/3af1bd0ef6114e24bf23d0e8fd9d7ebf389845d1#egg=python-digest',  # noqa pylint: disable=line-too-long
