40dfe17ad75b797ea1cef13f627ff6482b0118b6
WinnyTroy <kiraguwinnie@gmail.com>
2021-07-23 10:54:01 +0300

    Create oauth2 token storage model for google auth credentials persistent storage

---
+++ onadata/apps/api/models/oauthtokenstoage.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Oauth2 token storage model class
+"""
+from django.db import models
+from django.conf import settings
+
+
+class OauthStorageModel(models.Model):
+    """
+    Oauth2 Token storage model
+    """
+
+    id = models.OneToOneField(
+        settings.AUTH_USER_MODEL, primary_key=True,
+        on_delete=models.CASCADE)
+    credential = models.JSONField(null=True)
+
+    class Meta:
+        app_label = 'api'
