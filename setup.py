0027f5ba890c0fb96a4e5c92386341aada451b9f
Davis Raymond Muro <davisraymondmuro@outlook.com>
2021-08-30 13:00:45 +0300

    Add `django-prometheus` requirement

--- setup.py
+++ setup.py
@@ -117,6 +117,8 @@ setup(
         "deprecated",
         # Redis cache
         "django-redis",
+        # Prometheus
+        "django-prometheus",
     ],
     dependency_links=[
         'https://github.com/onaio/python-digest/tarball/3af1bd0ef6114e24bf23d0e8fd9d7ebf389845d1#egg=python-digest',  # noqa pylint: disable=line-too-long
