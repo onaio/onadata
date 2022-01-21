9b5685e006a53b41f6ebf971e34b20104cfd6105
Kipchirchir Sigei <arapgodsmack@gmail.com>
2022-01-21 16:58:40 +0300

    Add missing docstrings
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- onadata/apps/api/management/commands/create_user_profiles.py
+++ onadata/apps/api/management/commands/create_user_profiles.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Management Command to add missing user profiles to users
-"""
-
+"""Management Command to add missing user profiles to users."""
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
@@ -12,19 +9,17 @@ from onadata.libs.utils.model_tools import queryset_iterator
 
 
 class Command(BaseCommand):
+    """Create missing user profiles management command."""
     help = _("Build out missing user profiles")
 
     def handle(self, *args, **options):
         # get all users
-        try:
-            users = User.objects.all()
-            for user in queryset_iterator(users):
-                try:
-                    profile = user.profile
-                except UserProfile.DoesNotExist:
-                    profile, _ = UserProfile.objects.get_or_create(user=user)
-                    profile.save()
-        except User.DoesNotExist:
-            pass
+        users = User.objects.all()
+        for user in queryset_iterator(users):
+            try:
+                profile = user.profile
+            except UserProfile.DoesNotExist:
+                profile, _ = UserProfile.objects.get_or_create(user=user)
+                profile.save()
 
         self.stdout.write("User Profiles successfully created.")
--- onadata/apps/api/tests/management/commands/test_create_user_profiles.py
+++ onadata/apps/api/tests/management/commands/test_create_user_profiles.py
@@ -1,13 +1,20 @@
+# -*- coding: utf-8 -*-
+"""Test create user profile management command."""
 from django.contrib.auth.models import User
 from onadata.apps.main.models.user_profile import UserProfile
 from django.core.management import call_command
 from django.utils.six import StringIO
-
 from onadata.apps.main.tests.test_base import TestBase
 
 
 class CreateUserProfilesTest(TestBase):
+    """Test create user profile management command."""
     def test_create_user_profiles(self):
+        """
+        Test that create_user_profiles management command
+        successfully creates a user profile for users
+        missing profiles.
+        """
         user = User.objects.create(
             username='dave', email='dave@example.com')
         with self.assertRaises(UserProfile.DoesNotExist):
