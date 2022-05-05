35c4a1b97f5fce84a7feb3d9bc28239bfbd55d15
Ukang'a Dickson <ukanga@users.noreply.github.com>
2022-05-05 23:56:00 +0300

    exceptions.py: cleanup

--- onadata/libs/exceptions.py
+++ onadata/libs/exceptions.py
@@ -1,35 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Custom Expecting classes."""
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework.exceptions import APIException
 
 
 class EnketoError(Exception):
+    """Enketo specigic exceptions"""
 
-    default_message = _("There was a problem with your submissionor"
-                        " form. Please contact support.")
+    default_message = _(
+        "There was a problem with your submission or form. Please contact support."
+    )
 
     def __init__(self, message=None):
-        if message is None:
-            self.message = self.default_message
-        else:
-            self.message = message
-
-    def __str__(self):
-        return "{}".format(self.message)
+        self.message = message if message is not None else self.default_message
+        super().__init__(self.message)
 
 
 class NoRecordsFoundError(Exception):
-    pass
+    """Raise for when no records are found."""
 
 
 class NoRecordsPermission(Exception):
-    pass
+    """Raise when no permissions to access records."""
 
 
 class J2XException(Exception):
-    pass
+    """Raise for json-to-xls exceptions on external exports."""
 
 
 class ServiceUnavailable(APIException):
+    """Custom service unavailable exception."""
+
     status_code = 503
-    default_detail = 'Service temporarily unavailable, try again later.'
+    default_detail = "Service temporarily unavailable, try again later."
