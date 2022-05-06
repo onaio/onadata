660f96278ffc1cac52d666d6c21a84065e872778
Ukang'a Dickson <ukanga@users.noreply.github.com>
2022-05-07 00:12:42 +0300

    admin.py: cleanup

--- onadata/apps/api/admin.py
+++ onadata/apps/api/admin.py
@@ -5,8 +5,9 @@ from django.contrib import admin
 from onadata.apps.api.models import Team, OrganizationProfile, TempToken
 
 
-class TeamAdmin(admin.ModelAdmin):
-    """Filter by request.user unless is_superuser."""
+# pylint: disable=too-few-public-methods
+class FilterSuperuserMixin:
+    """Filter by request user and give full access to superuser."""
 
     def get_queryset(self, request):
         """Filter by request.user unless is_superuser."""
@@ -14,34 +15,24 @@ class TeamAdmin(admin.ModelAdmin):
         if request.user.is_superuser:
             return queryset
         return queryset.filter(user=request.user)
+
+
+class TeamAdmin(FilterSuperuserMixin, admin.ModelAdmin):
+    """Filter by request.user unless is_superuser."""
 
 
 admin.site.register(Team, TeamAdmin)
 
 
-class OrganizationProfileAdmin(admin.ModelAdmin):
+class OrganizationProfileAdmin(FilterSuperuserMixin, admin.ModelAdmin):
     """Filter by request.user unless is_superuser."""
-
-    def get_queryset(self, request):
-        """Filter by request.user unless is_superuser."""
-        queryset = super().get_queryset(request)
-        if request.user.is_superuser:
-            return queryset
-        return queryset.filter(user=request.user)
 
 
 admin.site.register(OrganizationProfile, OrganizationProfileAdmin)
 
 
-class TempTokenProfileAdmin(admin.ModelAdmin):
+class TempTokenProfileAdmin(FilterSuperuserMixin, admin.ModelAdmin):
     """Filter by request.user unless is_superuser."""
-
-    def get_queryset(self, request):
-        """Filter by request.user unless is_superuser."""
-        queryset = super().get_queryset(request)
-        if request.user.is_superuser:
-            return queryset
-        return queryset.filter(user=request.user)
 
 
 admin.site.register(TempToken, TempTokenProfileAdmin)
