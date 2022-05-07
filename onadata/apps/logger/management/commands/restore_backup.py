b4af3d98df8bf47c1f7bb65ca66868199675ce2c
Ukang'a Dickson <ukanga@users.noreply.github.com>
2022-05-07 05:47:03 +0300

    restore_backup.py: cleanup

--- onadata/apps/logger/management/commands/restore_backup.py
+++ onadata/apps/logger/management/commands/restore_backup.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+restore_backup command - Restore a zip backup of a form and all its submissions
+"""
 import os
 import sys
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import gettext_lazy, gettext as _
 
@@ -9,21 +13,27 @@ from onadata.libs.utils.backup_tools import restore_backup_from_zip
 
 
 class Command(BaseCommand):
+    """
+    restore_backup command - Restore a zip backup of a form and all its submissions
+    """
+
     args = "username input_file"
-    help = gettext_lazy("Restore a zip backup of a form and all its" " submissions")
+    help = gettext_lazy("Restore a zip backup of a form and all its submissions")
 
     def handle(self, *args, **options):
+        # pylint: disable=invalid-name
+        User = get_user_model()
         try:
             username = args[0]
         except IndexError:
             raise CommandError(
-                _("You must provide the username to publish the" " form to.")
+                _("You must provide the username to publish the form to.")
             )
             # make sure user exists
         try:
             User.objects.get(username=username)
         except User.DoesNotExist:
-            raise CommandError(_("The user '%s' does not exist.") % username)
+            raise CommandError(_(f"The user '{username}' does not exist."))
 
         try:
             input_file = args[1]
@@ -33,6 +43,4 @@ class Command(BaseCommand):
             input_file = os.path.realpath(input_file)
 
         num_instances, num_restored = restore_backup_from_zip(input_file, username)
-        sys.stdout.write(
-            "Restored %d of %d submissions\n" % (num_restored, num_instances)
-        )
+        sys.stdout.write(f"Restored {num_restored} of {num_instances } submissions\n")
