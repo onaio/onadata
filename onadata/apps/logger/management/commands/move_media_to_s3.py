7dba4eae8fd6a0117ac68b50c66a8667122f029a
Ukang'a Dickson <ukanga@users.noreply.github.com>
2022-05-07 15:27:34 +0300

    move_media_to_s3.py: cleanup

--- onadata/apps/logger/management/commands/move_media_to_s3.py
+++ onadata/apps/logger/management/commands/move_media_to_s3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+move_media_to_s3 - Moves all XLSForm file from local storage to S3 storage.
+"""
 import sys
 
 from django.core.files.storage import get_storage_class
@@ -11,26 +15,20 @@ from onadata.apps.logger.models.xform import XForm, upload_to as xform_upload_to
 
 
 class Command(BaseCommand):
+    """Moves all XLSForm file from local storage to S3 storage."""
+
     help = gettext_lazy(
         "Moves all attachments and xls files "
         "to s3 from the local file system storage."
     )
 
     def handle(self, *args, **kwargs):
-        try:
-            fs = get_storage_class("django.core.files.storage.FileSystemStorage")()
-            s3 = get_storage_class("storages.backends.s3boto.S3BotoStorage")()
-        except Exception:
-            self.stderr.write(
-                _(
-                    "Missing necessary libraries. Try running: pip install -r"
-                    "requirements/s3.pip"
-                )
-            )
-            sys.exit(1)
+        """Moves all XLSForm file from local storage to S3 storage."""
+        local_fs = get_storage_class("django.core.files.storage.FileSystemStorage")()
+        s3_fs = get_storage_class("storages.backends.s3boto.S3BotoStorage")()
 
         default_storage = get_storage_class()()
-        if default_storage.__class__ != s3.__class__:
+        if default_storage.__class__ != s3_fs.__class__:
             self.stderr.write(
                 _(
                     "You must first set your default storage to s3 in your "
@@ -49,19 +47,20 @@ class Command(BaseCommand):
             for i in cls.objects.all():
                 f = getattr(i, file_field)
                 old_filename = f.name
-                if f.name and fs.exists(f.name) and not s3.exists(upload_to(i, f.name)):
-                    f.save(fs.path(f.name), fs.open(fs.path(f.name)))
+                if (
+                    f.name
+                    and local_fs.exists(f.name)
+                    and not s3_fs.exists(upload_to(i, f.name))
+                ):
+                    f.save(local_fs.path(f.name), local_fs.open(local_fs.path(f.name)))
                     self.stdout.write(
                         _("\t+ '%(fname)s'\n\t---> '%(url)s'")
-                        % {"fname": fs.path(old_filename), "url": f.url}
+                        % {"fname": local_fs.path(old_filename), "url": f.url}
                     )
                 else:
+                    exists_locally = local_fs.exists(f.name)
+                    exists_s3 = not s3_fs.exists(upload_to(i, f.name))
                     self.stderr.write(
-                        "\t- (f.name=%s, fs.exists(f.name)=%s, not s3.exist"
-                        "s(upload_to(i, f.name))=%s)"
-                        % (
-                            f.name,
-                            fs.exists(f.name),
-                            not s3.exists(upload_to(i, f.name)),
-                        )
+                        f"\t- (f.name={f.name}, fs.exists(f.name)={exists_locally},"
+                        f" not s3.exist s3upload_to(i, f.name))={exists_s3})"
                     )
