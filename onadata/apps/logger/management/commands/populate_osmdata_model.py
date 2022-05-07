142333302a698aeac91c96fb0faeee8cb4495898
Ukang'a Dickson <ukanga@users.noreply.github.com>
2022-05-07 16:00:00 +0300

    populate_osmdata_model.py: cleanup

--- onadata/apps/logger/management/commands/populate_osmdata_model.py
+++ onadata/apps/logger/management/commands/populate_osmdata_model.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+populate_osmdata_model command - process OSM XML and save data in OsmData model/table.
+"""
 from django.utils.translation import gettext_lazy
 from django.core.management.base import BaseCommand
 
@@ -8,10 +12,14 @@ from onadata.libs.utils.osm import save_osm_data
 
 
 class Command(BaseCommand):
-    args = '<username>'
+    """Populate OsmData model with osm info."""
+
+    args = "<username>"
     help = gettext_lazy("Populate OsmData model with osm info.")
 
+    # pylint: disable=unused-argument
     def handle(self, *args, **kwargs):
+        """Populate OsmData model with osm info."""
         xforms = XForm.objects.filter(instances_with_osm=True)
 
         # username
@@ -20,20 +28,22 @@ class Command(BaseCommand):
 
         for xform in queryset_iterator(xforms):
             attachments = Attachment.objects.filter(
-                extension=Attachment.OSM,
-                instance__xform=xform
-            ).distinct('instance')
+                extension=Attachment.OSM, instance__xform=xform
+            ).distinct("instance")
 
             count = attachments.count()
-            c = 0
-            for a in queryset_iterator(attachments):
-                pk = a.instance.parsed_instance.pk
-                save_osm_data(pk)
-                c += 1
-                if c % 1000 == 0:
-                    self.stdout.write("%s:%s: Processed %s of %s." %
-                                      (xform.user.username,
-                                       xform.id_string, c, count))
+            counter = 0
+            for attachment in queryset_iterator(attachments):
+                instance_pk = attachment.instance.parsed_instance.pk
+                save_osm_data(instance_pk)
+                counter += 1
+                if counter % 1000 == 0:
+                    self.stdout.write(
+                        f"{xform.user.username}:{xform.id_string}: "
+                        f"Processed {counter} of {count}."
+                    )
 
-            self.stdout.write("%s:%s: processed %s of %s submissions." %
-                              (xform.user.username, xform.id_string, c, count))
+            self.stdout.write(
+                f"{xform.user.username}:{xform.id_string}: "
+                f"processed {counter} of {count} submissions."
+            )
