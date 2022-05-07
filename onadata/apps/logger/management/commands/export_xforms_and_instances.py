9f48aed09a58b471e9d0b4e3b1373aff9a678dc7
Ukang'a Dickson <ukanga@users.noreply.github.com>
2022-05-07 13:46:52 +0300

    export_xforms_and_instances.py: cleanup

--- onadata/apps/logger/management/commands/export_xforms_and_instances.py
+++ onadata/apps/logger/management/commands/export_xforms_and_instances.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4 coding=utf-8
+# vim: ai ts=4 sts=4 et sw=4
+# -*- coding=utf-8 -*-
+"""
+export_xformx_and_instances - exports XForms and submission instances into JSON files.
+"""
 import os
 
 from django.core.management.base import BaseCommand
@@ -12,9 +16,12 @@ PROJECT_ROOT = settings.PROJECT_ROOT
 
 
 class Command(BaseCommand):
+    """Export ODK forms and instances to JSON."""
+
     help = gettext_lazy("Export ODK forms and instances to JSON.")
 
     def handle(self, *args, **kwargs):
+        """Export ODK forms and instances to JSON."""
         fixtures_dir = os.path.join(PROJECT_ROOT, "json_xform_fixtures")
         if not os.path.exists(fixtures_dir):
             os.mkdir(fixtures_dir)
@@ -22,10 +29,10 @@ class Command(BaseCommand):
         xform_fp = os.path.join(fixtures_dir, "a-xforms.json")
         instance_fp = os.path.join(fixtures_dir, "b-instances.json")
 
-        xfp = open(xform_fp, 'w')
-        xfp.write(serialize("json", XForm.objects.all()))
-        xfp.close()
+        with open(xform_fp, "w", encoding="utf-8") as xfp:
+            xfp.write(serialize("json", XForm.objects.all()))
+            xfp.close()
 
-        ifp = open(instance_fp, 'w')
-        ifp.write(serialize("json", Instance.objects.all()))
-        ifp.close()
+        with open(instance_fp, "w", encoding="utf-8") as ifp:
+            ifp.write(serialize("json", Instance.objects.all()))
+            ifp.close()
