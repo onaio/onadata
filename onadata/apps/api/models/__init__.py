c87a51bfab0d7287d8c037553c36bb9cbc79e1a9
Ukang'a Dickson <ukanga@users.noreply.github.com>
2022-05-08 18:32:58 +0300

    __init__.py cleanup

--- onadata/apps/api/models/__init__.py
+++ onadata/apps/api/models/__init__.py
@@ -2,9 +2,8 @@
 """
 API models.
 """
+# flake8: noqa
 
-from onadata.apps.api.models.organization_profile import (
-    OrganizationProfile,
-)  # noqa F401
-from onadata.apps.api.models.team import Team  # noqa F401
-from onadata.apps.api.models.temp_token import TempToken  # noqa F401
+from onadata.apps.api.models.organization_profile import OrganizationProfile  # noqa
+from onadata.apps.api.models.team import Team  # noqa
+from onadata.apps.api.models.temp_token import TempToken  # noqa
