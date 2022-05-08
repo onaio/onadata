cbd3390187d09467cad70598967a5d252aa99ad0
Ukang'a Dickson <ukanga@users.noreply.github.com>
2022-05-08 17:03:33 +0300

    clone_xform.py: cleanup

--- onadata/libs/models/clone_xform.py
+++ onadata/libs/models/clone_xform.py
@@ -1,33 +1,40 @@
-from django.contrib.auth.models import User
-from onadata.apps.viewer.models.data_dictionary import \
-    DataDictionary, upload_to
+# -*- coding: utf-8 -*-
+"""
+CloneXForm class model.
+"""
+from django.contrib.auth import get_user_model
 from django.core.files.storage import default_storage
+
 from onadata.apps.logger.models.xform import XForm
+from onadata.apps.viewer.models.data_dictionary import DataDictionary, upload_to
 from onadata.libs.utils.user_auth import get_user_default_project
 
 
-class CloneXForm(object):
+class CloneXForm:
+    """The class takes an existing form's XLSForm and publishes it as a new form."""
+
     def __init__(self, xform, username, project=None):
         self.xform = xform
         self.username = username
         self.project = project
+        self.cloned_form = None
 
     @property
     def user(self):
-        return User.objects.get(username=self.username)
+        """Returns a User object for the given ``self.username``."""
+        return get_user_model().objects.get(username=self.username)
 
     def save(self, **kwargs):
-        user = User.objects.get(username=self.username)
+        """Publishes an exiting form's XLSForm as a new form."""
+        user = self.user
         project = self.project or get_user_default_project(user)
-        xls_file_path = upload_to(None, '%s%s.xlsx' % (
-                                  self.xform.id_string,
-                                  XForm.CLONED_SUFFIX),
-                                  self.username)
+        xls_file_path = upload_to(
+            None,
+            f"{self.xform.id_string}{XForm.CLONED_SUFFIX}.xlsx",
+            self.username,
+        )
         xls_data = default_storage.open(self.xform.xls.name)
         xls_file = default_storage.save(xls_file_path, xls_data)
         self.cloned_form = DataDictionary.objects.create(
-            user=user,
-            created_by=user,
-            xls=xls_file,
-            project=project
+            user=user, created_by=user, xls=xls_file, project=project
         )
--- onadata/libs/serializers/clone_xform_serializer.py
+++ onadata/libs/serializers/clone_xform_serializer.py
@@ -1,38 +1,53 @@
-from django.contrib.auth.models import User
+# -*- coding: utf-8 -*-
+"""
+Clone an XForm serializer.
+"""
+from django.contrib.auth import get_user_model
 from django.utils.translation import gettext as _
 
 from rest_framework import serializers
+
 from onadata.libs.models.clone_xform import CloneXForm
-from onadata.libs.serializers.fields.xform_field import XFormField
 from onadata.libs.serializers.fields.project_field import ProjectField
+from onadata.libs.serializers.fields.xform_field import XFormField
 
 
 class CloneXFormSerializer(serializers.Serializer):
+    """Clone an xform serializer class"""
+
     xform = XFormField()
     username = serializers.CharField(max_length=255)
     project = ProjectField(required=False)
 
+    # pylint: disable=no-self-use
     def create(self, validated_data):
+        """Uses  the CloneXForm class to clone/copy an XForm.
+
+        Returns the CloneXForm instance."""
         instance = CloneXForm(**validated_data)
         instance.save()
 
         return instance
 
+    # pylint: disable=no-self-use
     def update(self, instance, validated_data):
-        instance.xform = validated_data.get('xform', instance.xform)
-        instance.username = validated_data.get('username', instance.username)
-        instance.project = validated_data.get('project', instance.project)
+        instance.xform = validated_data.get("xform", instance.xform)
+        instance.username = validated_data.get("username", instance.username)
+        instance.project = validated_data.get("project", instance.project)
         instance.save()
 
         return instance
 
+    # pylint: disable=no-self-use
     def validate_username(self, value):
         """Check that the username exists"""
+        # pylint: disable=invalid-name
+        User = get_user_model()  # noqa N806
         try:
             User.objects.get(username=value)
-        except User.DoesNotExist:
-            raise serializers.ValidationError(_(
-                u"User '%(value)s' does not exist." % {"value": value}
-            ))
+        except User.DoesNotExist as e:
+            raise serializers.ValidationError(
+                _(f"User '{value}' does not exist.")
+            ) from e
 
         return value
