4f01f789502189784e39a0a9f8ff06aed63b834f
Davis Raymond Muro <davisraymondmuro@outlook.com>
2022-05-09 14:40:28 +0300

    Use correct scopes when creating flow object

--- onadata/libs/utils/google.py
+++ onadata/libs/utils/google.py
@@ -7,10 +7,11 @@ from google_auth_oauthlib.flow import Flow
 def create_flow(redirect_uri: Optional[str] = None) -> Flow:
     return Flow.from_client_config(
         settings.GOOGLE_FLOW,
-        scopes=['https://docs.google.com/feeds/',
-            'https://spreadsheets.google.com/feeds/',
+        scopes=['https://www.googleapis.com/auth/spreadsheets',
+            'https://www.googleapis.com/auth/docs',
             'https://www.googleapis.com/auth/drive.file'],
         redirect_uri=redirect_uri or settings.GOOGLE_STEP2_URI)
+
 # google_flow = OAuth2WebServerFlow(
 #     client_id=settings.GOOGLE_OAUTH2_CLIENT_ID,
 #     client_secret=settings.GOOGLE_OAUTH2_CLIENT_SECRET,
