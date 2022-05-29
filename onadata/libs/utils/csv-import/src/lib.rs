304e8446203ca09b70040ae00b2525a0706dfaeb
Davis Raymond Muro <davisraymondmuro@outlook.com>
2022-05-29 04:54:49 +0300

    Update code to package the library into a python module

--- onadata/libs/utils/csv-import/src/lib.rs
+++ onadata/libs/utils/csv-import/src/lib.rs
@@ -1,77 +1,78 @@
-use pyo3::prelude::*;
-use std::collections::{HashMap, HashSet};
-use std::fmt;
-use std::fs;
-use std::ops::ControlFlow;
+use std::collections::HashMap;
 
-use csv;
-
-const IGNORED_COLUMNS: Vec<&str> = vec![];
+use pyo3::{exceptions::PyValueError, prelude::*, types::PyDict};
 
 #[derive(Debug, Clone)]
 struct InvalidCSVFile {
     msg: String,
 }
 
-impl fmt::Display for InvalidCSVFile {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl InvalidCSVFile {
+    fn new(msg: &str) -> Self {
+        InvalidCSVFile {
+            msg: msg.to_string(),
+        }
+    }
+}
+
+impl std::fmt::Display for InvalidCSVFile {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "CSV File failed validation: {}", self.msg)
     }
 }
 
-fn validate_csv_file(
-    csv_path: &String,
-    columns_with_type: &HashMap<String, String>,
-) -> Result<Vec<String>, InvalidCSVFile> {
-    let mut reader: csv::Reader<fs::File> =
-        csv::Reader::from_path(csv_path).map_err(|_| InvalidCSVFile {
-            msg: String::from("Failed to read CSV File"),
-        })?;
-    let mut missing_headers: Vec<String> = Vec::new();
+impl std::convert::From<InvalidCSVFile> for PyErr {
+    fn from(err: InvalidCSVFile) -> Self {
+        PyValueError::new_err(err.to_string())
+    }
+}
 
-    let headers = reader.headers().map_err(|_| InvalidCSVFile {
-        msg: String::from("Failed to read CSV Headers"),
-    })?;
+#[pyfunction]
+pub fn validate_csv_file(
+    csv_path: String,
+    columns_with_type: HashMap<String, String>,
+) -> PyResult<Vec<String>> {
+    let mut reader = csv::Reader::from_path(csv_path)
+        .map_err(|_| InvalidCSVFile::new("Failed to open CSV File"))?;
 
-    let headers: Vec<String> = headers.iter().map(|record| record.to_string()).collect();
+    let headers: Vec<String> = reader
+        .headers()
+        .map_err(|_| InvalidCSVFile::new("Failed to read headers"))?
+        .iter()
+        .map(|header| header.to_string())
+        .collect();
 
-    columns_with_type.iter().for_each(|(key, value)| {
-        if !headers.contains(key) && value != "select all that apply" {
-            missing_headers.push(key.to_string());
-        }
-    });
+    let missing_headers: Vec<String> = columns_with_type
+        .iter()
+        .filter_map(|(key, value)| {
+            if !headers.contains(key) && value != "select all that apply" {
+                Some(key.to_owned())
+            } else {
+                None
+            }
+        })
+        .collect();
 
     if missing_headers.len() > 0 {
-        return Err(InvalidCSVFile {
-            msg: format!("Missing headers: {:?}", missing_headers.to_vec()),
-        });
+        return Err(PyValueError::new_err(format!(
+            "Imported CSV is missing the following fields: {}",
+            missing_headers.join(", ")
+        )));
     }
 
-    let mut additional_columns = Vec::new();
-
-    headers.iter().for_each(|header| {
-        if !columns_with_type.contains_key(header) && !header.to_string().contains("[") {
-            additional_columns.push(header.to_string())
+    let additional_columns: Vec<String> = headers.iter().filter_map(|header| {
+        if !columns_with_type.contains_key(header) && !header.contains("[") {
+            Some(header.to_owned())
+        } else {
+            None
         }
-    });
+    }).collect();
 
     Ok(additional_columns)
 }
 
-fn import_csv(
-    csv_path: String,
-    columns_with_type: HashMap<String, String>,
-) -> Result<(), InvalidCSVFile> {
-    let additional_columns = validate_csv_file(&csv_path, &columns_with_type)?;
-
-    todo!()
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+#[pymodule]
+fn import_helper(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(validate_csv_file, m)?)?;
+    Ok(())
 }
