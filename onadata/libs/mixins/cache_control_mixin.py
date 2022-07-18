1b1842f4de035ad43ed7fe43b1c7a58ac66580ee
Davis Raymond Muro <davisraymondmuro@outlook.com>
2022-07-18 11:58:39 +0300

    Update CacheControlMixin to support different values i.e no-store

--- onadata/libs/mixins/cache_control_mixin.py
+++ onadata/libs/mixins/cache_control_mixin.py
@@ -7,32 +7,36 @@ Adds Cache headers to a viewsets response.
 from django.conf import settings
 from django.utils.cache import patch_cache_control
 
-CACHE_MIXIN_SECONDS = 60
+
+CACHE_CONTROL_VALUE = "max-age=60"
 
 
-class CacheControlMixin:
-    """
-    Implements the CacheControlMixin class
+def set_cache_control(response, cache_control_value=CACHE_CONTROL_VALUE):
+    pragma = None
+    if hasattr(settings, 'CACHE_CONTROL_VALUE'):
+        cache_control_value = settings.CACHE_CONTROL_VALUE
+    if hasattr(settings, 'PRAGMA_VALUE'):
+        pragma = settings.PRAGMA_VALUE
+    response['Cache-Control'] = cache_control_value
+    if pragma:
+        response['Pragma'] = pragma
+    return response
 
-    Adds Cache headers to a viewsets response.
-    """
 
-    def set_cache_control(self, response, max_age=CACHE_MIXIN_SECONDS):
-        """Adds Cache headers to a response"""
-        if hasattr(settings, "CACHE_MIXIN_SECONDS"):
-            max_age = settings.CACHE_MIXIN_SECONDS
-
-        patch_cache_control(response, max_age=max_age)
-
+class CacheControlMixin(object):
     def finalize_response(self, request, response, *args, **kwargs):
-        """Overrides the finalize_response method
+        if request.method == 'GET' and not response.streaming and \
+                response.status_code in [200, 201, 202]:
+            response = set_cache_control(response)
 
-        Adds Cache headers to a response."""
-        if (
-            request.method == "GET"
-            and not response.streaming
-            and response.status_code in [200, 201, 202]
-        ):
-            self.set_cache_control(response)
+        return super(CacheControlMixin, self).finalize_response(
+            request, response, *args, **kwargs)
 
-        return super().finalize_response(request, response, *args, **kwargs)
+
+class CacheControlMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return set_cache_control(response)
