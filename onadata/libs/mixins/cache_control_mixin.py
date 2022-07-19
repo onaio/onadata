1f4849532801ad1534b6e57589a832017b6a7683
Davis Raymond Muro <davisraymondmuro@outlook.com>
2022-07-19 11:24:51 +0300

    Add `CacheControlBase` class

--- onadata/libs/mixins/cache_control_mixin.py
+++ onadata/libs/mixins/cache_control_mixin.py
@@ -7,42 +7,49 @@ from django.conf import settings
 from django.utils.cache import patch_cache_control
 
 
-def set_cache_control(response, cache_control_directives: Optional[dict] = None):
+class CacheControlBase:
     """
-    Sets the `Cache-Control` headers on a `Response`
-    Object
+    Base class for Cache Control header handlers
     """
-    cache_control_directives = {"max_age": 60}
-    if hasattr(settings, 'CACHE_CONTROL_DIRECTIVES'):
-        cache_control_directives = settings.CACHE_CONTROL_DIRECTIVES
+    CACHE_CONTROL_DIRECTIVES = {"max_age": 60}
 
-    patch_cache_control(response, **cache_control_directives)
-    return response
+    def set_cache_control(self, response, cache_control_directives: Optional[dict] = None):
+        """
+        Sets the `Cache-Control` headers on a `Response`
+        Object
+        """
+        cache_control_directives = self.CACHE_CONTROL_DIRECTIVES
+        if hasattr(settings, 'CACHE_CONTROL_DIRECTIVES'):
+            cache_control_directives = settings.CACHE_CONTROL_DIRECTIVES
+
+        patch_cache_control(response, **cache_control_directives)
+        return response
 
 
-class CacheControlMixin(object):
+class CacheControlMiddleware(CacheControlBase):
+    """
+    Django Middleware used to set `Cache-Control` header for every response
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return self.set_cache_control(response)
+
+
+class CacheControlMixin(CacheControlBase):
+    """
+    Django Rest Framework ViewSet mixin for Cache-Control
+    """
     def finalize_response(self, request, response, *args, **kwargs):
         if (
             request.method == "GET"
             and not response.streaming
             and response.status_code in [200, 201, 202]
         ):
-            response = set_cache_control(response)
+            response = self.set_cache_control(response)
 
-        return super(CacheControlMixin, self).finalize_response(
+        return super().finalize_response(
             request, response, *args, **kwargs
         )
-
-
-class CacheControlMiddleware:
-    """
-    Django Middleware used to set `Cache-Control`
-    header for every response
-    """
-
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        return set_cache_control(response)
