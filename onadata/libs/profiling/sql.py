bc315c716a2ce73db7e1a886ed51c4178232cfa1
Ukang'a Dickson <ukanga@users.noreply.github.com>
2022-08-20 15:25:55 +0300

    prospector cleanup onadata/libs/profiling

--- onadata/libs/profiling/sql.py
+++ onadata/libs/profiling/sql.py
@@ -6,14 +6,14 @@ import logging
 
 from django.db import connection
 
-sql_log = logging.getLogger('sql_logger')  # pylint: disable=C0103
-totals_log = logging.getLogger('sql_totals_logger')  # pylint: disable=C0103
+SQL_LOG = logging.getLogger("sql_logger")
+TOTALS_LOG = logging.getLogger("sql_totals_logger")
 
 # modified from
 # http://johnparsons.net/index.php/2013/08/15/easy-sql-query-counting-in-django
 
 
-class SqlTimingMiddleware(object):  # pylint: disable=R0903
+class SqlTimingMiddleware:
     """
     Logs the time taken by each sql query over requests.
     Logs the total time taken to run sql queries and the number of sql queries
@@ -24,17 +24,16 @@ class SqlTimingMiddleware(object):  # pylint: disable=R0903
         self.get_response = get_response
 
     def __call__(self, request):
-        path_info = '%s %s' % (request.method, request.path_info)
+        path_info = f"{request.method} {request.path_info}"
         response = self.get_response(request)
         sqltime = 0  # Variable to store execution time
         for query in connection.queries:
             # Add the time that the query took to the total
             sqltime += float(query["time"])
-            sql_log.debug(path_info, extra=query)
+            SQL_LOG.debug(path_info, extra=query)
 
-        totals_log.debug(
-            path_info,
-            extra={'time': sqltime,
-                   'num_queries': len(connection.queries)})
+        TOTALS_LOG.debug(
+            path_info, extra={"time": sqltime, "num_queries": len(connection.queries)}
+        )
 
         return response
