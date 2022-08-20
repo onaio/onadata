f8b952ad0ac8a87201a0c7667d5ef89c05904fe3
Ukang'a Dickson <ukanga@users.noreply.github.com>
2022-08-20 15:22:32 +0300

    prospector cleanup onadata/libs/models

--- onadata/libs/models/sorting.py
+++ onadata/libs/models/sorting.py
@@ -1,12 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Sorting utility functions.
+"""
 import json
-import six
 from typing import Dict
+
+import six
 
 
 def sort_from_mongo_sort_str(sort_str):
+    """Create a sort query list based on MongoDB sort string input."""
     sort_values = []
     if isinstance(sort_str, six.string_types):
-        if sort_str.startswith('{'):
+        if sort_str.startswith("{"):
             sort_dict = json.loads(sort_str)
             for k, v in sort_dict.items():
                 try:
@@ -14,7 +20,7 @@ def sort_from_mongo_sort_str(sort_str):
                 except ValueError:
                     pass
                 if v < 0:
-                    k = u'-{}'.format(k)
+                    k = f"-{k}"
                 sort_values.append(k)
         else:
             sort_values.append(sort_str)
@@ -22,34 +28,38 @@ def sort_from_mongo_sort_str(sort_str):
     return sort_values
 
 
-def json_order_by(
-        sort_list, none_json_fields: Dict = {}, model_name: str = ""):
+def json_order_by(sort_list, none_json_fields: Dict = None, model_name: str = ""):
+    """Returns SQL ORDER BY string portion based on JSON input."""
     _list = []
 
     for field in sort_list:
-        field_key = field.lstrip('-')
-        _str = u" json->>%s"\
-            if field_key not in none_json_fields.keys() else\
-            f'"{model_name}"."{none_json_fields.get(field_key)}"'
+        field_key = field.lstrip("-")
+        _str = (
+            " json->>%s"
+            if none_json_fields and field_key not in none_json_fields.keys()
+            else f'"{model_name}"."{none_json_fields.get(field_key)}"'
+        )
 
-        if field.startswith('-'):
-            _str += u" DESC"
+        if field.startswith("-"):
+            _str += " DESC"
         else:
-            _str += u" ASC"
+            _str += " ASC"
         _list.append(_str)
 
     if len(_list) > 0:
-        return u"ORDER BY {}".format(u",".join(_list))
+        return f'ORDER BY {"".join(_list)}'
 
-    return u""
+    return ""
 
 
-def json_order_by_params(sort_list, none_json_fields: Dict = {}):
+def json_order_by_params(sort_list, none_json_fields: Dict = None):
+    """Creates the ORDER BY parameters list from JSON input."""
     params = []
 
     for field in sort_list:
-        field = field.lstrip('-')
-        if field not in none_json_fields.keys():
-            params.append(field.lstrip('-'))
+        field = field.lstrip("-")
+        if none_json_fields is not None:
+            if field not in none_json_fields.keys():
+                params.append(field.lstrip("-"))
 
     return params
