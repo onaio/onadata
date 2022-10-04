236d7888bb5bef31ba156de9a3165c306623a16f
Kipchirchir Sigei <arapgodsmack@gmail.com>
2022-10-04 10:26:37 +0300

    Add additional message verbs
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- onadata/apps/messaging/constants.py
+++ onadata/apps/messaging/constants.py
@@ -12,6 +12,7 @@ from django.utils.translation import gettext as _
 XFORM = text('xform')
 PROJECT = text('project')
 USER = text('user')
+EXPORT = text('export')
 
 APP_LABEL_MAPPING = {
     XFORM: 'logger',
@@ -21,14 +22,30 @@ APP_LABEL_MAPPING = {
 
 MESSAGE = 'message'
 UNKNOWN_TARGET = _("Unknown target.")
+USER_ADDED_TO_PROJECT = "user_added_to_project"
+USER_REMOVED_FROM_PROJECT = "user_removed_from_project"
+PROJECT_CREATED = "project_created"
+PROJECT_EDITED = "project_edited"
+PROJECT_SHARED = "project_shared"
+PROJECT_DELETED = "project_deleted"
 SUBMISSION_CREATED = "submission_created"
 SUBMISSION_EDITED = "submission_edited"
 SUBMISSION_DELETED = "submission_deleted"
 SUBMISSION_REVIEWED = "submission_reviewed"
 FORM_UPDATED = "form_updated"
+FORM_CREATED = "form_created"
+FORM_DELETED = "form_deleted"
+EXPORT_CREATED = "export_created"
+EXPORT_DELETED = "export_deleted"
+PERMISSION_GRANTED = "permission_granted"
+PERMISSION_REVOKED = "permission_revoked"
 MESSAGE_VERBS = [
     MESSAGE, SUBMISSION_REVIEWED, SUBMISSION_CREATED, SUBMISSION_EDITED,
-    SUBMISSION_DELETED, FORM_UPDATED]
+    SUBMISSION_DELETED, FORM_UPDATED, FORM_CREATED, FORM_DELETED,
+    EXPORT_CREATED, EXPORT_DELETED, PROJECT_EDITED, PROJECT_SHARED,
+    PROJECT_CREATED, USER_ADDED_TO_PROJECT, USER_REMOVED_FROM_PROJECT,
+    PROJECT_DELETED
+]
 VERB_TOPIC_DICT = {
     SUBMISSION_CREATED: "submission/created",
     SUBMISSION_EDITED: "submission/edited",
