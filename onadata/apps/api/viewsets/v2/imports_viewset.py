e9b0afce737a8ec6698fc7b98d6bca08d436d1a1
Davis Raymond Muro <davisraymondmuro@outlook.com>
2022-10-05 10:26:44 +0300

    Add `create` route to the `ImportsViewSet`
    - Add _get_active_tasks utility function
    - Modify `retrieve` route; Remove uneeded if statement since the
      get_object function raises a 404 error if object is missing

--- onadata/apps/api/viewsets/v2/imports_viewset.py
+++ onadata/apps/api/viewsets/v2/imports_viewset.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from django.conf import settings
@@ -14,16 +15,20 @@ from onadata.apps.logger.models import XForm
 from onadata.libs.mixins.cache_control_mixin import CacheControlMixin
 from onadata.libs.mixins.etags_mixin import ETagsMixin
 from onadata.libs.utils.async_status import get_active_tasks
-from onadata.libs.utils.csv_import import submission_xls_to_csv, submit_csv, submit_csv_async
+from onadata.libs.utils.csv_import import (
+    submission_xls_to_csv,
+    submit_csv,
+    submit_csv_async,
+)
 from onadata.settings.common import CSV_EXTENSION, XLS_EXTENSIONS
 
 BaseViewset = get_baseviewset_class()
 
 
-class ImportsViewSet(ETagsMixin, CacheControlMixin,
-                     viewsets.ViewSet):
+class ImportsViewSet(ETagsMixin, CacheControlMixin, viewsets.ViewSet):
     permission_classes = [XFormPermissions]
     queryset = XForm.objects.filter(deleted_at__isnull=True)
+    task_names = ["onadata.libs.utils.csv_import.submit_csv_async"]
 
     def get_queryset(self):
         return XForm.objects.filter(deleted_at__isnull=True)
@@ -34,13 +39,46 @@ class ImportsViewSet(ETagsMixin, CacheControlMixin,
         self.check_object_permissions(self.request, obj)
         return obj
 
-    @action(detail=True, methods=["POST"])
-    def start(self, request, pk: int = None):
+    def _get_active_tasks(self, xform: XForm) -> str:
+        """
+        Utility function to retrieve the active tasks of an XForm
+        """
+        return get_active_tasks(self.task_names, xform)
+
+    def create(self, request, pk: int = None) -> Response:
+        """
+        Starts a new Import task for a given form; The route processes imports asynchronously
+        unless the `DISABLE_ASYNCHRONOUS_IMPORTS` setting is set to false.
+
+        Route:
+
+        Curl example:
+
+        $ curl -X POST "http://<domain>/api/v2/imports/<xform_id>"
+
+        Supported Query Parameters:
+
+        - overwrite: bool = Whether the server should permanently delete the data currently available on
+          the form then reimport the data using the csv_file/xls_file sent with the request.
+
+        Required Request Arguements:
+
+        - csv_file: file = A CSV File containing the forms data
+        - xls_file: file = An XLSX file containing the forms data
+
+        Possible Response status codes:
+
+        - 202 Accepted: Server has successfully accepted your request for data import and has queued the task
+        - 200 Ok: Server has successfully imported your data to the form; Only returned when asynchronous imports are disabled
+        - 400 Bad Request: Request has been refused due to incorrect/missing csv_file or xls_file file
+        - 403 Forbidden: The request was valid but the server refused to process it. An explanation on why it was refused can be found in the JSON Response
+        """
         xform = self.get_object(pk)
         resp = {}
-        import ipdb; ipdb.set_trace()
         csv_file = request.FILES.get("csv_file", None)
         xls_file = request.FILES.get("xls_file", None)
+        status_code = status.HTTP_400_BAD_REQUEST
+
         if csv_file is None and xls_file is None:
             resp.update({"error": "csv_file and xls_file field empty"})
         elif xls_file and xls_file.name.split(".")[-1] not in XLS_EXTENSIONS:
@@ -50,39 +88,56 @@ class ImportsViewSet(ETagsMixin, CacheControlMixin,
         else:
             if xls_file and xls_file.name.split(".")[-1] in XLS_EXTENSIONS:
                 csv_file = submission_xls_to_csv(xls_file)
-            overwrite = request.query_params.get("overwrite")
-            overwrite = (overwrite.lower() == "true" if isinstance(
-                overwrite, str) else overwrite)
-            size_threshold = settings.CSV_FILESIZE_IMPORT_ASYNC_THRESHOLD
-            try:
-                csv_size = csv_file.size
-            except AttributeError:
-                csv_size = csv_file.__sizeof__()
-            csv_file.seek(0)
-            upload_to = os.path.join(request.user.username, "csv_imports",
-                                     csv_file.name)
-            file_name = default_storage.save(upload_to, csv_file)
-            task = submit_csv_async.delay(request.user.username,
-                                          self.object.pk, file_name, overwrite)
-            if task is None:
-                raise ParseError("Task not found")
-            resp.update({"task_id": task.task_id})
 
-            return Response(
-                data=resp,
-                status=status.HTTP_200_OK
-                if resp.get("error") is None else status.HTTP_400_BAD_REQUEST,
-                content_type="application/json"
+            overwrite = request.query_params.get("overwrite")
+            overwrite = (
+                overwrite.lower() == "true" if isinstance(overwrite, str) else overwrite
             )
 
-    def retrieve(self, request, pk: int = None):
+            # Block overwrite imports from running in parallel
+            active_tasks = json.load(self._get_active_tasks(xform))
+            for task in active_tasks:
+                if task.get("overwrite", False):
+                    task_id = task.get("id")
+                    resp.update(
+                        {
+                            "reason": "An ongoing overwrite request with the ID {task_id} is being processed"
+                        }
+                    )
+                    status_code = status.HTTP_403_FORBIDDEN
+                    break
+
+            if not status_code == status.HTTP_403_FORBIDDEN:
+                try:
+                    csv_size = csv_file.size
+                except AttributeError:
+                    csv_size = csv_file.__sizeof__()
+                csv_file.seek(0)
+
+                if getattr(settings, "DISABLE_ASYNCHRONOUS_IMPORTS", False):
+                    resp.update(submit_csv(request.user.username, xform, csv_file))
+                    status_code = status.HTTP_200_OK
+                else:
+                    upload_to = os.path.join(
+                        request.user.username, "csv_imports", csv_file.name
+                    )
+                    file_name = default_storage.save(upload_to, csv_file)
+                    task = submit_csv_async.delay(
+                        request.user.username, xform.pk, file_name, overwrite
+                    )
+                    if task is None:
+                        raise ParseError("Task not found")
+                    resp.update({"task_id": task.task_id})
+                    status_code = status.HTTP_202_ACCEPTED
+
+        return Response(data=resp, status=status_code, content_type="application/json")
+
+    def retrieve(self, request, pk: int = None) -> Response:
         """Returns csv import async tasks that belong to this form"""
         xform = self.get_object(pk)
 
-        task_names = ["onadata.libs.utils.csv_import.submit_csv_async"]
-        if xform:
-            return Response(
-                data=get_active_tasks(task_names, xform),
-                status=status.HTTP_200_OK,
-                content_type="application/json",
-            )
+        return Response(
+            data=self._get_active_tasks(xform),
+            status=status.HTTP_200_OK,
+            content_type="application/json",
+        )
