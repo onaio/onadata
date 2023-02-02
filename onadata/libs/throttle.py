d71e02c28697fb43e15137efab979c2497470d50
Davis Raymond Muro <davis.ray.muro@gmail.com>
2023-02-02 14:46:39 +0300

    Add custom `RequestHeaderThrottle` class

---
+++ onadata/libs/throttle.py
@@ -0,0 +1,29 @@
+"""
+Module containing throttling utilities
+"""
+
+from django.conf import Settings
+
+from rest_framework.throttling import SimpleRateThrottle
+
+
+class RequestHeaderThrottle(SimpleRateThrottle):
+    """
+    Custom Throttling class that throttles requests that match a specific
+    header
+    """
+
+    scope = "header"
+    throttled_headers = getattr(
+        Settings,
+        "THROTTLE_HEADERS",
+        {"HTTP_USER_AGENT": "Google-HTTP-Java-Client/1.35.0 (gzip)"},
+    )
+
+    def get_cache_key(self, request, _):
+        for header, value in self.throttled_headers.items():
+            header_value = request.META.get(header, None)
+            if header_value == value:
+                ident = header_value
+                return self.cache_format % {"scope": self.scope, "ident": ident}
+        return None
