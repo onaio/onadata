3df01a4bdb2f4057348f75d3520ca0393e6e1c4f
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2023-04-13 17:14:42 +0300

    read instance from master db when running signl

--- onadata/apps/restservice/signals.py
+++ onadata/apps/restservice/signals.py
@@ -4,6 +4,7 @@ RestService signals module
 """
 import django.dispatch
 from django.conf import settings
+from multidb.pinning import use_master
 
 from onadata.apps.restservice.tasks import call_service_async
 from onadata.apps.restservice.utils import call_service
@@ -25,14 +26,15 @@ def call_webhooks(sender, **kwargs):  # pylint: disable=unused-argument
     if ASYNC_POST_SUBMISSION_PROCESSING_ENABLED:
         call_service_async.apply_async(args=[instance_id], countdown=1)
     else:
-        try:
-            instance = Instance.objects.get(pk=instance_id)
-        except Instance.DoesNotExist:
-            # if the instance has already been removed we do not send it to the
-            # service
-            pass
-        else:
-            call_service(instance)
+        with use_master:
+            try:
+                instance = Instance.objects.get(pk=instance_id)
+            except Instance.DoesNotExist:
+                # if the instance has already been removed we do not send it to the
+                # service
+                pass
+            else:
+                call_service(instance)
 
 
 trigger_webhook.connect(call_webhooks, dispatch_uid="call_webhooks")
