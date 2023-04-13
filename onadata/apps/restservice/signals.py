56616c4d8c511f52a5af18692fd03485294c9e8a
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2023-04-13 10:59:50 +0300

    do not run webhook signal as cellery task
    also revert previous changes

--- onadata/apps/restservice/signals.py
+++ onadata/apps/restservice/signals.py
@@ -6,6 +6,7 @@ import django.dispatch
 from django.conf import settings
 
 from onadata.apps.restservice.tasks import call_service_async
+from onadata.apps.restservice.utils import call_service
 
 ASYNC_POST_SUBMISSION_PROCESSING_ENABLED = getattr(
     settings, "ASYNC_POST_SUBMISSION_PROCESSING_ENABLED", False
@@ -23,7 +24,7 @@ def call_webhooks(sender, **kwargs):  # pylint: disable=unused-argument
     if ASYNC_POST_SUBMISSION_PROCESSING_ENABLED:
         call_service_async.apply_async(args=[instance_id], countdown=1)
     else:
-        call_service_async(instance_id)
+        call_service(instance_id)
 
 
 trigger_webhook.connect(call_webhooks, dispatch_uid="call_webhooks")
