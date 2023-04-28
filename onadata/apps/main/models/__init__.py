f30a434c4fb45f913fdb7d58d4da8372d1014186
Davis Raymond Muro <davis.ray.muro@gmail.com>
2023-04-28 10:02:12 +0300

    feat(user password): Add `PasswordHistory` model

--- onadata/apps/main/models/__init__.py
+++ onadata/apps/main/models/__init__.py
@@ -8,3 +8,4 @@ from onadata.apps.main.models.user_profile import UserProfile  # noqa
 from onadata.apps.main.models.meta_data import MetaData  # noqa
 from onadata.apps.main.models.google_oath import TokenStorageModel  # noqa
 from onadata.apps.main.models.audit import AuditLog  # noqa
+from onadata.apps.main.models.password_history import PasswordHistory  # noqa
---
+++ onadata/apps/main/models/password_history.py
@@ -0,0 +1,36 @@
+"""
+Password History Class Module
+
+This module contains the `PasswordHistory` model which is used to track
+password changes for a user.
+"""
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+
+class PasswordHistory(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="password_history"
+    )
+    hashed_password = models.CharField(max_length=128, unique=True, db_index=True)
+    changed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}-pass-{self.changed_at}"
+
+    @classmethod
+    def user_pre_save(cls, sender, instance=None, created=False, **kwargs):
+        if not created and instance.pk:
+            current_password = User.objects.get(pk=instance.pk).password
+            if current_password != instance.password:
+                cls.objects.create(
+                    user=instance, hashed_password=current_password
+                )
+
+    class Meta:
+        app_label = "main"
+
+models.signals.pre_save.connect(PasswordHistory.user_pre_save, sender=User)
