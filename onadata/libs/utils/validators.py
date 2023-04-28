ccc32b28b9566821f3a18da547a3c4daba87ed2b
Davis Raymond Muro <davis.ray.muro@gmail.com>
2023-04-28 10:52:37 +0300

    feat(password validators): add `PreviousPasswordValidator` class

---
+++ onadata/libs/utils/validators.py
@@ -0,0 +1,27 @@
+"""
+Module containing custom validator classes for the User Model
+"""
+from django.contrib.auth.hashers import check_password
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+
+
+class PreviousPasswordValidator:
+    def __init__(self, history_limt=5):
+        self.message = _("You cannot use a previously used password.")
+        self.history_limit = history_limt
+
+    def validate(self, password, user=None):
+        if user:
+            if user.check_password(password):
+                raise ValidationError(self.message)
+
+            pw_history = user.password_history.all()[:self.history_limit]
+            for pw in pw_history:
+                if check_password(password, pw.hashed_password):
+                    raise ValidationError(self.message)
+
+    def get_help_text(self):
+        return _(
+            "Your password cannot be the same as your previous password."
+        )
