7b09d5802f87324d2bb674454ef0c2f3ab3654b5
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2023-08-14 11:41:15 +0300

    Tag release v3.12.1 (#2468)
    * tag release v3.12.1
    
    * add change to v3.12.1

--- onadata/__init__.py
+++ onadata/__init__.py
@@ -6,7 +6,7 @@ visualization.
 """
 from __future__ import absolute_import, unicode_literals
 
-__version__ = "3.12.0"
+__version__ = "3.12.1"
 
 
 # This will make sure the app is always imported when
