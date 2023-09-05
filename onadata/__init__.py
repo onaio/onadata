7b8d0d75a9f653382588135f0dfe057d3f55d5cb
Kipchirchir Cheroigin <arapgodsmack@gmail.com>
2023-09-05 09:54:27 +0300

    Tag release v3.13.0 (#2473)
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- onadata/__init__.py
+++ onadata/__init__.py
@@ -6,7 +6,7 @@ visualization.
 """
 from __future__ import absolute_import, unicode_literals
 
-__version__ = "3.12.2"
+__version__ = "3.13.0"
 
 
 # This will make sure the app is always imported when
