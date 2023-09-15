0488a139293acf5652995ca7cea20cc8a3716c61
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2023-09-15 17:37:39 +0300

    update comment

--- onadata/apps/api/viewsets/xform_viewset.py
+++ onadata/apps/api/viewsets/xform_viewset.py
@@ -1030,8 +1030,8 @@ class XFormViewSet(
         cached_task_id: str = cache.get(cache_key)
 
         if cached_task_id and AsyncResult(cached_task_id).state.upper() != "FAILURE":
-            # If a task has not failed, we should wait it to complete or fail,
-            # so for now we do nothing
+            # FAILURE is the only state that should trigger regeneration if a regeneration
+            # had earlier been triggered
             return Response({"status": "STARTED"})
 
         # Task has either failed or does not exist in cache, we create a new async task
