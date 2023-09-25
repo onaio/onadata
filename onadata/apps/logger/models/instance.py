b6177cde4dcc2f4546d3d56a753e3899a430fd51
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2023-09-25 13:46:51 +0300

    remove json getter in model Instance
    there is already a json field present that already has the result the getter is recalculating

--- onadata/apps/logger/models/instance.py
+++ onadata/apps/logger/models/instance.py
@@ -893,11 +893,6 @@ class InstanceHistory(models.Model, InstanceBaseClass):
         return self.xform_instance.attachments.all()
 
     @property
-    def json(self):
-        """Returns the XML submission as a python dictionary object."""
-        return self.get_full_dict()
-
-    @property
     def status(self):
         """Returns the submission's status"""
         return self.xform_instance.status
