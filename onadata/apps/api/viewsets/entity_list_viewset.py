02de6ccf32a32b19887890240089532e33cb91c2
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2024-01-17 11:26:55 +0300

    optimize performance

--- onadata/apps/api/viewsets/entity_list_viewset.py
+++ onadata/apps/api/viewsets/entity_list_viewset.py
@@ -36,6 +36,16 @@ class EntityListViewSet(
     permission_classes = (AllowAny,)
     pagination_class = StandardPageNumberPagination
 
+    def get_queryset(self):
+        if self.action == "retrieve":
+            queryset = super().get_queryset()
+            return queryset.prefetch_related(
+                "registration_forms",
+                "follow_up_forms",
+            )
+
+        return super().get_queryset()
+
     def get_serializer_class(self):
         """Override get_serializer_class"""
         if self.action == "retrieve":
@@ -50,10 +60,17 @@ class EntityListViewSet(
     def entities(self, request, *args, **kwargs):
         """Returns a list of Entities"""
         entity_list = self.get_object()
-        entities_qs = Entity.objects.filter(
-            registration_form__entity_list=entity_list
-        ).order_by("pk")
-        page = self.paginate_queryset(entities_qs)
-        serializer = self.get_serializer(page, many=True)
+        entities_qs = (
+            Entity.objects.filter(registration_form__entity_list=entity_list)
+            .only("json")
+            .order_by("pk")
+        )
+        queryset = self.filter_queryset(entities_qs)
+        page = self.paginate_queryset(queryset)
 
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
