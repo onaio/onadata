8f616aa17a861ff9d7f16f1847d89c49926f4987
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2024-01-18 12:26:54 +0300

    add attribute to get dataset properties from an EntityList object

--- onadata/apps/logger/models/entity_list.py
+++ onadata/apps/logger/models/entity_list.py
@@ -3,6 +3,7 @@ EntityList model
 """
 
 from django.db import models
+from django.utils.functional import cached_property
 
 from django.utils.translation import gettext_lazy as _
 
@@ -34,3 +35,19 @@ class EntityList(AbstractBase):
 
     def __str__(self):
         return f"{self.name}|{self.project}"
+
+    @cached_property
+    def properties(self) -> list[str]:
+        """All dataset properties
+
+        Multiple forms can define matching or different properties for the same
+        dataset
+        """
+        registration_forms_qs = self.registration_forms.filter(is_active=True)
+        dataset_properties = []
+
+        for form in registration_forms_qs:
+            form_properties = list(form.get_save_to().keys())
+            dataset_properties += form_properties
+
+        return dataset_properties
