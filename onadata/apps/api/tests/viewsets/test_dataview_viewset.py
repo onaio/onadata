e7c8b413e1bb96ad7ee9f661cfe46b75848fd1d3
Ukang'a Dickson <ukanga@users.noreply.github.com>
2024-03-25 10:27:00 +0300

    Add missing dot in test error message

--- onadata/apps/api/tests/viewsets/test_dataview_viewset.py
+++ onadata/apps/api/tests/viewsets/test_dataview_viewset.py
@@ -212,7 +212,7 @@ class TestDataViewViewSet(TestAbstractViewSet):
         self.assertEqual(response.status_code, 404)
         response_data = json.loads(json.dumps(response.data))
         self.assertEqual(
-            response_data, {"detail": "No Attachment matches the given query"}
+            response_data, {"detail": "No Attachment matches the given query."}
         )
 
         # a user with permissions can view a specific attachment object
