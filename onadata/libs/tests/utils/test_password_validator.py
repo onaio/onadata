33a8fb6c8755d45b11652de1e2fa1100848ce08e
Kipchirchir Sigei <arapgodsmack@gmail.com>
2024-06-19 15:06:34 +0300

    Add docstrings to testcases
    Signed-off-by: Kipchirchir Sigei <arapgodsmack@gmail.com>

--- onadata/libs/tests/utils/test_password_validator.py
+++ onadata/libs/tests/utils/test_password_validator.py
@@ -5,24 +5,32 @@ from django.core.exceptions import ValidationError
 from onadata.apps.main.models.password_history import PasswordHistory
 from onadata.libs.utils.validators import PreviousPasswordValidator
 
+
 class PreviousPasswordValidatorTestCase(TestCase):
-    def test_validator_does_not_raise_valueerror_missing_pk(self):
+    """
+    Test case for the PreviousPasswordValidator class.
+    Ensures correct behavior of password validation.
+    """
+
+    def test_missing_pk(self):
+        """Validator does not raise ValueError for missing pk"""
         # Create a validator instance
         validator = PreviousPasswordValidator()
 
         # Create a user instance without saving it to the database
-        user = User(username='testuser')
+        user = User(username="testuser")
 
         # Call the validate method and ensure it does not raise a ValueError
         try:
-            validator.validate('somepassword', user=user)
+            validator.validate("somepassword", user=user)
         except ValueError:
             self.fail("PreviousPasswordValidator raised ValueError unexpectedly!")
 
-    def test_validator_raises_validationerror_for_reused_password(self):
+    def test_reused_password(self):
+        """Test ValidationError exception thrown on reused password"""
         # Create and save a user to the database
-        user = User.objects.create(username='testuser')
-        user.set_password('oldpassword')
+        user = User.objects.create(username="testuser")
+        user.set_password("oldpassword")
         user.save()
 
         # Add the old password to password history
@@ -33,15 +41,17 @@ class PreviousPasswordValidatorTestCase(TestCase):
 
         # Try using an old password
         with self.assertRaises(ValidationError) as cm:
-            validator.validate('oldpassword', user=user)
+            validator.validate("oldpassword", user=user)
 
         self.assertEqual(
-            str(cm.exception.message), "You cannot use a previously used password.")
+            str(cm.exception.message), "You cannot use a previously used password."
+        )
 
-    def test_validator_allows_new_password(self):
+    def test_allows_new_password(self):
+        """Test validator allows new password not used before"""
         # Create and save a user to the database
-        user = User.objects.create(username='testuser')
-        user.set_password('oldpassword')
+        user = User.objects.create(username="testuser")
+        user.set_password("oldpassword")
         user.save()
 
         # Add the old password to password history
@@ -52,6 +62,6 @@ class PreviousPasswordValidatorTestCase(TestCase):
 
         # Try using a new password
         try:
-            validator.validate('newpassword@123', user=user)
+            validator.validate("newpassword@123", user=user)
         except ValidationError:
             self.fail("PreviousPasswordValidator raised ValidationError unexpectedly!")
