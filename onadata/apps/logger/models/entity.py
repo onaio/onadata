2f8cf7f7b3c3d350b7c6d73ced767f7b880fcd95
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2024-07-25 15:25:29 +0300

    remove whitespace

--- onadata/apps/logger/models/entity.py
+++ onadata/apps/logger/models/entity.py
@@ -13,7 +13,6 @@ from onadata.apps.logger.models.instance import Instance
 from onadata.apps.logger.models.registration_form import RegistrationForm
 from onadata.libs.models import BaseModel
 
-
 User = get_user_model()
 
 
