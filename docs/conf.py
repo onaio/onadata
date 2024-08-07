8f7c0fd8d4b43cf1acebbadfe10f0382244ecc7a
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2024-08-07 10:15:32 +0300

    include markdown in Sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -41,13 +41,14 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
     "sphinx.ext.imgmath",
+    "recommonmark",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
 # The suffix of source filenames.
-source_suffix = ".rst"
+source_suffix = [".rst", ".md"]
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'
