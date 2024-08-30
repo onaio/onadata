18f6fde6704cb4982ece45c965b106e6064b7550
Eric Musyoka <wambua.eric01@gmail.com>
2024-08-30 18:12:39 +0300

    Log user profile activities

--- onadata/apps/messaging/constants.py
+++ onadata/apps/messaging/constants.py
@@ -26,13 +26,20 @@ SUBMISSION_EDITED = "submission_edited"
 SUBMISSION_DELETED = "submission_deleted"
 SUBMISSION_REVIEWED = "submission_reviewed"
 FORM_UPDATED = "form_updated"
+USER_CREATED = "user_created"
+USER_UPDATED = "user_updated"
+USER_PASSWORD_CHANGED = "password_changed"
 MESSAGE_VERBS = [
     MESSAGE, SUBMISSION_REVIEWED, SUBMISSION_CREATED, SUBMISSION_EDITED,
-    SUBMISSION_DELETED, FORM_UPDATED]
+    SUBMISSION_DELETED, FORM_UPDATED, USER_CREATED, USER_UPDATED,
+    USER_PASSWORD_CHANGED]
 VERB_TOPIC_DICT = {
     SUBMISSION_CREATED: "submission/created",
     SUBMISSION_EDITED: "submission/edited",
     SUBMISSION_DELETED: "submission/deleted",
     SUBMISSION_REVIEWED: "submission/reviewed",
-    FORM_UPDATED: "form/updated"
+    FORM_UPDATED: "form/updated",
+    USER_CREATED: "user/created",
+    USER_UPDATED: "user/updated",
+    USER_PASSWORD_CHANGED: "user/password_changed"
 }
