20c4cb8c9f899b87e4396934192aee1532b3a182
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2024-09-23 16:22:59 +0300

    bump version to v4.8.0-rc

--- onadata/__init__.py
+++ onadata/__init__.py
@@ -6,7 +6,7 @@ visualization.
 """
 from __future__ import absolute_import, unicode_literals
 
-__version__ = "4.7.1"
+__version__ = "4.8.0"
 
 
 # This will make sure the app is always imported when
