4dc84d425ee32ae7aefd7963a72b22f851eb9e57
FrankApiyo <franklineapiyo@gmail.com>
2024-10-19 16:41:17 +0300

    Remove deleted-by and deleted-at from the dataview serializer

--- onadata/libs/tests/serializers/test_dataview_serializer.py
+++ onadata/libs/tests/serializers/test_dataview_serializer.py
@@ -79,11 +79,8 @@ class TestDataViewSerializer(TestAbstractViewSet):
         serialized_dataview = DataViewSerializer(
             dataview, context={"request": request}
         ).data
-        self.assertEqual(
-            serialized_dataview["deleted_by"],
-            f"http://testserver/api/v1/users/{self.user.username}",
-        )
-        self.assertTrue("deleted_at" in serialized_dataview)
+        self.assertEqual(serialized_dataview["deleted_by"], None)
+        self.assertFalse("deleted_at" in serialized_dataview)
 
     def test_name_and_xform_are_unique(self):
         """
