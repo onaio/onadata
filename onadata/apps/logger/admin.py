9d9943e0b4706245e8088356e0794a01ec6e4198
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2024-11-26 15:26:18 +0300

    update XForm admin

--- onadata/apps/logger/admin.py
+++ onadata/apps/logger/admin.py
@@ -64,9 +64,8 @@ class XFormAdmin(FilterByUserMixin, VersionAdmin, admin.ModelAdmin):
 
     def get_actions(self, request):
         actions = super().get_actions(request)
-        if request.user.username[0].upper() != "J":
-            if "delete_selected" in actions:
-                del actions["delete_selected"]
+        if "delete_selected" in actions:
+            del actions["delete_selected"]
         return actions
 
 
