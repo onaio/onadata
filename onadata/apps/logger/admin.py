46295583bcceab6b9c2b620bbd8f8934b9fa2c86
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2024-11-26 15:54:48 +0300

    add comment

--- onadata/apps/logger/admin.py
+++ onadata/apps/logger/admin.py
@@ -65,6 +65,7 @@ class XFormAdmin(FilterByUserMixin, VersionAdmin, admin.ModelAdmin):
     def get_actions(self, request):
         actions = super().get_actions(request)
         if "delete_selected" in actions:
+            # disable delete_selected action
             del actions["delete_selected"]
         return actions
 
