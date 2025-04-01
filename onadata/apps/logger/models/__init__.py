0a64c4df98cdd1833e516b1dca32829b74d90291
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2025-04-01 11:53:26 +0300

    add KMSKey, XFormKey models

--- onadata/apps/logger/models/__init__.py
+++ onadata/apps/logger/models/__init__.py
@@ -2,22 +2,24 @@
 """
 Logger models.
 """
+
 from onadata.apps.logger.models.attachment import Attachment  # noqa
 from onadata.apps.logger.models.data_view import DataView  # noqa
+from onadata.apps.logger.models.entity import Entity, EntityHistory  # noqa
+from onadata.apps.logger.models.entity_list import EntityList  # noqa
+from onadata.apps.logger.models.follow_up_form import FollowUpForm  # noqa
 from onadata.apps.logger.models.instance import Instance  # noqa
+from onadata.apps.logger.models.kms import KMSKey, XFormKey  # noqa
 from onadata.apps.logger.models.merged_xform import MergedXForm  # noqa
 from onadata.apps.logger.models.note import Note  # noqa
 from onadata.apps.logger.models.open_data import OpenData  # noqa
 from onadata.apps.logger.models.osmdata import OsmData  # noqa
 from onadata.apps.logger.models.project import Project  # noqa
+from onadata.apps.logger.models.project_invitation import ProjectInvitation  # noqa
+from onadata.apps.logger.models.registration_form import RegistrationForm  # noqa
 from onadata.apps.logger.models.submission_review import SubmissionReview  # noqa
 from onadata.apps.logger.models.survey_type import SurveyType  # noqa
 from onadata.apps.logger.models.widget import Widget  # noqa
 from onadata.apps.logger.models.xform import XForm  # noqa
 from onadata.apps.logger.models.xform_version import XFormVersion  # noqa
 from onadata.apps.logger.xform_instance_parser import InstanceParseError  # noqa
-from onadata.apps.logger.models.project_invitation import ProjectInvitation  # noqa
-from onadata.apps.logger.models.entity_list import EntityList  # noqa
-from onadata.apps.logger.models.registration_form import RegistrationForm  # noqa
-from onadata.apps.logger.models.follow_up_form import FollowUpForm  # noqa
-from onadata.apps.logger.models.entity import Entity, EntityHistory  # noqa
