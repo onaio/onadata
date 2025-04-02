37b0502b89a36af17d86adf95840ad2d7d67ef1f
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2025-04-02 17:50:07 +0300

    add tests for get_kms_client, create_key

--- onadata/libs/kms/tools.py
+++ onadata/libs/kms/tools.py
@@ -2,12 +2,12 @@
 Key management utility functions
 """
 
-from datetime import datetime
-from datetime import timezone as tz
+from datetime import timedelta
 
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ImproperlyConfigured
+from django.utils import timezone
 
 from onadata.apps.api.models import OrganizationProfile
 from onadata.apps.logger.models import KMSKey
@@ -18,13 +18,13 @@ from onadata.libs.utils.model_tools import queryset_iterator
 KMS_CLIENTS = {"AWS": AWSKMSClient}
 
 
-def get_kms_provider():
+def _get_kms_provider():
     return getattr(settings, "KMS_PROVIDER", "AWS")
 
 
 def get_kms_client():
     """Retrieve the appropriate KMS client based on settings."""
-    kms_provider = get_kms_provider()
+    kms_provider = _get_kms_provider()
     kms_client_cls = KMS_CLIENTS.get(kms_provider)
 
     if not kms_client_cls:
@@ -40,19 +40,21 @@ def create_key(org: OrganizationProfile) -> KMSKey:
     :return: KMSKey
     """
     kms_client = get_kms_client()
-    now = datetime.now(tz=tz.utc)
+    now = timezone.now()
     description = f"Key-{now.strftime('%Y-%m-%d')}"
     metadata = kms_client.create_key(description=description)
     next_rotation_at = None
 
-    if hasattr(settings.KMS_ROTATION_DURATION):
+    if hasattr(settings, "KMS_ROTATION_DURATION") and isinstance(
+        settings.KMS_ROTATION_DURATION, timedelta
+    ):
         next_rotation_at = now + settings.KMS_ROTATION_DURATION
 
     content_type = ContentType.objects.get_for_model(org)
     provider_choice_map = {
         "AWS": KMSKey.KMSProvider.AWS,
     }
-    provider = provider_choice_map.get(get_kms_provider().upper())
+    provider = provider_choice_map.get(_get_kms_provider().upper())
 
     return KMSKey.objects.create(
         key_id=metadata["key_id"],
@@ -75,14 +77,14 @@ def rotate_key(kms_key: KMSKey) -> KMSKey:
     # so we create a new key
     new_key = create_key(kms_key.content_object)
 
-    kms_key.rotated_at = datetime.now(tz=tz.utc)
+    kms_key.rotated_at = timezone.now()
     kms_key.save(update_fields=["rotated_at"])
 
     # Update forms using the old key to use the new key
     xform_qs = kms_key.xforms.all()
 
     for xform in queryset_iterator(xform_qs):
-        new_version = datetime.now(tz=tz.utc).strftime("%Y%m%d%H%M")
+        new_version = timezone.now().strftime("%Y%m%d%H%M")
         json_dict = xform.json_dict()
         json_dict["public_key"] = new_key.public_key
         json_dict["version"] = new_version
