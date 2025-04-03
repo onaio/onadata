75c902ab6ded059518331db123bd8da9807d8795
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2025-04-03 16:51:45 +0300

    refactor code

--- onadata/apps/logger/tests/models/test_kms.py
+++ onadata/apps/logger/tests/models/test_kms.py
@@ -30,8 +30,7 @@ class KMSKeyTestCase(TestBase):
     def test_creation(self, mock_now):
         """We can create a KMSKey."""
         mock_now.return_value = self.mocked_now
-        next_rotation_at = self.mocked_now + timedelta(days=2)
-        rotated_at = self.mocked_now - timedelta(days=2)
+        expiry_date = self.mocked_now + timedelta(days=2)
         disabled_at = self.mocked_now
 
         kms_key = KMSKey.objects.create(
@@ -39,22 +38,22 @@ class KMSKeyTestCase(TestBase):
             description="Key-2025-04-01",
             public_key="Fake public key",
             provider=KMSKey.KMSProvider.AWS,
-            next_rotation_at=next_rotation_at,
-            rotated_at=rotated_at,
+            expiry_date=expiry_date,
             disabled_at=disabled_at,
             content_type=self.content_type,
             object_id=self.org.id,
+            disabled_by=self.user,
         )
 
         self.assertEqual(f"{kms_key}", "1234")
         self.assertEqual(kms_key.key_id, "1234")
         self.assertEqual(kms_key.description, "Key-2025-04-01")
         self.assertEqual(kms_key.provider, KMSKey.KMSProvider.AWS)
-        self.assertEqual(kms_key.next_rotation_at, next_rotation_at)
-        self.assertEqual(kms_key.rotated_at, rotated_at)
+        self.assertEqual(kms_key.expiry_date, expiry_date)
         self.assertEqual(kms_key.disabled_at, disabled_at)
         self.assertEqual(kms_key.object_id, self.org.id)
         self.assertEqual(kms_key.content_type, self.content_type)
+        self.assertEqual(kms_key.disabled_by, self.user)
 
     def test_default_values(self):
         """Default values for optional fields are correct."""
@@ -67,9 +66,9 @@ class KMSKeyTestCase(TestBase):
         )
 
         self.assertIsNone(kms_key.description)
-        self.assertIsNone(kms_key.next_rotation_at)
-        self.assertIsNone(kms_key.rotated_at)
+        self.assertIsNone(kms_key.expiry_date)
         self.assertIsNone(kms_key.disabled_at)
+        self.assertIsNone(kms_key.disabled_by)
 
     def test_key_id_provider_unique(self):
         """key_id, provider are unique together."""
