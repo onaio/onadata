21d1e90646bb78eeb76eb456230ca72858e7c649
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2025-04-03 16:51:45 +0300

    refactor code

--- onadata/apps/logger/models/kms.py
+++ onadata/apps/logger/models/kms.py
@@ -2,6 +2,7 @@
 Key management models
 """
 
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
@@ -9,6 +10,8 @@ from django.utils.translation import gettext_lazy as _
 
 from onadata.apps.logger.models.xform import XForm
 from onadata.libs.models import BaseModel
+
+User = get_user_model()
 
 
 class KMSKey(BaseModel):
@@ -19,9 +22,9 @@ class KMSKey(BaseModel):
     description = models.CharField(null=True, blank=True, max_length=255)
     public_key = models.TextField()
     provider = models.IntegerField(choices=KMSProvider.choices)
-    next_rotation_at = models.DateTimeField(null=True, blank=True)
-    rotated_at = models.DateTimeField(null=True, blank=True)
+    expiry_date = models.DateTimeField(null=True, blank=True)
     disabled_at = models.DateTimeField(null=True, blank=True)
+    disabled_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField(null=True, blank=True)
     content_object = GenericForeignKey("content_type", "object_id")
@@ -29,6 +32,10 @@ class KMSKey(BaseModel):
     class Meta(BaseModel.Meta):
         app_label = "logger"
         unique_together = ("key_id", "provider")
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+            models.Index(fields=["disabled_at"]),
+        ]
 
     def __str__(self):
         return self.key_id
