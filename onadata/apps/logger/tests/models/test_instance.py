7d373a233f8b7a7ba46a6553a9284395cba66b35
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2025-04-08 14:19:21 +0300

    decrypt submission automatically

--- onadata/apps/logger/tests/models/test_instance.py
+++ onadata/apps/logger/tests/models/test_instance.py
@@ -15,18 +15,29 @@ from django.test import override_settings
 
 from django_digest.test import DigestAuth
 
-from onadata.apps.logger.models import (Entity, EntityList, Instance,
-                                        RegistrationForm, SubmissionReview,
-                                        XForm)
-from onadata.apps.logger.models.instance import (get_id_string_from_xml_str,
-                                                 numeric_checker)
+from onadata.apps.logger.models import (
+    Entity,
+    EntityList,
+    Instance,
+    RegistrationForm,
+    SubmissionReview,
+    SurveyType,
+    XForm,
+)
+from onadata.apps.logger.models.instance import (
+    get_id_string_from_xml_str,
+    numeric_checker,
+)
 from onadata.apps.main.models.meta_data import MetaData
 from onadata.apps.main.tests.test_base import TestBase
-from onadata.apps.viewer.models.parsed_instance import (ParsedInstance,
-                                                        query_data,
-                                                        query_fields_data)
-from onadata.libs.serializers.submission_review_serializer import \
-    SubmissionReviewSerializer
+from onadata.apps.viewer.models.parsed_instance import (
+    ParsedInstance,
+    query_data,
+    query_fields_data,
+)
+from onadata.libs.serializers.submission_review_serializer import (
+    SubmissionReviewSerializer,
+)
 from onadata.libs.utils.common_tags import MONGO_STRFTIME, SUBMITTED_BY
 from onadata.libs.utils.user_auth import get_user_default_project
 
@@ -1386,3 +1397,71 @@ class TestInstance(TestBase):
 
         self.assertEqual(merged_multiples_columns, expected_columns)
         self.assertEqual(split_multiples_columns, expected_columns)
+
+    @patch("onadata.apps.logger.signals.decrypt_instance_async.delay")
+    def test_decrypt_instance(self, mock_dec_async):
+        """Instance is decrypted."""
+        metadata_xml = """
+        <data xmlns="http://opendatakit.org/submissions" encrypted="yes"
+            id="test_valigetta" version="202502131337">
+            <base64EncryptedKey>fake0key</base64EncryptedKey>
+            <meta xmlns="http://openrosa.org/xforms">
+                <instanceID>uuid:a10ead67-7415-47da-b823-0947ab8a8ef0</instanceID>
+            </meta>
+            <media>
+                <file>sunset.png.enc</file>
+                <file>forest.mp4.enc</file>
+            </media>
+            <encryptedXmlFile>submission.xml.enc</encryptedXmlFile>
+            <base64EncryptedElementSignature>fake-signature</base64EncryptedElementSignature>
+        </data>
+        """.strip()
+        md = """
+        | survey  |
+        |         | type  | name   | label                |
+        |         | photo | sunset | Take photo of sunset |
+        |         | video | forest | Take a video of forest|
+        """
+        xform = self._publish_markdown(md, self.user, id_string="nature")
+        survey_type = SurveyType.objects.create(slug="slug-foo")
+
+        with override_settings(KMS_AUTO_DECRYPT_INSTANCE=True):
+            instance = Instance.objects.create(
+                xform=xform,
+                xml=metadata_xml,
+                user=self.user,
+                survey_type=survey_type,
+            )
+            mock_dec_async.assert_called_once_with(instance.pk)
+
+        # Auto-decrypt is disabled
+        mock_dec_async.reset_mock()
+        instance.delete()
+
+        with override_settings(KMS_AUTO_DECRYPT_INSTANCE=False):
+            instance = Instance.objects.create(
+                xform=xform,
+                xml=metadata_xml,
+                user=self.user,
+                survey_type=survey_type,
+            )
+            mock_dec_async.assert_not_called()
+
+        # Auto-decrypt config is missing
+        mock_dec_async.reset_mock()
+        instance.delete()
+        instance = Instance.objects.create(
+            xform=xform,
+            xml=metadata_xml,
+            user=self.user,
+            survey_type=survey_type,
+        )
+        mock_dec_async.assert_not_called()
+
+        # Unencrypted submission
+        mock_dec_async.reset_mock()
+
+        with override_settings(KMS_AUTO_DECRYPT_INSTANCE=True):
+            self._publish_transportation_form_and_submit_instance()
+
+            mock_dec_async.assert_not_called()
