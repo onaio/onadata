ea7c606c5fa168f095a7714b8ecf44aeb4b98753
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2025-04-29 14:34:32 +0300

    update docstring

--- onadata/libs/utils/email.py
+++ onadata/libs/utils/email.py
@@ -195,15 +195,9 @@ def send_mass_mail(
     The default from Django, django.core.mail.send_mass_mail does not support
     HTML message
 
-    Given a datatuple of (subject, text_content, html_content, from_email,
-    recipient_list), sends each message to each recipient list. Returns the
-    number of emails sent.
-
-    If from_email is None, the DEFAULT_FROM_EMAIL setting is used.
     If auth_user and auth_password are set, they're used to log in.
     If auth_user is None, the EMAIL_HOST_USER setting is used.
     If auth_password is None, the EMAIL_HOST_PASSWORD setting is used.
-
     """
     connection = connection or get_connection(
         username=user, password=password, fail_silently=fail_silently
