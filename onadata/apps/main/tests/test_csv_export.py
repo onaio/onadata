9ef81f5eb4f16a07799c51f321f39909765f3997
Ukang'a Dickson <ukanga@users.noreply.github.com>
2025-05-15 15:10:59 +0300

    chore: Test select one and select multiple in repeats at the same time

--- onadata/apps/main/tests/test_csv_export.py
+++ onadata/apps/main/tests/test_csv_export.py
@@ -13,8 +13,7 @@ from django.utils.dateparse import parse_datetime
 from onadata.apps.logger.models import XForm
 from onadata.apps.main.tests.test_base import TestBase
 from onadata.apps.viewer.models import DataDictionary, Export
-from onadata.apps.viewer.models.data_dictionary import \
-    create_or_update_export_register
+from onadata.apps.viewer.models.data_dictionary import create_or_update_export_register
 from onadata.libs.utils.export_tools import generate_export
 
 
@@ -220,7 +219,7 @@ class TestCsvExport(TestBase):
             md, self.user, id_string="tutorial_w_repeats"
         )
         xform = XForm.objects.get(pk=data_dictionary.pk)
-        path = os.path.join(self.fixture_dir, "tutorial_w_repeats_selects.xml")
+        path = os.path.join(self.fixture_dir, "tutorial_w_repeats_select_one.xml")
         self._make_submission(path, forced_submission_time=self._submission_time)
         export_options = {"extension": "csv", "split_select_multiples": True}
         export = generate_export(Export.CSV_EXPORT, xform, None, export_options)
@@ -372,7 +371,7 @@ class TestCsvExport(TestBase):
         |          | begin repeat                        | children           | 5. Children                      |
         |          | text                                | childs_name        | 5.1 Child's name?                |
         |          | integer                             | childs_age         | 5.2 Child's age?                 |
-        |          | select all that apply from colors   | childs_colors       | 5.3 Child's favourite colors ?   |
+        |          | select all that apply from colors   | childs_colors      | 5.3 Child's favourite colors ?   |
         |          | end repeat                          |                    |                                  |
         |          | geopoint                            | gps                | 6. Record your GPS coordinates.  |
         |          | select all that apply from browsers | web_browsers       | 7. What web browsers do you use? |
@@ -538,6 +537,208 @@ class TestCsvExport(TestBase):
                     "12",
                     "red blue",
                     "Dick",
+                    "1",
+                    "green orange",
+                    "-1.2625621 36.7921711 0.0 20.0",
+                    "-1.2625621",
+                    "36.7921711",
+                    "0.0",
+                    "20.0",
+                    "firefox chrome",
+                    "uuid:b31c6ac2-b8ca-4180-914f-c844fa10ed3b",
+                ],
+            )
+
+    def test_csv_repeat_with_selects(self):
+        """
+        Test that select one and select multiple in a repeat are exported correctly.
+        """
+        md = """
+        | survey   |
+        |          |
+        |          | type                                | name               | label                            |
+        |          | text                                | name               | 1. What is your name?            |
+        |          | integer                             | age                | 2. How old are you?              |
+        |          | image                               | picture            | 3. May I take your picture?      |
+        |          | select one from yes_no              | has_children       | 4. Do you have any children?     |
+        |          | begin repeat                        | children           | 5. Children                      |
+        |          | text                                | childs_name        | 5.1 Child's name?                |
+        |          | integer                             | childs_age         | 5.2 Child's age?                 |
+        |          | select one from yes_no              | child_is_nursing   | 5.3 Is the child nursing?        |
+        |          | select all that apply from colors   | childs_colors      | 5.4 Child's favourite colors ?   |
+        |          | end repeat                          |                    |                                  |
+        |          | geopoint                            | gps                | 6. Record your GPS coordinates.  |
+        |          | select all that apply from browsers | web_browsers       | 7. What web browsers do you use? |
+        | choices  |
+        |          | list name                           | name               | label                            |
+        |          | yes_no                              | 0                  | No                               |
+        |          | yes_no                              | 1                  | Yes                              |
+        |          | browsers                            | firefox            | Mozilla Firefox                  |
+        |          | browsers                            | chrome             | Google Chrome                    |
+        |          | browsers                            | ie                 | Internet Explorer                |
+        |          | browsers                            | safari             | Safari                           |
+        |          | colors                              | red                | Red                              |
+        |          | colors                              | blue               | Blue                             |
+        |          | colors                              | green              | Green                            |
+        |          | colors                              | white              | White                            |
+        |          | colors                              | orange             | Orange                           |
+        | settings |
+        |          | form_id                             |
+        |          | tutorial_w_repeats                  |
+        """
+        data_dictionary = self._publish_markdown(
+            md, self.user, id_string="tutorial_w_repeats"
+        )
+        xform = XForm.objects.get(pk=data_dictionary.pk)
+        path = os.path.join(self.fixture_dir, "tutorial_w_repeats_selects.xml")
+        self._make_submission(path, forced_submission_time=self._submission_time)
+        export_options = {"extension": "csv", "split_select_multiples": True}
+        export = generate_export(Export.CSV_EXPORT, xform, None, export_options)
+        storage = storages["default"]
+        self.assertTrue(storage.exists(export.filepath))
+        path, ext = os.path.splitext(export.filename)
+        self.assertEqual(ext, ".csv")
+        with storage.open(export.filepath, "r") as csv_file:
+            reader = csv.reader(csv_file)
+            rows = [row for row in reader]
+            actual_headers = [h for h in rows[0]]
+            expected_headers = [
+                "name",
+                "age",
+                "picture",
+                "has_children",
+                "children[1]/childs_name",
+                "children[1]/childs_age",
+                "children[1]/child_is_nursing",
+                "children[1]/childs_colors/red",
+                "children[1]/childs_colors/blue",
+                "children[1]/childs_colors/green",
+                "children[1]/childs_colors/white",
+                "children[1]/childs_colors/orange",
+                "children[2]/childs_name",
+                "children[2]/childs_age",
+                "children[2]/child_is_nursing",
+                "children[2]/childs_colors/red",
+                "children[2]/childs_colors/blue",
+                "children[2]/childs_colors/green",
+                "children[2]/childs_colors/white",
+                "children[2]/childs_colors/orange",
+                "gps",
+                "web_browsers/chrome",
+                "web_browsers/firefox",
+                "web_browsers/ie",
+                "web_browsers/safari",
+                "meta/instanceID",
+                "_id",
+                "_uuid",
+                "_submission_time",
+                "_date_modified",
+                "_tags",
+                "_notes",
+                "_version",
+                "_duration",
+                "_gps_altitude",
+                "_gps_latitude",
+                "_gps_longitude",
+                "_gps_precision",
+                "_submitted_by",
+                "_total_media",
+                "_media_count",
+                "_media_all_received",
+            ]
+            self.assertEqual(sorted(expected_headers), sorted(actual_headers))
+            self.assertEqual(
+                rows[1][:30],
+                [
+                    "Bob",
+                    "25",
+                    "n/a",
+                    "1",
+                    "Tom",
+                    "12",
+                    "0",
+                    "True",
+                    "True",
+                    "False",
+                    "False",
+                    "False",
+                    "Dick",
+                    "1",
+                    "1",
+                    "False",
+                    "False",
+                    "True",
+                    "False",
+                    "True",
+                    "-1.2625621 36.7921711 0.0 20.0",
+                    "-1.2625621",
+                    "36.7921711",
+                    "0.0",
+                    "20.0",
+                    "True",
+                    "True",
+                    "False",
+                    "False",
+                    "uuid:b31c6ac2-b8ca-4180-914f-c844fa10ed3b",
+                ],
+            )
+        export_options = {"extension": "csv", "split_select_multiples": False}
+        export = generate_export(Export.CSV_EXPORT, xform, None, export_options)
+        storage = storages["default"]
+        self.assertTrue(storage.exists(export.filepath))
+        path, ext = os.path.splitext(export.filename)
+        self.assertEqual(ext, ".csv")
+        with storage.open(export.filepath, "r") as csv_file:
+            reader = csv.reader(csv_file)
+            rows = [row for row in reader]
+            actual_headers = [h for h in rows[0]]
+            expected_headers = [
+                "name",
+                "age",
+                "picture",
+                "has_children",
+                "children[1]/childs_name",
+                "children[1]/childs_age",
+                "children[1]/child_is_nursing",
+                "children[1]/childs_colors",
+                "children[2]/childs_name",
+                "children[2]/childs_age",
+                "children[2]/child_is_nursing",
+                "children[2]/childs_colors",
+                "gps",
+                "web_browsers",
+                "meta/instanceID",
+                "_id",
+                "_uuid",
+                "_submission_time",
+                "_date_modified",
+                "_tags",
+                "_notes",
+                "_version",
+                "_duration",
+                "_gps_altitude",
+                "_gps_latitude",
+                "_gps_longitude",
+                "_gps_precision",
+                "_submitted_by",
+                "_total_media",
+                "_media_count",
+                "_media_all_received",
+            ]
+            self.assertEqual(sorted(expected_headers), sorted(actual_headers))
+            self.assertEqual(
+                rows[1][:19],
+                [
+                    "Bob",
+                    "25",
+                    "n/a",
+                    "1",
+                    "Tom",
+                    "12",
+                    "0",
+                    "red blue",
+                    "Dick",
+                    "1",
                     "1",
                     "green orange",
                     "-1.2625621 36.7921711 0.0 20.0",
