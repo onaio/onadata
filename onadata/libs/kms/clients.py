6d8a2d4805ae7ee026fe0ab6f954957bf1fd213f
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2025-06-10 14:27:05 +0300

    save KMSAPIClient token to avoid unnecessary re-authentiction by different processes

--- onadata/libs/kms/clients.py
+++ onadata/libs/kms/clients.py
@@ -5,6 +5,15 @@ from valigetta.exceptions import InvalidAPIURLException
 from valigetta.kms import APIKMSClient as BaseAPIClient
 from valigetta.kms import AWSKMSClient as BaseAWSClient
 
+from onadata.libs.utils.cache_tools import (
+    KMS_TOKEN_CACHE_KEY,
+    KMS_TOKEN_CACHE_TTL,
+    safe_cache_get,
+    safe_cache_set,
+)
+
+# pylint: disable=too-few-public-methods
+
 
 def setting(name, default=None):
     """
@@ -19,67 +28,38 @@ def setting(name, default=None):
     return getattr(settings, name, default)
 
 
-# pylint: disable=too-few-public-methods
-class BaseClient:
-    def __init__(self, **custom_settings):
-        default_settings = self.get_default_settings()
-
-        for name, value in default_settings.items():
-            if not hasattr(self, name):
-                setattr(self, name, value)
-
-        for name, value in custom_settings.items():
-            if name not in default_settings:
-                raise ImproperlyConfigured(
-                    f"Invalid setting '{name}' for {self.__class__.__name__}"
-                )
-            setattr(self, name, value)
-
-    def get_default_settings(self):
-        return {}
-
-
-class AWSKMSClient(BaseClient, BaseAWSClient):
-    def __init__(self, **custom_settings):
-        BaseClient.__init__(self, **custom_settings)
-        BaseAWSClient.__init__(
-            self,
-            aws_access_key_id=self.aws_access_key_id,
-            aws_secret_access_key=self.aws_secret_access_key,
-            region_name=self.region_name,
-        )
-
-    def get_default_settings(self):
-        return {
-            "aws_access_key_id": setting(
+class AWSKMSClient(BaseAWSClient):
+    def __init__(self):
+        super().__init__(
+            aws_access_key_id=setting(
                 "AWS_KMS_ACCESS_KEY_ID", setting("AWS_ACCESS_KEY_ID")
             ),
-            "aws_secret_access_key": setting(
+            aws_secret_access_key=setting(
                 "AWS_KMS_SECRET_ACCESS_KEY", setting("AWS_SECRET_ACCESS_KEY")
             ),
-            "region_name": setting("AWS_KMS_REGION_NAME"),
-        }
+            region_name=setting("AWS_KMS_REGION_NAME"),
+        )
 
 
-class APIKMSClient(BaseClient, BaseAPIClient):
-    def __init__(self, **custom_settings):
-        BaseClient.__init__(self, **custom_settings)
-
+class APIKMSClient(BaseAPIClient):
+    def __init__(self):
         try:
-            BaseAPIClient.__init__(
-                self,
-                client_id=self.client_id,
-                client_secret=self.client_secret,
-                urls=self.urls,
+            super().__init__(
+                client_id=setting("KMS_API_CLIENT_ID"),
+                client_secret=setting("KMS_API_CLIENT_SECRET"),
+                urls=setting("KMS_API_URLS"),
+                token=self.get_token_from_cache(),
+                on_token_refresh=self.save_token_to_cache,
             )
         except InvalidAPIURLException as exc:
             raise ImproperlyConfigured(
                 f"Invalid setting 'KMS_API_URLS' for {self.__class__.__name__}", exc
             ) from exc
 
-    def get_default_settings(self):
-        return {
-            "client_id": setting("KMS_API_CLIENT_ID"),
-            "client_secret": setting("KMS_API_CLIENT_SECRET"),
-            "urls": setting("KMS_API_URLS"),
-        }
+    @classmethod
+    def save_token_to_cache(cls, token):
+        safe_cache_set(KMS_TOKEN_CACHE_KEY, token, KMS_TOKEN_CACHE_TTL)
+
+    @classmethod
+    def get_token_from_cache(cls):
+        return safe_cache_get(KMS_TOKEN_CACHE_KEY)
