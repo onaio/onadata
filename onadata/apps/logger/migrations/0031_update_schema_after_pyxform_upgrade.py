05e8627ce4b7b0374c4a21b6038e13b0a0691450
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2025-08-22 12:11:16 +0300

    improve performance for migration

--- onadata/apps/logger/migrations/0031_update_schema_after_pyxform_upgrade.py
+++ onadata/apps/logger/migrations/0031_update_schema_after_pyxform_upgrade.py
@@ -6,6 +6,7 @@ import logging
 
 from django.db import migrations
 
+from multidb.pinning import use_master
 from pyxform.builder import create_survey_element_from_dict
 from pyxform.errors import PyXFormError
 
@@ -92,41 +93,46 @@ def update_xform_schema(apps, schema_editor):
     from onadata.apps.logger.models.xform import XForm as LiveXForm
 
     XForm = apps.get_model("logger", "XForm")
-    xform_qs = XForm.objects.filter(deleted_at__isnull=True, encrypted=False).only(
-        "id", "json"
-    )
+    xform_qs = XForm.objects.filter(deleted_at__isnull=True, encrypted=False).only("id")
     processed = 0
     patched = 0
 
-    for xform in xform_qs.iterator(chunk_size=50):
-        processed += 1
-        print(f"processed {processed} xforms")
+    with use_master():
+        for xform in xform_qs.iterator(chunk_size=50):
+            processed += 1
+            print(f"processed {processed} xforms")
 
-        try:
-            json_data = (
-                json.loads(xform.json) if isinstance(xform.json, str) else xform.json
-            )
-            _ = create_survey_element_from_dict(json_data)
-
-        except (KeyError, PyXFormError):
             try:
-                # Try to recreate the full schema from the xlsform
-                survey = LiveXForm.objects.get(id=xform.id).get_survey_from_xlsform()
-                XForm.objects.filter(id=xform.id).update(json=survey.to_json_dict())
+                json_data = (
+                    json.loads(xform.json)
+                    if isinstance(xform.json, str)
+                    else xform.json
+                )
+                _ = create_survey_element_from_dict(json_data)
 
             except (KeyError, PyXFormError):
-                # If the full schema creation fails, try to patch the JSON
-                print(f"recreating XForm {xform.id} failed, perfoming patch")
-                process_children(json_data["children"], ensure_choices_exist(json_data))
-                XForm.objects.filter(id=xform.id).update(json=json_data)
-                patched += 1
+                try:
+                    # Try to recreate the full schema from the xlsform
+                    survey = LiveXForm.objects.get(
+                        id=xform.id
+                    ).get_survey_from_xlsform()
+                    XForm.objects.filter(id=xform.id).update(json=survey.to_json_dict())
 
-        except TypeError:
-            pass
+                except (KeyError, PyXFormError):
+                    # If the full schema creation fails, try to patch the JSON
+                    print(f"recreating XForm {xform.id} failed, perfoming patch")
+                    process_children(
+                        json_data["children"], ensure_choices_exist(json_data)
+                    )
+                    XForm.objects.filter(id=xform.id).update(json=json_data)
+                    patched += 1
 
-        except Exception as e:
-            print(xform.pk, xform, e, type(e))
-            break
+            except TypeError:
+                pass
+
+            except Exception as e:
+                print(xform.pk, xform, e, type(e))
+                break
 
     print(f"patched {patched} xforms")
 
