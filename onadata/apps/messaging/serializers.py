f2f27837e2da36e8787b404c4a42170fb22d3a23
FrankApiyo <franklineapiyo@gmail.com>
2025-11-20 14:59:34 +0300

    Fix static analysis error

--- onadata/apps/messaging/serializers.py
+++ onadata/apps/messaging/serializers.py
@@ -78,6 +78,7 @@ class MessageSerializer(serializers.ModelSerializer):
             for field in extra_fields:
                 self.fields.pop(field)
 
+    # pylint: disable=too-many-locals
     def create(self, validated_data):
         """
         Creates the Message in the Action model
