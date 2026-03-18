f9c1fb60d751c5fd596cdcabfc9c51c7881a86db
Muchiri <kelvinmuchiri63@gmail.com>
2026-03-18 14:09:03 +0300

    fix: entity rejected if duplicate in other project
    Entity should be created if a duplicate Entity with the same uuid belongs to a different project. Only duplicates within the same project should be disallowed

---
+++ onadata/libs/utils/tests/test_entities_utils.py
@@ -0,0 +1,101 @@
+"""Tests for module onadata.libs.utils.entity_utils"""
+
+from django.db.models.signals import post_save
+
+from onadata.apps.logger.models import Entity, EntityList, Instance, Project
+from onadata.apps.logger.signals import create_or_update_entity
+from onadata.apps.main.tests.test_base import TestBase
+from onadata.libs.utils.entities_utils import create_or_update_entity_from_instance
+
+
+class CreateUpdateEntityTestCase(TestBase):
+    """Tests for create_or_update_entity_from_instance"""
+
+    @classmethod
+    def setUpClass(cls):
+        # Disable signals
+        post_save.disconnect(sender=Instance, dispatch_uid="create_or_update_entity")
+
+    @classmethod
+    def tearDownClass(cls):
+        # Re-enable signals
+        post_save.connect(
+            sender=Instance,
+            dispatch_uid="create_or_update_entity",
+            receiver=create_or_update_entity,
+        )
+
+    def setUp(self):
+        super().setUp()
+
+        self.project = Project.objects.create(
+            name="Entities", created_by=self.user, organization=self.user
+        )
+        self.xform = self._publish_registration_form(self.user)
+        self.xml = (
+            '<?xml version="1.0" encoding="UTF-8"?>'
+            '<data xmlns:jr="http://openrosa.org/javarosa" xmlns:orx='
+            '"http://openrosa.org/xforms" id="trees_registration" version="2022110901">'
+            "<formhub><uuid>d156a2dce4c34751af57f21ef5c4e6cc</uuid></formhub>"
+            "<location>-1.286905 36.772845 0 0</location>"
+            "<species>purpleheart</species>"
+            "<circumference>300</circumference>"
+            "<intake_notes />"
+            "<meta>"
+            "<instanceID>uuid:9d3f042e-cfec-4d2a-8b5b-212e3b04802b</instanceID>"
+            "<instanceName>300cm purpleheart</instanceName>"
+            '<entity create="1" dataset="trees" id="dbee4c32-a922-451c-9df7-42f40bf78f48">'
+            "<label>300cm purpleheart</label>"
+            "</entity>"
+            "</meta>"
+            "</data>"
+        )
+        self.instance = Instance.objects.create(
+            xml=self.xml, user=self.user, xform=self.xform
+        )
+
+    def test_entity_created_if_duplicate_other_project(self):
+        """Entity is created if duplicate uuid belongs to different project"""
+        # Simulate an existing Entity same uuid, different project
+        project = Project.objects.create(
+            name="Project B", created_by=self.user, organization=self.user
+        )
+        self._publish_registration_form(self.user, project)
+        entity_list = EntityList.objects.get(name="trees", project=project)
+        Entity.objects.create(
+            entity_list=entity_list,
+            json={
+                "species": "purpleheart",
+                "geometry": "-1.286905 36.772845 0 0",
+                "circumference_cm": 30,
+                "label": "300cm purpleheart",
+            },
+            uuid="dbee4c32-a922-451c-9df7-42f40bf78f48",
+        )
+
+        self.assertEqual(Entity.objects.count(), 1)
+
+        create_or_update_entity_from_instance(self.instance)
+
+        self.assertEqual(Entity.objects.count(), 2)
+
+    def test_entity_rejected_if_duplicate_same_project(self):
+        """Entity is rejected if duplicate uuid belongs to same project"""
+        # Simulate an existing Entity same uuid, same project
+        entity_list = EntityList.objects.get(name="trees", project=self.project)
+        Entity.objects.create(
+            entity_list=entity_list,
+            json={
+                "species": "purpleheart",
+                "geometry": "-1.286905 36.772845 0 0",
+                "circumference_cm": 30,
+                "label": "300cm purpleheart",
+            },
+            uuid="dbee4c32-a922-451c-9df7-42f40bf78f48",
+        )
+
+        self.assertEqual(Entity.objects.count(), 1)
+
+        create_or_update_entity_from_instance(self.instance)
+
+        self.assertEqual(Entity.objects.count(), 1)
