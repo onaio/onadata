db903e968c09987aad5f1189f75e94f9acf3a824
Muchiri <kelvinmuchiri63@gmail.com>
2026-09-21 12:06:55 +0300

    chore: update docstring

--- onadata/apps/api/viewsets/v2/organization_profile_viewset.py
+++ onadata/apps/api/viewsets/v2/organization_profile_viewset.py
@@ -36,7 +36,7 @@ from onadata.libs.utils.cache_tools import safe_cache_get, safe_cache_set
 
 # pylint: disable=too-many-ancestors
 class OrganizationProfileViewSet(OrganizationProfileViewSetV1):
-    """List, Retrieve, Update, Create/Register Organizations."""
+    """List, Retrieve, Update, Create Organizations."""
 
     # Ordered so that pages neither overlap nor skip records. The users are
     # selected as every organization in a list links to them.
